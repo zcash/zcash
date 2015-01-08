2b1c458db41b6a0ec898473a8d6360587cf9e076
Nathan Wilcox <nejucomo@gmail.com>
2015-01-08 15:16:31 -0800

    Enable automake verbosity in the build.sh script; this causes make to show each compilation/linking command prior to execution.  (Also, pass any other args to make.)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./autogen.sh
 CPPFLAGS='-I../../libzerocash -I../../libzerocash/depinst/include -I../../libzerocash/depinst/include/libsnark -DCURVE_BN128 -Wno-literal-suffix -std=c++11' ./configure --enable-hardening --with-incompatible-bdb
-make
+make AM_DEFAULT_VERBOSITY=1 "$@"
