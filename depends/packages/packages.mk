cce7754eb809472015b1611a3b6a029d0b476837
Cory Fields <cory-nospam-@coryfields.com>
2015-05-05 20:30:20 -0400

    Depends: Add ZeroMQ package

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,7 @@
 zcash_packages := libsnark libgmp libsodium
 packages := boost openssl $(zcash_packages) googletest googlemock
+packages_darwin:=zeromq
+packages_linux:=zeromq
 native_packages := native_ccache
 
 wallet_packages=bdb
