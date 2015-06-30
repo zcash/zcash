81ae51c8834d2544d69d39448fdd0df221a1d4ee
mruddy <mruddy@users.noreply.github.com>
2015-06-30 09:40:44 -0400

    add tests for the decodescript rpc. add mention of the rpc regression tests to the testing seciton of the main readme.

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -35,6 +35,7 @@ testScripts=(
     'walletbackup.py'
     'nodehandling.py'
     'reindex.py'
+    'decodescript.py'
     'zcjoinsplit.py'
     'zcjoinsplitdoublespend.py'
     'getblocktemplate.py'
