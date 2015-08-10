c660c033321f48f5894453881b18a3e15af9f0d1
Daira Hopwood <daira@jacaranda.org>
2015-08-10 23:29:49 +0100

    Build without Qt. refs #176
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 # BUG: parameterize the platform/host directory:
 PREFIX="$(pwd)/depends/x86_64-unknown-linux-gnu/"
 
-make -C ./depends/ V=1
+make -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
-./configure --prefix="${PREFIX}"
+./configure --prefix="${PREFIX}" --with-gui=no
 make V=1
