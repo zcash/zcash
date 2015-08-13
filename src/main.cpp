e279e5f90aeaba08285a5c0c7a1b00cb3c2106b5
Patrick Strateman <patrick.strateman@gmail.com>
2015-08-13 02:31:46 -0700

    Record nMinPingUsecTime

--- src/main.cpp
+++ src/main.cpp
@@ -4937,6 +4937,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                     if (pingUsecTime > 0) {
                         // Successful ping time measurement, replace previous
                         pfrom->nPingUsecTime = pingUsecTime;
+                        pfrom->nMinPingUsecTime = std::min(pfrom->nMinPingUsecTime, pingUsecTime);
                     } else {
                         // This should never happen
                         sProblem = "Timing mishap";
