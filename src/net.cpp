75c0598caccfed9f505ebd6c6e13f5dad9470bcc
Patrick Strateman <patrick.strateman@gmail.com>
2015-08-25 15:33:29 -0700

    CNodeRef copy constructor and assignment operator

--- src/net.cpp
+++ src/net.cpp
@@ -678,6 +678,22 @@ public:
 
     CNode& operator *() const {return *_pnode;};
     CNode* operator ->() const {return _pnode;};
+
+    CNodeRef& operator =(const CNodeRef& other)
+    {
+        if (this != &other) {
+            _pnode->Release();
+            _pnode = other._pnode;
+            _pnode->AddRef();
+        }
+        return *this;
+    }
+
+    CNodeRef(const CNodeRef& other):
+        _pnode(other._pnode)
+    {
+        _pnode->AddRef();
+    }
 private:
     CNode *_pnode;
 };
