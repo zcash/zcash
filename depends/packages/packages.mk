0388c23a768f12dc47c1d2cdd49ee0f66577644b
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-09-17 14:30:46 +0200

    depends: fix platform specific packages variable
    prefix instead of postfix

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,7 @@
 zcash_packages := libsnark libgmp libsodium
 packages := boost openssl $(zcash_packages) googletest googlemock
-packages_darwin:=zeromq
-packages_linux:=zeromq
+darwin_packages:=zeromq
+linux_packages:=zeromq
 native_packages := native_ccache
 
 wallet_packages=bdb
