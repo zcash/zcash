89bccddcd8195059b64567bfb9c3cfe5ca0138cd
Wladimir J. van der Laan <laanwj@gmail.com>
2015-09-18 15:45:38 +0200

    rpc: Split option -rpctimeout into -rpcservertimeout and -rpcclienttimeout
    The two timeouts for the server and client, are essentially different:
    
    - In the case of the server it should be a lower value to avoid clients
    clogging up connection slots
    
    - In the case of the client it should be a high value to accomedate slow
      responses from the server, for example for slow queries or when the
      lock is contended
    
    Split the options into `-rpcservertimeout` and `-rpcclienttimeout` with
    respective defaults of 30 and 900.

--- src/init.cpp
+++ src/init.cpp
@@ -484,7 +484,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-rpcthreads=<n>", strprintf(_("Set the number of threads to service RPC calls (default: %d)"), DEFAULT_HTTP_THREADS));
     if (showDebug) {
         strUsage += HelpMessageOpt("-rpcworkqueue=<n>", strprintf("Set the depth of the work queue to service RPC calls (default: %d)", DEFAULT_HTTP_WORKQUEUE));
-        strUsage += HelpMessageOpt("-rpctimeout=<n>", strprintf("Timeout during HTTP requests (default: %d)", DEFAULT_HTTP_TIMEOUT));
+        strUsage += HelpMessageOpt("-rpcservertimeout=<n>", strprintf("Timeout during HTTP requests (default: %d)", DEFAULT_HTTP_SERVER_TIMEOUT));
     }
 
     // Disabled until we can lock notes and also tune performance of libsnark which by default uses multiple threads
