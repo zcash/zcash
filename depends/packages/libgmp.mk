2db8d3e686295e7e828ac34a6ea29844d6d33ddc
Taylor Hornby <havoc@defuse.ca>
2015-10-01 16:01:06 -0600

    Turn off libgmp shared libraries, so that it links statically.

--- depends/packages/libgmp.mk
+++ depends/packages/libgmp.mk
@@ -4,14 +4,14 @@ $(package)_download_path=https://gmplib.org/download/gmp/
 $(package)_file_name=gmp-6.0.0a.tar.bz2
 $(package)_sha256_hash=7f8e9a804b9c6d07164cf754207be838ece1219425d64e28cfa3e70d5c759aaf
 $(package)_dependencies=
-$(package)_config_opts=--enable-cxx
+$(package)_config_opts=--enable-cxx --disable-shared
 
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) CPPFLAGS='-fPIC'
 endef
 
 define $(package)_stage_cmds
