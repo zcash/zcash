7ad6f5cb0e33ed3a5cb899ac0e6d9c34461cab0b
Nathan Wilcox <nejucomo@gmail.com>
2015-10-06 12:58:59 -0700

    Complain loudly if the arch-specific build dir is not present to avoid false-positive passes.

--- qa/zerocash/ensure-no-dot-so-in-depends.py
+++ qa/zerocash/ensure-no-dot-so-in-depends.py
@@ -6,23 +6,29 @@ import os
 def main():
     this_script = os.path.abspath(sys.argv[0])
     basedir = os.path.dirname(this_script)
-    lib_folder = os.path.join(
+    arch_dir = os.path.join(
         basedir,
         '..',
         '..',
         'depends',
         'x86_64-unknown-linux-gnu',
-        'lib'
     )
-
-    libraries = os.listdir(lib_folder)
 
     exit_code = 0
 
-    for lib in libraries:
-        if lib.find(".so") != -1:
-            print lib
-            exit_code = 1
+    if os.path.isdir(arch_dir):
+        lib_dir = os.path.join(arch_dir, 'lib')
+        libraries = os.listdir(lib_dir)
+
+        for lib in libraries:
+            if lib.find(".so") != -1:
+                print lib
+                exit_code = 1
+    else:
+        exit_code = 2
+        print "arch-specific build dir not present: {}".format(arch_dir)
+        print "Did you build the ./depends tree?"
+        print "Are you on a currently unsupported architecture?"
 
     if exit_code == 0:
         print "PASS."
