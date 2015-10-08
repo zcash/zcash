4683a438a12681d7da046636363d35ef4911b237
Taylor Hornby <havoc@defuse.ca>
2015-10-08 15:51:13 -0600

    Build the libzerocash tests in bin/ and run them.

--- qa/zerocash/full-test-suite.sh
+++ qa/zerocash/full-test-suite.sh
@@ -26,9 +26,9 @@ function run_test_phase
 
 # Test phases:
 run_test_phase "${REPOROOT}/qa/zerocash/ensure-no-dot-so-in-depends.py"
-run_test_phase "${REPOROOT}/src/libzerocash/tests/zerocashTest"
-run_test_phase "${REPOROOT}/src/libzerocash/tests/merkleTest"
-run_test_phase "${REPOROOT}/src/libzerocash/zerocash_pour_ppzksnark/tests/test_zerocash_pour_ppzksnark"
+run_test_phase "${REPOROOT}/depends/x86_64-unknown-linux-gnu/bin/zerocashTest"
+run_test_phase "${REPOROOT}/depends/x86_64-unknown-linux-gnu/bin/merkleTest"
+run_test_phase "${REPOROOT}/depends/x86_64-unknown-linux-gnu/bin/test_zerocash_pour_ppzksnark"
 run_test_phase "${REPOROOT}/qa/zerocash/zc-system-test.py"
 
 exit $SUITE_EXIT_STATUS
