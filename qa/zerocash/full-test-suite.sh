c25538aa6b4a65b69f02dbd9d5befb71cae5154d
Taylor Hornby <havoc@defuse.ca>
2015-10-14 14:34:57 -0600

    Run full-test-suite.sh on Travis-CI.

--- qa/zerocash/full-test-suite.sh
+++ qa/zerocash/full-test-suite.sh
@@ -27,8 +27,8 @@ cd "${REPOROOT}"
 
 # Test phases:
 run_test_phase "${REPOROOT}/qa/zerocash/ensure-no-dot-so-in-depends.py"
+run_test_phase make check
 run_test_phase "${REPOROOT}/qa/zerocash/zc-system-test.py"
-run_test_phase "make check"
 
 exit $SUITE_EXIT_STATUS
 
