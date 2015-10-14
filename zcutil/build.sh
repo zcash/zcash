f5c0a8668706fb47813e41c0d38ccb2576e3ca6f
Taylor Hornby <havoc@defuse.ca>
2015-10-14 11:35:31 -0600

    Forward CLI arguments to build.sh to the two make commands.

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 # BUG: parameterize the platform/host directory:
 PREFIX="$(pwd)/depends/x86_64-unknown-linux-gnu/"
 
-make -C ./depends/ V=1 NO_QT=1
+make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
 ./configure --prefix="${PREFIX}" --with-gui=no CXXFLAGS='-O0 -g'
-make V=1
+make "$@" V=1
