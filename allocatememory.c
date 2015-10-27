3bc3205da105aa3dfd077e95ce1576e448587c0b
Taylor Hornby <havoc@defuse.ca>
2015-10-27 12:14:09 -0600

    Use alloc'd memory

--- allocatememory.c
+++ allocatememory.c
@@ -9,8 +9,16 @@
 
 int main(int argc, char **argv) {
     unsigned long long allocated = 0;
+    unsigned char *x;
+    int i;
     while (allocated < MAX_MEMORY) {
-        malloc(STEP);
+        x = malloc(STEP);
+        if (x == NULL) {
+            printf("null result!\n");
+        }
+        for (i = 0; i < STEP; i++) {
+            x[i] = i;
+        }
         allocated += STEP;
         sleep(1);
         printf("Allocated %llu\n", allocated);
