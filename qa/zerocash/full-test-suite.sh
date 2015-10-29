77ba6487c1764bc0f157638c7a44a9b65add953a
Sean Bowe <ewillbefull@gmail.com>
2015-10-28 23:09:48 -0600

    Added new test suite for zc-raw RPC

--- qa/zerocash/full-test-suite.sh
+++ qa/zerocash/full-test-suite.sh
@@ -31,6 +31,7 @@ cd "${REPOROOT}"
 run_test_phase "${REPOROOT}/qa/zerocash/ensure-no-dot-so-in-depends.py"
 run_test_phase make check
 run_test_phase "${REPOROOT}/qa/zerocash/zc-system-test.py"
+run_test_phase "${REPOROOT}/qa/zerocash/zc-raw-system-test.py"
 
 exit $SUITE_EXIT_STATUS
 
