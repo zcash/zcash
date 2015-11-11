1eb21653585599162bf6127bb4190ce3a0b9f4da
Taylor Hornby <havoc@defuse.ca>
2015-11-11 15:51:24 -0700

    Update zc-system-test.py

--- qa/zerocash/full-test-suite.sh
+++ qa/zerocash/full-test-suite.sh
@@ -30,8 +30,8 @@ cd "${REPOROOT}"
 # Test phases:
 run_test_phase "${REPOROOT}/qa/zerocash/ensure-no-dot-so-in-depends.py"
 run_test_phase make check
-run_test_phase "${REPOROOT}/qa/zerocash/zc-system-test.py"
-run_test_phase "${REPOROOT}/qa/zerocash/zc-raw-system-test.py"
+run_test_phase "${REPOROOT}/qa/zerocash/zc-system-test.py" --pk-path=$1 --vk-path=$2
+run_test_phase "${REPOROOT}/qa/zerocash/zc-raw-system-test.py" --pk-path=$1 --vk-path=$2
 
 exit $SUITE_EXIT_STATUS
 
