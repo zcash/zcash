db27db2e4358098f15ce9eae13d79c8f260fe083
MarcoFalke <falke.marco@gmail.com>
2015-11-15 17:58:01 +0100

    [qa] keypool: DRY: Use test framework

--- qa/rpc-tests/keypool.py
+++ qa/rpc-tests/keypool.py
@@ -8,14 +8,9 @@
 # Add python-bitcoinrpc to module search path:
 
 from test_framework.authproxy import JSONRPCException
-from test_framework.util import assert_equal, check_json_precision, initialize_chain, \
-    start_nodes, start_node, stop_nodes, wait_bitcoinds, bitcoind_processes
-
-import os
-import sys
-import shutil
-import tempfile
-import traceback
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal, initialize_chain, \
+    start_nodes, start_node, bitcoind_processes
 
 def check_array_result(object_array, to_match, expected):
     """
@@ -38,12 +33,15 @@ def check_array_result(object_array, to_match, expected):
     if num_matched == 0:
         raise AssertionError("No objects matched %s"%(str(to_match)))
 
-    def run_test(nodes, tmpdir):
+class KeyPoolTest(BitcoinTestFramework):
+
+    def run_test(self):
+        nodes = self.nodes
         # Encrypt wallet and wait to terminate
         nodes[0].encryptwallet('test')
         bitcoind_processes[0].wait()
         # Restart node 0
-        nodes[0] = start_node(0, tmpdir)
+        nodes[0] = start_node(0, self.options.tmpdir)
         # Keep creating keys
         addr = nodes[0].getnewaddress()
         try:
@@ -88,57 +86,12 @@ def check_array_result(object_array, to_match, expected):
         except JSONRPCException as e:
             assert_equal(e.error['code'], -12)
 
-    def main():
-        import optparse
+    def setup_chain(self):
+        print("Initializing test directory "+self.options.tmpdir)
+        initialize_chain(self.options.tmpdir)
 
-        parser = optparse.OptionParser(usage="%prog [options]")
-        parser.add_option("--nocleanup", dest="nocleanup", default=False, action="store_true",
-                        help="Leave bitcoinds and test.* datadir on exit or error")
-        parser.add_option("--srcdir", dest="srcdir", default="../../src",
-                        help="Source directory containing bitcoind/bitcoin-cli (default: %default%)")
-        parser.add_option("--tmpdir", dest="tmpdir", default=tempfile.mkdtemp(prefix="test"),
-                        help="Root directory for datadirs")
-        (options, args) = parser.parse_args()
-
-        os.environ['PATH'] = options.srcdir+":"+os.environ['PATH']
-
-        check_json_precision()
-
-        success = False
-        nodes = []
-        try:
-            print("Initializing test directory "+options.tmpdir)
-            if not os.path.isdir(options.tmpdir):
-                os.makedirs(options.tmpdir)
-            initialize_chain(options.tmpdir)
-
-            nodes = start_nodes(1, options.tmpdir, extra_args=[['-experimentalfeatures', '-developerencryptwallet']])
-
-            run_test(nodes, options.tmpdir)
-
-            success = True
-
-        except AssertionError as e:
-            print("Assertion failed: "+e.message)
-        except JSONRPCException as e:
-            print("JSONRPC error: "+e.error['message'])
-            traceback.print_tb(sys.exc_info()[2])
-        except Exception as e:
-            print("Unexpected exception caught during testing: ", e.error['message'], str(sys.exc_info()[0]))
-            traceback.print_tb(sys.exc_info()[2])
-
-        if not options.nocleanup:
-            print("Cleaning up")
-            stop_nodes(nodes)
-            wait_bitcoinds()
-            shutil.rmtree(options.tmpdir)
-
-        if success:
-            print("Tests successful")
-            sys.exit(0)
-        else:
-            print("Failed")
-            sys.exit(1)
+    def setup_network(self):
+        self.nodes = start_nodes(1, self.options.tmpdir, extra_args=[['-experimentalfeatures', '-developerencryptwallet']])
 
 if __name__ == '__main__':
-    main()
+    KeyPoolTest().main()
