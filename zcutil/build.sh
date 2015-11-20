c6200213cf030d739656c1ec9d8c09ec92967bd8
Nathan Wilcox <nejucomo@gmail.com>
2015-11-19 16:38:34 -0800

    Add some helpful followup tips to the end of ./zcutil/build.sh.

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -10,3 +10,18 @@ make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
 ./configure --prefix="${PREFIX}" --with-gui=no CXXFLAGS='-O0 -g'
 make "$@" V=1
+
+cat <<EOF
+
+NOTE:
+
+In order to run zcash, you need to fetch the zkSNARK parameters.
+Do so by running: $(readlink -f ./zcutil/fetch-params.sh)
+
+It is safe to run that script even if you have already installed the
+parameters (it will just verify their integrity).
+
+Once parameters are installed, you can run our thorough suite of tests
+with: $(readlink -f ./qa/zcash/full-test-suite.sh)
+
+EOF
