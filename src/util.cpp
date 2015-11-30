8a183335674e9a991a101b236695ecd20f56caea
Taylor Hornby <havoc@defuse.ca>
2015-11-30 13:03:21 -0700

    Rename .bitcoin (DataDir), bitcoind, and bitcoin-cli.

--- src/util.cpp
+++ src/util.cpp
@@ -402,13 +402,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Zcash
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Zcash
+    // Mac: ~/Library/Application Support/Zcash
+    // Unix: ~/.zcash
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Zcash";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -420,10 +420,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     TryCreateDirectory(pathRet);
-    return pathRet / "Bitcoin";
+    return pathRet / "Zcash";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".zcash";
 #endif
 #endif
 }
