7dd56a50b2f461e9a4c8bd6d6fbee6f07541699b
Nathan Wilcox <nejucomo@gmail.com>
2015-12-08 13:55:01 -0800

    Do not use bitcoin's current serialization headers. (This may introduce an incompatibility.) Also remove copy-paste error.

--- depends/packages/libzerocash.mk
+++ depends/packages/libzerocash.mk
@@ -8,10 +8,6 @@ $(package)_git_commit=29df1b8bb28dff1c3e7b5952944aa55b42c36f7b
 $(package)_dependencies=libsnark crypto++ openssl boost libgmp
 $(package)_patches=
 
-define $(package)_preprocess_cmds
-	rm libzerocash/allocators.h libzerocash/serialize.h libzerocash/streams.h
-endef
-
 # FIXME: How do we know, at the point where the _build_cms are run, that the
 # $(host_prefix)/include/libsnark folder is there? The lifecycle of that folder
 # is as follows:
@@ -26,7 +22,6 @@ endef
 #
 #                       ** Which is it? **
 #
-$(package)_cppflags += -I$(BASEDIR)/../src -I. -I$(host_prefix)/include -I$(host_prefix)/include/libsnark -DCURVE_ALT_BN128 -DBOOST_SPIRIT_THREADSAFE -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -std=c++11 -pipe -O2 -O0 -g -Wstack-protector -fstack-protector-all -fPIE -fvisibility=hidden
 $(package)_cppflags += -I$(BASEDIR)/../src -I. -I$(host_prefix)/include -I$(host_prefix)/include/libsnark -DCURVE_ALT_BN128 -DBOOST_SPIRIT_THREADSAFE -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -std=c++11 -pipe -O2 -O0 -g -Wstack-protector -fstack-protector-all -fPIE -fvisibility=hidden -fPIC
 
 define $(package)_build_cmds
