a3e249b805c96402856c99f07a09ac1c2f19a771
Nathan Wilcox <nejucomo@gmail.com>
2015-12-21 19:22:07 -0800

    Upgrade libzerocash to a recent version without bitcoin serialization; also remove cut'n'paste error.

--- depends/packages/libzerocash.mk
+++ depends/packages/libzerocash.mk
@@ -2,15 +2,11 @@ package=libzerocash
 $(package)_download_path=https://github.com/Electric-Coin-Company/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=b79290e4ef412c07d519995caa1016d78d8263c6805a8ecfa1f42f8754978f95
-$(package)_git_commit=29df1b8bb28dff1c3e7b5952944aa55b42c36f7b
+$(package)_sha256_hash=c758b1f2b3372fb0e228442745668d0498a183cd0a4bcc423271e4ff3ddde85e
+$(package)_git_commit=69df6c95d97a1f1ee1fece0a6a7eef7d6a577dbc
 
 $(package)_dependencies=libsnark crypto++ openssl boost libgmp
 $(package)_patches=
-
-define $(package)_preprocess_cmds
-	rm libzerocash/allocators.h libzerocash/serialize.h libzerocash/streams.h
-endef
 
 # FIXME: How do we know, at the point where the _build_cms are run, that the
 # $(host_prefix)/include/libsnark folder is there? The lifecycle of that folder
@@ -26,7 +22,6 @@ endef
 #
 #                       ** Which is it? **
 #
-$(package)_cppflags += -I$(BASEDIR)/../src -I. -I$(host_prefix)/include -I$(host_prefix)/include/libsnark -DCURVE_ALT_BN128 -DBOOST_SPIRIT_THREADSAFE -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -std=c++11 -pipe -O2 -O0 -g -Wstack-protector -fstack-protector-all -fPIE -fvisibility=hidden
 $(package)_cppflags += -I$(BASEDIR)/../src -I. -I$(host_prefix)/include -I$(host_prefix)/include/libsnark -DCURVE_ALT_BN128 -DBOOST_SPIRIT_THREADSAFE -DHAVE_BUILD_INFO -D__STDC_FORMAT_MACROS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -std=c++11 -pipe -O2 -O0 -g -Wstack-protector -fstack-protector-all -fPIE -fvisibility=hidden -fPIC
 
 define $(package)_build_cmds
