0bcfe2998f9afb2bd07dff6e6e721a28b3ae1421
Sean Bowe <ewillbefull@gmail.com>
2016-03-18 23:44:30 -0600

    Implementation and integration of ECIES replacement "NoteEncryption", and removal of crypto++ dependency.

---
+++ src/crypto/prf.h
@@ -0,0 +1,38 @@
+/*
+Zcash uses SHA256Compress as a PRF for various components
+within the zkSNARK circuit.
+*/
+
+#include "uint256.h"
+#include "crypto/sha256.h"
+#include <boost/static_assert.hpp>
+
+uint256 PRF(bool a, bool b, bool c, bool d,
+            uint256 x,
+            uint256 y)
+{
+    uint256 res;
+    unsigned char blob[64];
+
+    memcpy(&blob[0], x.begin(), 32);
+    memcpy(&blob[32], y.begin(), 32);
+
+    // ZCASH TODO: Justify bit-order endianness for this
+    blob[0] &= 0xF0;
+    blob[0] |= (a ? 1 << 0 : 0) | (b ? 1 << 1 : 0) | (c ? 1 << 2 : 0) | (d ? 1 << 3 : 0);
+
+    CSHA256 hasher;
+    hasher.Write(blob, 64);
+    hasher.FinalizeNoPadding(res.begin());
+
+    return res;
+}
+
+template<unsigned char T>
+uint256 PRF_addr(uint256 a_sk)
+{
+    uint256 y;
+    *(y.end() - 1) = T;
+
+    return PRF(0, 0, 0, 0, a_sk, y);
+}
