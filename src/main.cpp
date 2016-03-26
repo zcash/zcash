12772467d39868f3571a04bc7a05b0cd18431a4f
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-26 19:09:22 +0100

    Verify that outbound connections have expected services
    (cherry picked from commit fc83f181530fb566726e5f3f4197fc5586d77fd8)

--- src/main.cpp
+++ src/main.cpp
@@ -5664,6 +5664,15 @@ bool static ProcessMessage(const CChainParams& chainparams, CNode* pfrom, string
         {
             addrman.SetServices(pfrom->addr, pfrom->nServices);
         }
+        if (pfrom->nServicesExpected & ~pfrom->nServices)
+        {
+            LogPrint("net", "peer=%d does not offer the expected services (%08x offered, %08x expected); disconnecting\n", pfrom->id, pfrom->nServices, pfrom->nServicesExpected);
+            pfrom->PushMessage(NetMsgType::REJECT, strCommand, REJECT_NONSTANDARD,
+                               strprintf("Expected to offer services %08x", pfrom->nServicesExpected));
+            pfrom->fDisconnect = true;
+            return false;
+        }
+
         if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // disconnect from peers older than this proto version
