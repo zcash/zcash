5b709e7d001027a8ab42126ca30f0aa6ae7fd7a4
Taylor Hornby <taylor@defuse.ca>
2016-04-05 14:44:19 -0600

    Get an (unimplemented) performance measurement tool building.

---
+++ src/zerocash/tests/zerocashPerformance.cpp
@@ -0,0 +1,35 @@
+/** @file
+ *****************************************************************************
+
+ A test for Zerocash.
+
+ *****************************************************************************
+ * @author     This file is part of libzerocash, developed by the Zerocash
+ *             project and contributors (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#include <stdlib.h>
+#include <iostream>
+
+#include "timer.h"
+
+#include "zerocash/Zerocash.h"
+#include "zerocash/ZerocashParams.h"
+#include "zerocash/Address.h"
+#include "zerocash/CoinCommitment.h"
+#include "zerocash/Coin.h"
+#include "zerocash/IncrementalMerkleTree.h"
+#include "zerocash/MintTransaction.h"
+#include "zerocash/PourTransaction.h"
+#include "zerocash/PourInput.h"
+#include "zerocash/PourOutput.h"
+#include "zerocash/utils/util.h"
+
+using namespace std;
+using namespace libsnark;
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+}
