e42359dcbf6dc99037620abfb654d15a0815bc96
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-18 15:15:57 +0200

    test: Rename wallet.dat to wallet_test.dat
    Indicate that the file name is not hardcoded, and a little bit of safety
    so that it never nukes the main wallet.
    
    Suggestion by Marco Falke.

--- src/wallet/test/wallet_test_fixture.cpp
+++ src/wallet/test/wallet_test_fixture.cpp
@@ -9,7 +9,7 @@ WalletTestingSetup::WalletTestingSetup(): TestingSetup()
     bitdb.MakeMock();
 
     bool fFirstRun;
-    pwalletMain = new CWallet("wallet.dat");
+    pwalletMain = new CWallet("wallet_test.dat");
     pwalletMain->LoadWallet(fFirstRun);
     RegisterValidationInterface(pwalletMain);
 
