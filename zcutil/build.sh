d16d5ef832bde125364cee387e9a7f9fed600952
Nathan Wilcox <nejucomo@gmail.com>
2016-04-18 11:15:34 -0700

    Add coverage support scoped to only the zcash-gtest run; invoke with make zcash-cov; make cov is a superset.

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -1,12 +1,39 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
 
+set -eu -o pipefail
+
+if [ "x$*" = 'x--help' ]
+then
+    cat <<EOF
+Usage:
+
+$0 --help
+  Show this help message and exit.
+
+$0 [ --enable-lcov ] [ MAKEARGS... ]
+  Build Zcash and most of its transitive dependencies from
+  source. MAKEARGS are applied to both dependencies and Zcash itself. If
+  --enable-lcov is passed, Zcash is configured to add coverage
+  instrumentation, thus enabling "make cov" to work.
+EOF
+    exit 0
+fi
+
+set -x
 cd "$(dirname "$(readlink -f "$0")")/.."
+
+# If --enable-lcov is the first argument, enable lcov coverage support:
+LCOV_ARG=''
+if [ "x${1:-}" = 'x--enable-lcov' ]
+then
+    LCOV_ARG='--enable-lcov'
+    shift
+fi
 
 # BUG: parameterize the platform/host directory:
 PREFIX="$(pwd)/depends/x86_64-unknown-linux-gnu/"
 
 make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
-./configure --prefix="${PREFIX}" --with-gui=no CXXFLAGS='-O0 -g'
+./configure --prefix="${PREFIX}" --with-gui=no "$LCOV_ARG" CXXFLAGS='-O0 -g'
 make "$@" V=1
