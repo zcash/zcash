7b1acda395879f4ff4b430647b98328f785ba2e4
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-26 14:34:40 +0200

    chain: Add assertion in case of missing records in index db

--- src/chain.cpp
+++ src/chain.cpp
@@ -90,6 +90,7 @@ CBlockIndex* CBlockIndex::GetAncestor(int height)
             pindexWalk = pindexWalk->pskip;
             heightWalk = heightSkip;
         } else {
+            assert(pindexWalk->pprev);
             pindexWalk = pindexWalk->pprev;
             heightWalk--;
         }
