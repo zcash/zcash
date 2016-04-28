0000d2312134b8d9b2b855c399f527374c8eee23
Taylor Hornby <taylor@defuse.ca>
2016-04-28 10:04:24 -0600

    Add empty merkle/noteencryption tests so Sean can rebase.

---
+++ src/gtest/test_merkletree.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(tautologies, eight_eq_eight) {
+    ASSERT_EQ(8, 8);
+}
---
+++ src/gtest/test_noteencryption.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(tautologies, nine_eq_nine) {
+    ASSERT_EQ(9, 9);
+}
