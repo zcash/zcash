c75d6bd0fad60e1ab6421b481d5eb575f4a5ce3e
Sean Bowe <ewillbefull@gmail.com>
2016-05-10 17:46:58 -0600

    Initialize libsodium in the gtest suite.

--- src/gtest/main.cpp
+++ src/gtest/main.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
+#include "sodium.h"
 
 int main(int argc, char **argv) {
+  assert(sodium_init() != -1);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
