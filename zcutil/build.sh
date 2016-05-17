67f3fe3d253e1e4d7ef8b0a95387a7a082cc3a69
Taylor Hornby <taylor@defuse.ca>
2016-05-17 13:36:44 -0600

    Fix FORTIFY_SOURCE build errors.

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -35,5 +35,5 @@ PREFIX="$(pwd)/depends/x86_64-unknown-linux-gnu/"
 
 make "$@" -C ./depends/ V=1 NO_QT=1
 ./autogen.sh
-./configure --prefix="${PREFIX}" --with-gui=no "$LCOV_ARG" CXXFLAGS='-O0 -g'
+./configure --prefix="${PREFIX}" --with-gui=no "$LCOV_ARG" CXXFLAGS='-Og -g'
 make "$@" V=1
