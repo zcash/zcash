b3e712a4d22930b0cc62a430814345aa9f58f910
Jack Grigg <jack@z.cash>
2016-05-21 11:48:52 +1200

    Minor error message tweak

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -115,7 +115,7 @@ bool AppInit(int argc, char* argv[])
 
         if (fCommandLine)
         {
-            fprintf(stderr, "Error: There is no RPC client functionality in zcashd anymore. Use the zcash-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in zcashd. Use the zcash-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
