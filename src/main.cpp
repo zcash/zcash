e031b43c3cf7df5064b90cc133e282b429b4340f
Gregory Maxwell <greg@xiph.org>
2016-06-13 17:27:44 +0000

    Treat orphans as implicit inv for parents, discard when parents rejected.
    An orphan whos parents were rejected is never going to connect, so there
     is little utility in keeping it.
    
    Orphans also helpfully tell us what we're missing, so go ahead and treat
     it as INVed.
    
    (cherry picked from commit bitcoin/bitcoin@8c99d1b525562ab3b733e9d7ef770882646bad5c)
    
    Zcash: Adapted to our INV changes.

--- src/main.cpp
+++ src/main.cpp
@@ -7499,13 +7499,29 @@ bool static ProcessMessage(const CChainParams& chainparams, CNode* pfrom, string
                  !tx.GetSaplingBundle().IsPresent() &&
                  !tx.GetOrchardBundle().IsPresent())
         {
-            AddOrphanTx(tx, pfrom->GetId());
+            bool fRejectedParents = false; // It may be the case that the orphans parents have all been rejected
+            for (const CTxIn& txin : tx.vin) {
+                if (recentRejects->contains(txin.prevout.hash)) {
+                    fRejectedParents = true;
+                    break;
+                }
+            }
+            if (!fRejectedParents) {
+                for (const CTxIn& txin : tx.vin) {
+                    CInv inv(MSG_TX, txin.prevout.hash);
+                    pfrom->AddKnownTxId(inv.hash);
+                    if (!AlreadyHave(inv)) pfrom->AskFor(inv);
+                }
+                AddOrphanTx(tx, pfrom->GetId());
 
-            // DoS prevention: do not allow mapOrphanTransactions to grow unbounded
-            unsigned int nMaxOrphanTx = (unsigned int)std::max((int64_t)0, GetArg("-maxorphantx", DEFAULT_MAX_ORPHAN_TRANSACTIONS));
-            unsigned int nEvicted = LimitOrphanTxSize(nMaxOrphanTx);
-            if (nEvicted > 0)
-                LogPrint("mempool", "mapOrphan overflow, removed %u tx\n", nEvicted);
+                // DoS prevention: do not allow mapOrphanTransactions to grow unbounded
+                unsigned int nMaxOrphanTx = (unsigned int)std::max((int64_t)0, GetArg("-maxorphantx", DEFAULT_MAX_ORPHAN_TRANSACTIONS));
+                unsigned int nEvicted = LimitOrphanTxSize(nMaxOrphanTx);
+                if (nEvicted > 0)
+                    LogPrint("mempool", "mapOrphan overflow, removed %u tx\n", nEvicted);
+            } else {
+                LogPrint("mempool", "not keeping orphan with rejected parents %s\n",tx.GetHash().ToString());
+            }
         } else {
             // Add the wtxid of this transaction to our reject filter.
             // Unlike upstream Bitcoin Core, we can unconditionally add
