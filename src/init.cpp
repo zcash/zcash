dd74e9dd32ee78bb08b86b025c181b37ba4f0d9f
aniemerg <niemerg@gmail.com>
2016-06-19 09:54:54 -0400

    Suppress Libsnark Debugging Info
    Suppress the Libsnark debugging information displayed on launch

--- src/init.cpp
+++ src/init.cpp
@@ -632,6 +632,10 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     // ********************************************************* Step 0: Load zcash params
     ZC_LoadParams();
+    // These must be disabled for now, they are buggy and we probably don't
+    // want any of libsnark's profiling in production anyway.
+    libsnark::inhibit_profiling_info = true;
+    libsnark::inhibit_profiling_counters = true;
 
     // ********************************************************* Step 1: setup
 #ifdef _MSC_VER
@@ -1260,10 +1264,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         mempool.ReadFeeEstimates(est_filein);
     fFeeEstimatesInitialized = true;
 
-    // These must be disabled for now, they are buggy and we probably don't
-    // want any of libsnark's profiling in production anyway.
-    libsnark::inhibit_profiling_info = true;
-    libsnark::inhibit_profiling_counters = true;
 
     // ********************************************************* Step 8: load wallet
 #ifdef ENABLE_WALLET
