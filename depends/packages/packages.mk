00bcf2230dcc2e0c7ca66a951270795e4fe474aa
fanquake <fanquake@gmail.com>
2016-06-22 17:56:07 +0800

    [depends] ZeroMQ 4.1.5

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,5 @@
 zcash_packages := libsnark libgmp libsodium
-packages := boost openssl $(zcash_packages) googletest googlemock
-darwin_packages:=zeromq
-linux_packages:=zeromq
+packages := boost openssl zeromq $(zcash_packages) googletest googlemock
 native_packages := native_ccache
 
 wallet_packages=bdb
