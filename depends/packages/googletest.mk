487b4a5835b5f787e7bb59e288ddbe3fe6db7e06
Taylor Hornby <taylor@defuse.ca>
2016-06-22 19:45:10 -0600

    Igor Stenski's -m64 patch (see zcash-dev).

--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -6,7 +6,7 @@ $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=f73a6546fdf9fce9ff93a5015e0333a8af3062a152a9ad6bcb772c96687016cc
 
 define $(package)_build_cmds
-  $(MAKE) -C make CXXFLAGS=-fPIC gtest.a
+  $(MAKE) -C make CXXFLAGS=-fPIC -m64 gtest.a
 endef
 
 define $(package)_stage_cmds
