b78daf7de75a223eac467f1cbae185aaa8966f2a
Sean Bowe <ewillbefull@gmail.com>
2016-07-16 16:46:49 -0600

    Change `serial` to `nf` in txdb.

--- src/txdb.h
+++ src/txdb.h
@@ -35,7 +35,7 @@ public:
     CCoinsViewDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
 
     bool GetAnchorAt(const uint256 &rt, ZCIncrementalMerkleTree &tree) const;
-    bool GetNullifier(const uint256 &serial) const;
+    bool GetNullifier(const uint256 &nf) const;
     bool GetCoins(const uint256 &txid, CCoins &coins) const;
     bool HaveCoins(const uint256 &txid) const;
     uint256 GetBestBlock() const;
