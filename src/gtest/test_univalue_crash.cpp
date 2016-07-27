16fb96629680e357dc38902f6b31743c465b4cda
Taylor Hornby <taylor@defuse.ca>
2016-07-27 12:17:29 -0600

    Add univalue crash test case.

---
+++ src/gtest/test_univalue_crash.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include "univalue/univalue.h"
+#include "util.h"
+#include <stdint.h>
+#include <exception>
+
+#include "data/univalue_test_case.h"
+
+TEST(test_univalue_crash, test_crash) {
+    std::string response(json_test_string);
+
+    UniValue valReply(UniValue::VSTR);
+    if (!valReply.read(response)) {
+        throw std::runtime_error("couldn't parse the JSON");
+    }
+    if (valReply.empty()) {
+        throw std::runtime_error("The JSON is empty.");
+    }
+    //const UniValue& reply = valReply.get_obj();
+    //if (reply.empty()) {
+        //throw std::runtime_error("The JSON is empty.");
+    //}
+    ASSERT_TRUE(1==1);
+}
