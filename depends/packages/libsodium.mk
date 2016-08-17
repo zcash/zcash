a635d6e9959730a004894a18e4e258628d40c7aa
Taylor Hornby <taylor@defuse.ca>
2016-08-17 10:43:06 -0600

    Upgrade libsodium for AVX2-detection bugfix.

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,8 +1,8 @@
 package=libsodium
-$(package)_version=1.0.8
+$(package)_version=1.0.11
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
-$(package)_file_name=libsodium-1.0.8.tar.gz
-$(package)_sha256_hash=c0f191d2527852641e0a996b7b106d2e04cbc76ea50731b2d0babd3409301926
+$(package)_file_name=libsodium-1.0.11.tar.gz
+$(package)_sha256_hash=a14549db3c49f6ae2170cbbf4664bd48ace50681045e8dbea7c8d9fb96f9c765
 $(package)_dependencies=
 $(package)_config_opts=
 
