6c7cc8ebea366f5356943f97323e61684bd320b4
Gaurav Rana <bitcoinsSG@gmail.com>
2016-08-20 12:27:12 +0000

    update zcash-cli stop message
    changed "Bitcoin server stopping" to "Zcash server stopping"

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -254,10 +254,10 @@ Value stop(const Array& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Bitcoin server.");
+            "\nStop Zcash server.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    return "Bitcoin server stopping";
+    return "Zcash server stopping";
 }
 
 
