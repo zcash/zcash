8fd71a194eeb0e8ece1dff1f603c45b336d9e17b
Lars-Magnus Skog <ralphtheninja@riseup.net>
2016-08-21 15:09:58 +0200

    changed module name from "bitcoin" to "Zcash" in FormatException()

--- src/util.cpp
+++ src/util.cpp
@@ -381,7 +381,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "Zcash";
 #endif
     if (pex)
         return strprintf(
