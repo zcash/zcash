a0a3334c4df4798e247e95bb5a84718b240e2252
Simon <simon@bitcartel.com>
2016-09-06 15:15:33 -0700

    Add z_getbalance and z_gettotalbalance RPC calls to close #1201.

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -98,6 +98,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "zcbenchmark", 1 },
     { "zcbenchmark", 2 },
     { "getblocksubsidy", 0},
+    { "z_getbalance", 1},
+    { "z_gettotalbalance", 0},
     { "z_sendmany", 1},
     { "z_sendmany", 2},
     { "z_getoperationstatus", 0},
