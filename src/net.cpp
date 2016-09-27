e5bef4962b285acb5303fe0079269ff678c66335
Wladimir J. van der Laan <laanwj@gmail.com>
2016-09-27 14:05:24 +0200

    net: Consistent checksum handling
    In principle, the checksums of P2P packets are simply 4-byte blobs which
    are the first four bytes of SHA256(SHA256(payload)).
    
    Currently they are handled as little-endian 32-bit integers half of the
    time, as blobs the other half, sometimes copying the one to the other,
    resulting in somewhat confused code.
    
    This PR changes the handling to be consistent both at packet creation
    and receiving, making it (I think) easier to understand.
    
    (cherry picked from commit bitcoin/bitcoin@41e58faf043864a64a6db08a8df527fa5fd1ec5b)

--- src/net.cpp
+++ src/net.cpp
@@ -2356,10 +2356,8 @@ void CNode::EndMessage() UNLOCK_FUNCTION(cs_vSend)
 
     // Set the checksum
     uint256 hash = Hash(ssSend.begin() + CMessageHeader::HEADER_SIZE, ssSend.end());
-    unsigned int nChecksum = 0;
-    memcpy(&nChecksum, &hash, sizeof(nChecksum));
-    assert(ssSend.size () >= CMessageHeader::CHECKSUM_OFFSET + sizeof(nChecksum));
-    memcpy((char*)&ssSend[CMessageHeader::CHECKSUM_OFFSET], &nChecksum, sizeof(nChecksum));
+    assert(ssSend.size () >= CMessageHeader::CHECKSUM_OFFSET + CMessageHeader::CHECKSUM_SIZE);
+    memcpy((char*)&ssSend[CMessageHeader::CHECKSUM_OFFSET], hash.begin(), CMessageHeader::CHECKSUM_SIZE);
 
     LogPrint("net", "(%d bytes) peer=%d\n", nSize, id);
 
