e3320f4f0f2d5c5f7b6520a77d0828f6ae98e922
Kevin Gallagher <kevin@z.cash>
2016-10-11 10:56:01 -0700

    Increases timeout to 30s, wait before retry to 3s
    Default: 20 tries

--- zcutil/fetch-params.sh
+++ zcutil/fetch-params.sh
@@ -22,7 +22,7 @@ function fetch_params {
             --progress=dot:giga \
             --output-document="$dlname" \
             --continue \
-            --retry-connrefused --waitretry=1 --timeout=10 --tries 0 \
+            --retry-connrefused --waitretry=3 --timeout=30 \
             "$url"
 
         shasum -a 256 --check <<EOF
