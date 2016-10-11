f964718c838e0dff4721f44ac5118028a5d78457
Kevin Gallagher <kevin@z.cash>
2016-10-10 20:34:55 -0700

    Set wget retry options for fetching parameters
    This will retry upon refused connections and similar errors, wait 1 second
    before the next retry, time out after 10 seconds if no data is received or
    the connection times out, plus try an infinite number of times.

--- zcutil/fetch-params.sh
+++ zcutil/fetch-params.sh
@@ -22,6 +22,7 @@ function fetch_params {
             --progress=dot:giga \
             --output-document="$dlname" \
             --continue \
+            --retry-connrefused --waitretry=1 --timeout=10 --tries 0 \
             "$url"
 
         shasum -a 256 --check <<EOF
