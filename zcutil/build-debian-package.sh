3327ef3df05526b865549846efef1880e537fec7
Kevin Gallagher <kevin@z.cash>
2016-10-13 20:13:37 -0700

    Moves zcash-fetch-params to /usr/bin
    Since the file is managed by the package manager, this is a more appropriate
    location for that script. Also updates the timestamp in the changelog.

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -21,12 +21,12 @@ BUILD_DIR="$BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64"
 if [ -d $BUILD_DIR ]; then
     rm -R $BUILD_DIR
 fi
-mkdir -p $BUILD_DIR/DEBIAN $BUILD_DIR/usr/bin $BUILD_DIR/usr/local/bin
+mkdir -p $BUILD_DIR/DEBIAN $BUILD_DIR/usr/bin
 
 cp -r $SRC_PATH/contrib/DEBIAN/* $BUILD_DIR/DEBIAN/
 cp $SRC_PATH/src/zcashd $BUILD_DIR/usr/bin/
 cp $SRC_PATH/src/zcash-cli $BUILD_DIR/usr/bin/
-cp $SRC_PATH/zcutil/fetch-params.sh $BUILD_DIR/usr/local/bin/zcash-fetch-params
+cp $SRC_PATH/zcutil/fetch-params.sh $BUILD_DIR/usr/bin/zcash-fetch-params
 
 # Create the deb package
 dpkg-deb --build $BUILD_DIR
