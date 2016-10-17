5f089b00f206d094f283e2b946848dfca5654f81
jl777 <jameslee777@yahoo.com>
2016-10-17 13:47:57 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2952,8 +2952,8 @@ bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, bool f
     if (block.GetBlockTime() > GetAdjustedTime() + 60)
         return state.Invalid(error("CheckBlockHeader(): block timestamp too far in the future"),REJECT_INVALID, "time-too-new");
     nBits = block.nBits;
-    if ( block.nHeight < NOTARIZED_HEIGHT || (block.nHeight == NOTARIZED_HEIGHT && NOTARIZED_HASH != block.GetHash()) )
-        return(false);
+    //if ( block.nHeight < NOTARIZED_HEIGHT || (block.nHeight == NOTARIZED_HEIGHT && NOTARIZED_HASH != block.GetHash()) )
+    //    return(false);
     if ( (retval= komodo_blockhdrcheck(&block,&nBits)) == 0 )
     {
         // Check Equihash solution is valid
