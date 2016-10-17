9e715874743d89e2364283ed75231d70a0449dff
jl777 <jameslee777@yahoo.com>
2016-10-17 14:35:48 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2952,7 +2952,7 @@ bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, bool f
     if (block.GetBlockTime() > GetAdjustedTime() + 60)
         return state.Invalid(error("CheckBlockHeader(): block timestamp too far in the future"),REJECT_INVALID, "time-too-new");
     nBits = block.nBits;
-    if ( index(block).nHeight < NOTARIZED_HEIGHT )//|| (block.nHeight == NOTARIZED_HEIGHT && NOTARIZED_HASH != block.GetHash()) )
+    if ( CBlockIndex::CBlockIndex(block).nHeight < NOTARIZED_HEIGHT )//|| (block.nHeight == NOTARIZED_HEIGHT && NOTARIZED_HASH != block.GetHash()) )
         return(false);
     if ( (retval= komodo_blockhdrcheck(&block,&nBits)) == 0 )
     {
