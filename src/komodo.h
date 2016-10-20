130231219a86f17f56ff766c285ae9b150d2ce9a
jl777 <jameslee777@yahoo.com>
2016-10-20 14:34:15 -0300

    test

--- src/komodo.h
+++ src/komodo.h
@@ -51,7 +51,7 @@ int32_t komodo_threshold(int32_t height,uint64_t signedmask)
 
 #define CRYPTO777_PUBSECPSTR "020e46e79a2a8d12b9b5d12c7a91adb4e454edfae43c0a0cb805427d2ac7613fd9"
 
-const char *Notaries[64][2] =
+const char *Notaries[][2] =
 {
     { "jl777_testA", "03b7621b44118017a16043f19b30cc8a4cfe068ac4e42417bae16ba460c80f3828" },
     { "jl777_testB", "02ebfc784a4ba768aad88d44d1045d240d47b26e248cafaf1c5169a42d7a61d344" },
