d8e06e3f580f799c2774807ac98a5d91890cc1e3
Jack Grigg <jack@z.cash>
2016-10-20 14:45:44 -0500

    Add test that encryptwallet is disabled

--- src/test/rpc_wallet_tests.cpp
+++ src/test/rpc_wallet_tests.cpp
@@ -1090,6 +1090,9 @@ BOOST_AUTO_TEST_CASE(rpc_wallet_encrypted_wallet_zkeys)
     Array arr = retValue.get_array();
     BOOST_CHECK(arr.size() == n);
 
+    // Verify that the wallet encryption RPC is disabled
+    BOOST_CHECK_THROW(CallRPC("encryptwallet passphrase"), runtime_error);
+
     // Encrypt the wallet (we can't call RPC encryptwallet as that shuts down node)
     SecureString strWalletPass;
     strWalletPass.reserve(100);
