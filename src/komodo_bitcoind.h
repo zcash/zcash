46ff7513444427c6ba685a617f2599f1d88917a4
jl777 <jameslee777@yahoo.com>
2016-10-25 18:09:30 -0300

    test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -102,7 +102,7 @@ char *komodo_issuemethod(char *method,char *params)
     }
     return(retstr);
 }
-//curl --url "http://127.0.0.1:14555" --user "user782512472:pass1476445872" --data "{\"method\":\"getinfo\",\"params\":[]}"
+//curl --url "http://127.0.0.1:13033" --user "user1557335368:pass111720054" --data "{\"method\":\"getinfo\",\"params\":[]}"
 
 uint32_t komodo_txtime(uint256 hash)
 {
