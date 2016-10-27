b7f7def877f839ad685a7a2f83b5e57fe7d97879
Jack Grigg <jack@z.cash>
2016-10-27 08:43:07 -0700

    Rework zcutil/build-debian-package.sh to place files correctly
    See http://www.tldp.org/HOWTO/html_single/Debian-Binary-Package-Building-HOWTO/
    for a guide on how to prepare a distribution for using dpkg-deb.

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -8,6 +8,7 @@ set -x
 BUILD_PATH="/tmp/zcbuild"
 PACKAGE_NAME="zcash"
 SRC_PATH=`pwd`
+SRC_DEB=$SRC_PATH/contrib/DEBIAN
 
 umask 022
 
@@ -21,12 +22,38 @@ BUILD_DIR="$BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64"
 if [ -d $BUILD_DIR ]; then
     rm -R $BUILD_DIR
 fi
-mkdir -p $BUILD_DIR/DEBIAN $BUILD_DIR/usr/bin
 
-cp -r $SRC_PATH/contrib/DEBIAN/* $BUILD_DIR/DEBIAN/
-cp $SRC_PATH/src/zcashd $BUILD_DIR/usr/bin/
-cp $SRC_PATH/src/zcash-cli $BUILD_DIR/usr/bin/
-cp $SRC_PATH/zcutil/fetch-params.sh $BUILD_DIR/usr/bin/zcash-fetch-params
+DEB_BIN=$BUILD_DIR/usr/bin
+DEB_DOC=$BUILD_DIR/usr/share/doc/$PACKAGE_NAME
+DEB_MAN=$BUILD_DIR/usr/share/man/man1
+mkdir -p $BUILD_DIR/DEBIAN $DEB_BIN $DEB_DOC $DEB_MAN
+chmod 0755 -R $BUILD_DIR/*
+
+# Copy control
+cp $SRC_DEB/control $BUILD_DIR/DEBIAN
+# Currently empty
+#cp $SRC_DEB/postinst $BUILD_DIR/DEBIAN
+#cp $SRC_DEB/postrm $BUILD_DIR/DEBIAN
+#cp $SRC_DEB/preinst $BUILD_DIR/DEBIAN
+#cp $SRC_DEB/prerm $BUILD_DIR/DEBIAN
+cp $SRC_DEB/rules $BUILD_DIR/DEBIAN
+# Copy binaries
+cp $SRC_PATH/src/zcashd $DEB_BIN
+cp $SRC_PATH/src/zcash-cli $DEB_BIN
+cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/zcash-fetch-params
+# Copy docs
+cp $SRC_PATH/doc/release-notes/release-notes-$PACKAGE_VERSION.md $DEB_DOC/changelog
+cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
+cp $SRC_DEB/copyright $DEB_DOC
+cp -r $SRC_DEB/examples $DEB_DOC
+# Copy manpages
+cp $SRC_DEB/manpages/zcashd.1 $DEB_MAN
+cp $SRC_DEB/manpages/zcash-cli.1 $DEB_MAN
+# Gzip files
+gzip --best -n $DEB_DOC/changelog
+gzip --best -n $DEB_DOC/changelog.Debian
+gzip --best -n $DEB_MAN/zcashd.1
+gzip --best -n $DEB_MAN/zcash-cli.1
 
 # Create the deb package
 fakeroot dpkg-deb --build $BUILD_DIR
