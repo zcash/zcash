2f5d62fe32f0426500a9f66ccae98c54d322cfae
Jack Grigg <jack@z.cash>
2016-10-26 20:34:33 -0700

    Remove Founders' Reward override from #1398

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -138,14 +138,6 @@ struct {
 // NOTE: These tests rely on CreateNewBlock doing its own self-validation!
 BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 {
-    // #1398 START
-    // Current test data expects to receive a reward script for the address
-    // 2N2e2FRfP9D1dRN1oRWkH7pbFM69eGNAuQ4 even though the test is run on mainnet
-    // and not testnet, and there are many founders reward addresses not just one.
-    // When test data is re-generated, we will no longer need to do this.
-    Params(CBaseChainParams::MAIN).fMinerTestModeForFoundersRewardScript = true;
-    // #1398 END
-
     CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     CBlockTemplate *pblocktemplate;
     CMutableTransaction tx,tx2;
