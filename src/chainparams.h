ce0d45422bdb4930029bb1bd33f49be659e2fd47
jl777 <jameslee777@yahoo.com>
2016-10-29 08:58:03 -0300

    test

--- src/chainparams.h
+++ src/chainparams.h
@@ -16,6 +16,7 @@
 #define GENESIS_NBITS 0x1f00ffff
 extern uint32_t ASSETCHAINS_TIMESTAMP;
 CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward);
+CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward);
 
 #include <vector>
 
