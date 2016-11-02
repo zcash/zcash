4a8e2406f5549a31dcbbe7c3cf87cbdd41483819
jl777 <jameslee777@yahoo.com>
2016-11-02 08:51:30 -0300

    test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -188,9 +188,12 @@ const char *komodo_opreturn(int32_t height,uint64_t value,uint8_t *opretbuf,int3
 {
     uint8_t rmd160[20],addrtype,shortflag,pubkey33[33]; int32_t i,j,n,len,tokomodo=0; char base[4],coinaddr[64],destaddr[64]; struct pax_transaction space; uint256 txids[64]; uint16_t vouts[64]; int64_t fiatoshis,checktoshis; const char *typestr = "unknown";
     tokomodo = (komodo_is_issuer() == 0);
-    for (i=0; i<opretlen; i++)
-        printf("%02x",opretbuf[i]);
-    printf(" opret[%c] tokomodo.%d\n",opretbuf[0],tokomodo);
+    if ( ASSETCHAINS_SYMBOL[0] != 0 )
+    {
+        for (i=0; i<opretlen; i++)
+            printf("%02x",opretbuf[i]);
+        printf(" opret[%c] tokomodo.%d\n",opretbuf[0],tokomodo);
+    }
     if ( opretbuf[0] == ((tokomodo == 0) ? 'D' : 'W') )
     {
         if ( opretlen == 34 )
