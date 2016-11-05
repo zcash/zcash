fe5c742952fc1ad7d84d2d0b2ae55c0abde1c811
Simon <simon@bitcartel.com>
2016-11-05 09:49:05 -0700

    Revert "Remove stale comment"
    This reverts commit 328d39d29c009a97c763c28f8ce94184dea287b6.
    Part of fix for #1779

--- src/wallet/asyncrpcoperation_sendmany.cpp
+++ src/wallet/asyncrpcoperation_sendmany.cpp
@@ -493,6 +493,7 @@ bool AsyncRPCOperation_sendmany::main_impl() {
                     throw JSONRPCError(RPC_WALLET_ERROR, "Could not find previous JoinSplit anchor");
                 }
                 
+                // NOTE: We assume the last commitment, output 1, is the change we want
                 for (const uint256& commitment : prevJoinSplit.commitments) {
                     tree.append(commitment);
                     previousCommitments.push_back(commitment);                   
