b7a6218ccac2eb80eb1a7a4b6c03e946bf4527f8
DeepWebCash <root@deepwebcash.com>
2016-11-09 23:42:30 +0800

    add missing method

--- src/crypto/equihash.cpp
+++ src/crypto/equihash.cpp
@@ -55,21 +55,6 @@ int Equihash<N,K>::InitialiseState(eh_HashState& base_state)
                                                          personalization);
 }
 
-template<unsigned int N, unsigned int K>
-int Equihash<N,K>::InitialiseState(eh_HashState& base_state)
-{
-    uint32_t le_N = htole32(N);
-    uint32_t le_K = htole32(K);
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
-    memcpy(personalization, "DeepWebCashPoW", 8);
-    memcpy(personalization+8,  &le_N, 4);
-    memcpy(personalization+12, &le_K, 4);
-    return crypto_generichash_blake2b_init_salt_personal(&base_state,
-                                                         NULL, 0, // No key.
-                                                         (512/N)*N/8,
-                                                         NULL,    // No salt.
-                                                         personalization);
-}
 
 void GenerateHash(const eh_HashState& base_state, eh_index g,
                   unsigned char* hash, size_t hLen)
@@ -794,6 +779,8 @@ bool Equihash<N,K>::IsValidSolution(const eh_HashState& base_state, std::vector<
 
 // Explicit instantiations for Equihash<96,3>
 template int Equihash<96,3>::InitialiseState(eh_HashState& base_state);
+template int Equihash<96,3>::InitialiseStateV0(eh_HashState& base_state);
+
 template bool Equihash<96,3>::BasicSolve(const eh_HashState& base_state,
                                          const std::function<bool(std::vector<unsigned char>)> validBlock,
                                          const std::function<bool(EhSolverCancelCheck)> cancelled);
@@ -804,6 +791,8 @@ template bool Equihash<96,3>::IsValidSolution(const eh_HashState& base_state, st
 
 // Explicit instantiations for Equihash<200,9>
 template int Equihash<200,9>::InitialiseState(eh_HashState& base_state);
+template int Equihash<200,9>::InitialiseStateV0(eh_HashState& base_state);
+
 template bool Equihash<200,9>::BasicSolve(const eh_HashState& base_state,
                                           const std::function<bool(std::vector<unsigned char>)> validBlock,
                                           const std::function<bool(EhSolverCancelCheck)> cancelled);
@@ -814,6 +803,7 @@ template bool Equihash<200,9>::IsValidSolution(const eh_HashState& base_state, s
 
 // Explicit instantiations for Equihash<96,5>
 template int Equihash<96,5>::InitialiseState(eh_HashState& base_state);
+template int Equihash<96,5>::InitialiseStateV0(eh_HashState& base_state);
 template bool Equihash<96,5>::BasicSolve(const eh_HashState& base_state,
                                          const std::function<bool(std::vector<unsigned char>)> validBlock,
                                          const std::function<bool(EhSolverCancelCheck)> cancelled);
@@ -824,6 +814,8 @@ template bool Equihash<96,5>::IsValidSolution(const eh_HashState& base_state, st
 
 // Explicit instantiations for Equihash<48,5>
 template int Equihash<48,5>::InitialiseState(eh_HashState& base_state);
+template int Equihash<48,5>::InitialiseStateV0(eh_HashState& base_state);
+
 template bool Equihash<48,5>::BasicSolve(const eh_HashState& base_state,
                                          const std::function<bool(std::vector<unsigned char>)> validBlock,
                                          const std::function<bool(EhSolverCancelCheck)> cancelled);
