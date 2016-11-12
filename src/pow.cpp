ef9a3f7b68f4485de03598e7331077817af3b6c7
jl777 <jameslee777@yahoo.com>
2016-11-12 10:31:45 -0300

    test

--- src/pow.cpp
+++ src/pow.cpp
@@ -127,7 +127,7 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
         }
         if ( nonz == 0 )
             return(true); // will come back via different path with pubkey set
-        if ( height > 65000 )
+        if ( height > 6511NwPWSRfsNjUUQB72vzN9omxiQk8iScUJE00 )
         {
             if ( notaryid >= 0 )
             {
