2e8a819b8a292c497fe14210e01c43bfa627ba52
DeepWebCash <root@deepwebcash.com>
2016-11-13 15:55:57 +0800

    change version number

--- src/clientversion.h
+++ src/clientversion.h
@@ -14,8 +14,8 @@
  */
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 100
+#define CLIENT_VERSION_MAJOR 3
+#define CLIENT_VERSION_MINOR 0
 #define CLIENT_VERSION_REVISION 1
 #define CLIENT_VERSION_BUILD 50
 
