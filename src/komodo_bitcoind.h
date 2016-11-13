cfbf96d10a86bb9f911785609737b282de69d844
jl777 <jameslee777@yahoo.com>
2016-11-13 07:42:58 -0300

    test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -475,7 +475,7 @@ int32_t komodo_is_special(int32_t height,uint8_t pubkey33[33])
                 Minerids[height-i] = komodo_minerid(height-i);
                 if ( Minerids[height - i] == -2 )
                 {
-                    fprintf(stderr,"second -2 for Minerids[%d] current.%d\n",height-i,CURRENT_HEIGHT);
+                    //fprintf(stderr,"second -2 for Minerids[%d] current.%d\n",height-i,CURRENT_HEIGHT);
                     return(0);
                 }
             }
