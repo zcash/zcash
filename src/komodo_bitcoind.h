2dbf06e85912f7cab05cb4bb9832ee14267ddc1b
jl777 <jameslee777@yahoo.com>
2016-11-18 08:51:22 -0300

    test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -431,6 +431,7 @@ int32_t komodo_block2height(CBlock *block)
 
 void komodo_block2pubkey33(uint8_t *pubkey33,CBlock& block)
 {
+    int32_t n;
 #ifdef KOMODO_ZCASH
     uint8_t *ptr = (uint8_t *)block.vtx[0].vout[0].scriptPubKey.data();
 #else
