ca3c6549819d049d6b0a78ca1af081a8d363a363
jl777 <jameslee777@yahoo.com>
2016-11-23 18:21:28 -0300

    test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -528,6 +528,10 @@ const char *komodo_opreturn(int32_t height,uint64_t value,uint8_t *opretbuf,int3
             }
         }
     }
+    else if ( opretbuf[0] == 'X' )
+    {
+        printf("got X opreturn\n");
+    }
     return(typestr);
 }
 
