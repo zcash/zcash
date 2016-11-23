cddf571e366d84dbd0b9ab14e0787808890ebf35
jl777 <jameslee777@yahoo.com>
2016-11-23 18:46:22 -0300

    test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -536,7 +536,8 @@ const char *komodo_opreturn(int32_t height,uint64_t value,uint8_t *opretbuf,int3
             for (i=0; i<n; i++)
             {
                 bitcoin_address(coinaddr,60,&rmd160s[i*20],20);
-                if ( komodo_paxmark(height,txids[i],vouts[i],height) == 0 && baseids[i] >= 0 )
+                printf("i.%d of %d: %.8f -> %s\n",i,n,dstr(values[i]),coinaddr);
+                if ( komodo_paxmark(height,txids[i],vouts[i],height) == 0 )
                     komodo_gateway_deposit(coinaddr,0,0,0,0,txids[i],vouts[i],height,0,(char *)"KMD",0);
             }
         }
