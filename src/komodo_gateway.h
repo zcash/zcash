fbd1641c2967c3cba3ce9c3683d873eb4a807192
jl777 <jameslee777@yahoo.com>
2016-11-23 12:37:36 -0300

    test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -116,10 +116,9 @@ int32_t komodo_rwapproval(int32_t rwflag,uint8_t *opretbuf,struct pax_transactio
     else
     {
         for (i=0; i<32; i++)
-        {
             ((uint8_t *)&pax->txid)[31-i] = opretbuf[len++];
-            printf("%02x",((uint8_t *)&pax->txid)[31-i]);
-        }
+        for (i=0; i<32; i++)
+            printf("%02x",((uint8_t *)&pax->txid)[i]);
         pax->vout = opretbuf[len++];
         pax->vout += ((uint32_t)opretbuf[len++] << 8);
         printf(" txid v.%d\n",pax->vout);
