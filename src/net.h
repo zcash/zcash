e286250ce49309bfa931b622fabfc37100246266
Matt Corallo <git@bluematt.me>
2016-11-25 18:03:12 -0800

    Make fDisconnect an std::atomic

--- src/net.h
+++ src/net.h
@@ -287,7 +287,7 @@ public:
     bool fInbound;
     bool fNetworkNode;
     bool fSuccessfullyConnected;
-    bool fDisconnect;
+    std::atomic_bool fDisconnect;
     // We use fRelayTxes for two purposes -
     // a) it allows us to not relay tx invs before receiving the peer's version message
     // b) the peer may tell us in its version message that we should not relay tx invs
