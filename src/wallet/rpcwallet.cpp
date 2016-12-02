0b6c5cb4f811bd7b159159b52259eb7a26e247fe
jl777 <jameslee777@yahoo.com>
2016-12-02 17:36:44 -0300

    test

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -510,8 +510,8 @@ Value paxdeposit(const Array& params, bool fHelp)
     if (!destaddress.IsValid())
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid dest Bitcoin address");
     for (i=0; i<33; i++)
-        printf("%02x",pubkey37[i]);
-    printf(" ht.%d srcaddr.(%s) %s fiatoshis.%lld -> dest.(%s) komodoshis.%llu seed.%llx\n",height,(char *)params[0].get_str().c_str(),(char *)base.c_str(),(long long)fiatoshis,destaddr,(long long)komodoshis,(long long)seed);
+        fprintf(stderr,"%02x",pubkey37[i]);
+    fprintf(stderr," ht.%d srcaddr.(%s) %s fiatoshis.%lld -> dest.(%s) komodoshis.%llu seed.%llx\n",height,(char *)params[0].get_str().c_str(),(char *)base.c_str(),(long long)fiatoshis,destaddr,(long long)komodoshis,(long long)seed);
     EnsureWalletIsUnlocked();
     CWalletTx wtx;
     uint8_t opretbuf[64]; int32_t opretlen; uint64_t fee = komodoshis / 1000;
