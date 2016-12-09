250195b07f9e451d9c40654397a54f198fbaaf7e
jl777 <jameslee777@yahoo.com>
2016-12-09 12:33:57 +0200

    tes

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -270,7 +270,10 @@ uint64_t komodo_paxtotal()
                 else if ( pax->type == 'W' )
                 {
                     //if ( strcmp(str,ASSETCHAINS_SYMBOL) == 0 )
-                        printf("got WITHDRAW.%s kmd.%d ht.%d %.8f -> %.8f\n",str,pax->height,pax->otherheight,dstr(pax->fiatoshis),dstr(pax->komodoshis));
+                    bitcoin_address(coinaddr,addrtype,rmd160,20);
+                    checktoshis = komodo_paxprice(&seed,pax->height,pax->source,(char *)"KMD",(uint64_t)pax->fiatoshis);
+
+                        printf("got WITHDRAW.%s kmd.%d ht.%d %.8f -> %.8f/%.8f\n",pax->source,pax->height,pax->otherheight,dstr(pax->fiatoshis),dstr(pax->komodoshis),dstr(checktoshs));
                 }
             }
         }
