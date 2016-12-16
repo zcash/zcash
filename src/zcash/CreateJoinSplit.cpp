d0ce704abefb2e6d524401ca08e85882195f6e18
Jack Grigg <jack@z.cash>
2016-12-17 02:39:35 +1300

    Add a tool for profiling the creation of JoinSplits

---
+++ src/zcash/CreateJoinSplit.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2016 The Zcash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "../util.h"
+#include "primitives/transaction.h"
+#include "zcash/JoinSplit.hpp"
+
+using namespace libzcash;
+
+int main(int argc, char **argv)
+{
+    auto p = ZCJoinSplit::Unopened();
+    p->loadVerifyingKey((ZC_GetParamsDir() / "sprout-verifying.key").string());
+    p->setProvingKeyPath((ZC_GetParamsDir() / "sprout-proving.key").string());
+    p->loadProvingKey();
+
+    // construct a proof.
+
+    for (int i = 0; i < 5; i++) {
+        uint256 anchor = ZCIncrementalMerkleTree().root();
+        uint256 pubKeyHash;
+
+        JSDescription jsdesc(*p,
+                             pubKeyHash,
+                             anchor,
+                             {JSInput(), JSInput()},
+                             {JSOutput(), JSOutput()},
+                             0,
+                             0);
+    }
+}
