c2c03b7a90f8048bb3b353cbd6009e6334f404b4
jl777 <jameslee777@yahoo.com>
2016-12-22 14:25:15 +0200

    test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -772,7 +772,7 @@ const char *komodo_opreturn(int32_t height,uint64_t value,uint8_t *opretbuf,int3
                     if ( strcmp(base,ASSETCHAINS_SYMBOL) == 0 )
                         printf("########### %p withdrawn %s += %.8f\n",basesp,base,dstr(value));
                 }
-                if ( strcmp(base,"RUB") == 0 && pax->approved == 0 )
+                if ( strcmp(base,"RUB") == 0 && (pax == 0 || pax->approved == 0) )
                     printf("notarize %s %.8f -> %.8f kmd.%d other.%d\n",ASSETCHAINS_SYMBOL,dstr(value),dstr(komodoshis),kmdheight,height);
             }
             komodo_gateway_deposit(coinaddr,0,(char *)"KMD",value,rmd160,txid,vout,'W',kmdheight,height,source,0);
