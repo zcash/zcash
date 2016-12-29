9c511c5cabced534d6786fc74c72fcc097872819
Joel Barker <joel@talkingcucumber.com>
2016-12-29 14:38:15 +0800

    conditional packages

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -8,9 +8,22 @@ $(package)_git_commit=9be18569b8abcda1245c3912877075259599c0f1
 
 $(package)_dependencies=libgmp libsodium
 
+ifeq ($(build_os),darwin)
 define $(package)_build_cmds
 CC=gcc-5 CXX=g++-5 CXXFLAGS="-arch x86_64 -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=0 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT
 endef
+else
+define $(package)_build_cmds
+  CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
+endef
+
+
+
+
+endif
+
+
+
 
 define $(package)_stage_cmds
   $(MAKE) install STATIC=1 DEPINST=$(host_prefix) PREFIX=$($(package)_staging_dir)$(host_prefix) CURVE=ALT_BN128 NO_SUPERCOP=1
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,12 @@
 zcash_packages := libsnark libgmp libsodium
+
+ifeq ($(build_os),darwin)
 packages := boost openssl $(zcash_packages)
+else
+packages := boost openssl $(zcash_packages) googletest googlemock
+endif
+
+
 native_packages := native_ccache
 
 wallet_packages=bdb
