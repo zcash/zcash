16fb1e08667844582e4c59a6b922fa4f43ebca21
Cory Fields <cory-nospam-@coryfields.com>
2016-12-31 02:05:13 -0500

    net: wait until the node is destroyed to delete its recv buffer
    when vRecvMsg becomes a private buffer, it won't make sense to allow other
    threads to mess with it anymore.
    
    (cherry picked from commit 60425870d78cf2bef1fce926ad53f51166c6a3f0)

--- src/net.cpp
+++ src/net.cpp
@@ -417,11 +417,6 @@ void CNode::CloseSocketDisconnect()
         LogPrint("net", "disconnecting peer=%d\n", id);
         CloseSocket(hSocket);
     }
-
-    // in case this fails, we'll empty the recv buffer when the CNode is deleted
-    TRY_LOCK(cs_vRecvMsg, lockRecv);
-    if (lockRecv)
-        vRecvMsg.clear();
 }
 
 void CNode::PushVersion()
