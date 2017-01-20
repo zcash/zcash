b9ecd81fa992afa61e09eaad2849cac008ee8f3d
jl777 <jameslee777@yahoo.com>
2017-01-20 21:13:07 +0200

    test

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -561,7 +561,7 @@ Value paxwithdraw(const Array& params, bool fHelp)
 Value kvupdate(const Array& params, bool fHelp)
 {
     CWalletTx wtx; Object ret; uint8_t keyvalue[IGUANA_MAXSCRIPTSIZE],opretbuf[IGUANA_MAXSCRIPTSIZE]; int32_t opretlen,i;
-    uint16_t keylen,valuesize; uint8_t *key,*value=0; struct komodo_kv *ptr; uint64_t fee;
+    uint16_t keylen,valuesize=0; uint8_t *key,*value=0; struct komodo_kv *ptr; uint64_t fee;
     if (fHelp || params.size() != 2 )
         throw runtime_error("kvupdate key value");
     if (!EnsureWalletIsAvailable(fHelp))
@@ -573,10 +573,13 @@ Value kvupdate(const Array& params, bool fHelp)
         ret.push_back(Pair("coin",(char *)(ASSETCHAINS_SYMBOL[0] == 0 ? "KMD" : ASSETCHAINS_SYMBOL)));
         ret.push_back(Pair("height", (int64_t)chainActive.Tip()->nHeight));
         ret.push_back(Pair("key",params[0].get_str()));
+        ret.push_back(Pair("keylen",keylen));
         if ( params.size() == 2 && params[1].get_str().c_str() != 0 )
         {
-            ret.push_back(Pair("value",params[1].get_str()));
             value = (uint8_t *)params[1].get_str().c_str();
+            valuesize = (int32_t)strlen(params[1].get_str().c_str();
+            ret.push_back(Pair("value",params[1].get_str()));
+            ret.push_back(Pair("valuesize",valuesize));
         }
         iguana_rwnum(1,&keyvalue[0],sizeof(keylen),&keylen);
         iguana_rwnum(1,&keyvalue[2],sizeof(valuesize),&valuesize);
@@ -594,8 +597,8 @@ Value kvupdate(const Array& params, bool fHelp)
         if (!destaddress.IsValid())
             throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid dest Bitcoin address");
         SendMoney(destaddress.Get(),10000,false,wtx,opretbuf,opretlen,fee);
-        return wtx.GetHash().GetHex();
-    } else ret.push_back(Pair("error",(char *)"null key"));
+        ret.push_back(Pair("txid",wtx.GetHash()));
+ } else ret.push_back(Pair("error",(char *)"null key"));
     return ret;
 }
 
