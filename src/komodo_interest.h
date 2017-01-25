c8c894b6979c19435b99af9581be1541927fc6d7
jl777 <jameslee777@yahoo.com>
2017-01-25 09:43:25 +0200

    test

--- src/komodo_interest.h
+++ src/komodo_interest.h
@@ -90,10 +90,11 @@ uint64_t komodo_interest(int32_t txheight,uint64_t nValue,uint32_t nLockTime,uin
                         (txheight == 155492 && nValue == 3983427592291LL) ||
                         (txheight == 155613 && nValue == 9997409999999797LL) ||
                         (txheight == 157927 && nValue == 9997410667451072LL) ||
-                        (txheight == 155613 && nValue == 2590000000000LL)
+                        (txheight == 155613 && nValue == 2590000000000LL) ||
                         (txheight == 155949 && nValue == 4000000000000LL) )
                         exception = 1;
-                    printf(">>>>>>>>>>>> exception.%d txheight.%d %.8f locktime %u vs tiptime %u <<<<<<<<<\n",exception,txheight,(double)nValue/COIN,nLockTime,tiptime);
+                    if ( exception == 0 )
+                        printf(">>>>>>>>>>>> exception.%d txheight.%d %.8f locktime %u vs tiptime %u <<<<<<<<<\n",exception,txheight,(double)nValue/COIN,nLockTime,tiptime);
                 }
                 if ( exception == 0 )
                 {
