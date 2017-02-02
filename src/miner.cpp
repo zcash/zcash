965f0f7e972d466d516b562554985ed0c2d4413f
jl777 <jameslee777@yahoo.com>
2017-02-02 17:23:45 +0200

    test

--- src/miner.cpp
+++ src/miner.cpp
@@ -626,14 +626,17 @@ void static BitcoinMiner(CWallet *pwallet)
                     if ( nonzpkeys > 0 )
                     {
                         for (j=0; j<65; j++)
+                            printf("%d ",mids[j]);
+                        printf(" <- minerids from ht.%d\n",pindexPrev->nHeight);
+                        for (j=0; j<65; j++)
                             if ( mids[j] == notaryid )
                                 break;
                         if ( j == 65 )
                         {
                             hashTarget = arith_uint256().SetCompact(KOMODO_MINDIFF_NBITS);
                             fprintf(stderr,"I am the chosen one for %s ht.%d\n",ASSETCHAINS_SYMBOL,pindexPrev->nHeight+1);
-                        }
-                    }
+                        } else printf("duplicate at j.%d\n",j);
+                    } else fprintf(stderr,"no nonz pubkeys\n");
                 } else Mining_start = 0;
             } else Mining_start = 0;
             while (true)
