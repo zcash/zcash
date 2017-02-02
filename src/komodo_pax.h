cf4609dbb198a81ed6ac7555bc0b64afb370aef3
jl777 <jameslee777@yahoo.com>
2017-02-02 14:58:19 +0200

    test

--- src/komodo_pax.h
+++ src/komodo_pax.h
@@ -515,10 +515,10 @@ uint64_t komodo_paxpriceB(uint64_t *seedp,int32_t height,char *base,char *rel,ui
             zeroes++;
         else nonz++;
     }
+    printf("kmdbtc %llu btcusd %llu ",(long long)kmdbtc,(long long)btcusd);
+    printf("komodo_paxprice nonz.%d of numvotes.%d seed.%llu\n",nonz,numvotes,(long long)*seedp);
     if ( nonz <= (numvotes >> 1) )
     {
-        printf("kmdbtc %llu btcusd %llu\n",(long long)kmdbtc,(long long)btcusd);
-        printf("komodo_paxprice nonz.%d of numvotes.%d seed.%llu\n",nonz,numvotes,(long long)*seedp);
         return(0);
     }
     return(komodo_paxcorrelation(votes,numvotes,*seedp) * basevolume / 100000);
