bca493a831305cf58bcd18235ca8f210de907783
jl777 <jameslee777@yahoo.com>
2017-02-02 16:05:44 +0200

    test

--- src/komodo_notary.h
+++ src/komodo_notary.h
@@ -128,12 +128,12 @@ int32_t komodo_electednotary(uint8_t *pubkey33,int32_t height)
     for (i=0; i<33; i++)
         sprintf(&pubkeystr[i*2],"%02x",pubkey33[i]);
     pubkeystr[66] = 0;
-    printf("%s vs\n",pubkeystr);
+    //printf("%s vs\n",pubkeystr);
     for (i=0; i<sizeof(Notaries_elected)/sizeof(*Notaries_elected); i++)
     {
         if ( strcmp(pubkeystr,(char *)Notaries_elected[i][1]) == 0 )
             return(i);
-        printf("%s\n",(char *)Notaries_elected[i][1]);
+        //printf("%s\n",(char *)Notaries_elected[i][1]);
     }
     return(-1);
 }
