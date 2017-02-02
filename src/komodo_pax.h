df96c2949d9618930aff99f144a93663b07259b3
jl777 <jameslee777@yahoo.com>
2017-02-02 15:17:54 +0200

    test

--- src/komodo_pax.h
+++ src/komodo_pax.h
@@ -526,8 +526,11 @@ uint64_t komodo_paxpriceB(uint64_t seed,int32_t height,char *base,char *rel,uint
 uint64_t komodo_paxprice(uint64_t *seedp,int32_t height,char *base,char *rel,uint64_t basevolume)
 {
     int32_t i,nonz=0; int64_t diff; uint64_t price,seed,sum = 0;
-    if ( chainActive.Tip() != 0 && height > chainActive.Tip()->nHeight )
+    if ( ASSETCHAINS_SYMBOL[0] == 0 && chainActive.Tip() != 0 && height > chainActive.Tip()->nHeight )
+    {
+        printf("height.%d vs tip.%d\n",height,chainActive.Tip()->nHeight);
         return(0);
+    }
     *seedp = komodo_seed(height);
     portable_mutex_lock(&komodo_mutex);
     for (i=0; i<17; i++)
@@ -597,7 +600,7 @@ void komodo_paxpricefeed(int32_t height,uint8_t *pricefeed,int32_t opretlen)
 uint64_t PAX_fiatdest(uint64_t *seedp,int32_t tokomodo,char *destaddr,uint8_t pubkey33[33],char *coinaddr,int32_t height,char *origbase,int64_t fiatoshis)
 {
     uint8_t shortflag = 0; char base[4]; int32_t i,baseid; uint8_t addrtype,rmd160[20]; int64_t komodoshis = 0;
-    *seedp = 0;
+    *seedp = komodo_seed(height);
     if ( (baseid= komodo_baseid(origbase)) < 0 || baseid == MAX_CURRENCIES )
     {
         if ( origbase[0] != 0 )
