f42cde3c3022148686cc7176511a4121b055e1b8
Jack Grigg <jack@z.cash>
2017-02-23 12:42:28 +0000

    Remove reference to -reindex-chainstate
    Revert this when bitcoin/bitcoin#7917 is merged.

--- src/init.cpp
+++ src/init.cpp
@@ -1343,7 +1343,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
             if (!fReset) {
                 bool fRet = uiInterface.ThreadSafeQuestion(
                     strLoadError + ".\n\n" + _("Do you want to rebuild the block database now?"),
-                    strLoadError + ".\nPlease restart with -reindex or -reindex-chainstate to recover.",
+                    strLoadError + ".\nPlease restart with -reindex to recover.",
                     "", CClientUIInterface::MSG_ERROR | CClientUIInterface::BTN_ABORT);
                 if (fRet) {
                     fReindex = true;
