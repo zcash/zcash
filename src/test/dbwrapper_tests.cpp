64b4790789f5519e397210095a1139a63c6a539a
Wladimir J. van der Laan <laanwj@gmail.com>
2017-02-26 21:08:26 +0100

    test: Replace remaining sprintf with snprintf
    Use of `sprintf` is seen as a red flag as many of its uses are insecure.
    OpenBSD warns about it while compiling, and some modern platforms, e.g.
    [cloudlibc from cloudabi](https://github.com/NuxiNL/cloudlibc) don't
    even provide it anymore.
    
    Although our uses of these functions are secure, it can't hurt to
    replace them anyway. There are only 3 occurences left, all in the
    tests.

--- src/test/dbwrapper_tests.cpp
+++ src/test/dbwrapper_tests.cpp
@@ -194,7 +194,7 @@ BOOST_AUTO_TEST_CASE(iterator_string_ordering)
     CDBWrapper dbw(ph, (1 << 20), true, false);
     for (int x=0x00; x<10; ++x) {
         for (int y = 0; y < 10; y++) {
-            sprintf(buf, "%d", x);
+            snprintf(buf, sizeof(buf), "%d", x);
             StringContentsSerializer key(buf);
             for (int z = 0; z < y; z++)
                 key += key;
@@ -210,12 +210,12 @@ BOOST_AUTO_TEST_CASE(iterator_string_ordering)
             seek_start = 0;
         else
             seek_start = 5;
-        sprintf(buf, "%d", seek_start);
+        snprintf(buf, sizeof(buf), "%d", seek_start);
         StringContentsSerializer seek_key(buf);
         it->Seek(seek_key);
         for (int x=seek_start; x<10; ++x) {
             for (int y = 0; y < 10; y++) {
-                sprintf(buf, "%d", x);
+                snprintf(buf, sizeof(buf), "%d", x);
                 string exp_key(buf);
                 for (int z = 0; z < y; z++)
                     exp_key += exp_key;
