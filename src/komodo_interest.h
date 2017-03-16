295c54ebb8e3e14ac08404a20d59fd101a535ae1
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-16 19:01:45 +0200

    Test

--- src/komodo_interest.h
+++ src/komodo_interest.h
@@ -115,7 +115,7 @@ uint64_t komodo_interest(int32_t txheight,uint64_t nValue,uint32_t nLockTime,uin
                 numerator = (nValue * KOMODO_INTEREST);
                 interest = (numerator / denominator) / COIN;
             }
-            //fprintf(stderr,"komodo_interest %lld %.8f nLockTime.%u tiptime.%u minutes.%d interest %lld %.8f (%llu / %llu)\n",(long long)nValue,(double)nValue/COIN,nLockTime,tiptime,minutes,(long long)interest,(double)interest/COIN,(long long)numerator,(long long)denominator);
+            fprintf(stderr,"komodo_interest %lld %.8f nLockTime.%u tiptime.%u minutes.%d interest %lld %.8f (%llu / %llu)\n",(long long)nValue,(double)nValue/COIN,nLockTime,tiptime,minutes,(long long)interest,(double)interest/COIN,(long long)numerator,(long long)denominator);
         }
     }
     return(interest);
