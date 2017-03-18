f8413078852633764e6545af38069ee7210fb643
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-18 08:35:35 +0200

    Test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -737,7 +737,7 @@ int32_t komodo_check_deposit(int32_t height,const CBlock& block) // verify above
                         printf("checkdeposit.[%s]: skip %s %.8f when avail %.8f deposited %.8f, issued %.8f withdrawn %.8f approved %.8f redeemed %.8f\n",ASSETCHAINS_SYMBOL,symbol,dstr(pax->fiatoshis),dstr(available),dstr(deposited),dstr(issued),dstr(withdrawn),dstr(approved),dstr(redeemed));
                         continue;
                     }
-                    if ( ((opcode == 'I' && (pax->fiatoshis == 0 || pax->fiatoshis == block.vtx[0].vout[i].nValue)) || (opcode == 'X' && (pax->komodoshis == 0 || pax->komodoshis == block.vtx[0].vout[i].nValue))) )
+                    if ( ((opcode == 'I' && (pax->fiatoshis == block.vtx[0].vout[i].nValue) || (opcode == 'X' && pax->komodoshis == block.vtx[0].vout[i].nValue)) )
                     {
                         if ( pax->marked != 0 && height >= 80820 )
                         {
