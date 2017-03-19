aeebb21eed8e165a777083c0192fcf4015ded5c8
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-19 19:10:37 +0200

    Test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -734,7 +734,8 @@ int32_t komodo_check_deposit(int32_t height,const CBlock& block) // verify above
         }
         if ( KOMODO_PASSPORT_INITDONE == 0 ) // komodo_isrealtime(&ht) == 0 ||  init time already in DB
             return(0);
-        if ( baseid == USD && (height == 1339 || height == 1344) )
+        // 7b6e26d9ffbb7cc1b71185c38906b71632324d1c9df3a2596d09e1353a714c27
+        if ( baseid == USD && (height == 1338 || height == 1339 || height == 1344) )
             return(0);
     }
     if ( script[offset] == opcode && opretlen < block.vtx[0].vout[n-1].scriptPubKey.size() )
