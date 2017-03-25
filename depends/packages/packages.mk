f493d3e044189db0c77793c614055c9c9b1715fc
Simon <simon@bitcartel.com>
2017-03-25 09:03:22 -0700

    Add --disable-proton flag to build.sh.  Proton has build/linker issues with gcc 4.9.2 and requires gcc 5.x.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,6 +1,7 @@
 rust_packages := rust librustzcash
+proton_packages := proton
 zcash_packages := libsnark libgmp libsodium
-packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock proton
+packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock
 native_packages := native_ccache
 
 wallet_packages=bdb
