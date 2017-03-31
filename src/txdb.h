5befbed4bc4f074a799ec413f595eb21f6cf06c2
Simon <simon@bitcartel.com>
2017-03-31 10:25:39 -0400

    Fixes #2793. Backport commit f33afd3 to increase dbcache default.

--- src/txdb.h
+++ src/txdb.h
@@ -20,8 +20,8 @@ struct CDiskTxPos;
 class uint256;
 
 //! -dbcache default (MiB)
-static const int64_t nDefaultDbCache = 100;
-//! max. -dbcache in (MiB)
+static const int64_t nDefaultDbCache = 450;
+//! max. -dbcache (MiB)
 static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 16384 : 1024;
 //! min. -dbcache in (MiB)
 static const int64_t nMinDbCache = 4;
