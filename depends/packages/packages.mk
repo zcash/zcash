26a8f68ea8a021c1fa096fd4528b6b98e89a2e62
Sean Bowe <ewillbefull@gmail.com>
2017-04-10 14:25:04 -0600

    Remove libsnark from depends system and integrate it into build system.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,6 +1,6 @@
 rust_packages := rust librustzcash
 proton_packages := proton
-zcash_packages := libsnark libgmp libsodium
+zcash_packages := libgmp libsodium
 packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock
 native_packages := native_ccache
 
