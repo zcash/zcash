235d7a248b94e9dbd7d314e07460e074646c1d91
cronicc <cronicc@users.noreply.github.com>
2017-04-17 18:24:09 +0200

    Update build scripts #69 fixes #70 (#72)
    * #69 Update build-win.sh
    
    * #69 update build-mac.sh
    
    * update for rust exclusion

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -37,14 +37,30 @@ then
     shift
 fi
 
+# If --disable-mining is the next argument, disable mining code:
+MINING_ARG=''
+if [ "x${1:-}" = 'x--disable-mining' ]
+then
+    MINING_ARG='--enable-mining=no'
+    shift
+fi
+
+# If --disable-rust is the next argument, disable Rust code:
+RUST_ARG=''
+if [ "x${1:-}" = 'x--disable-rust' ]
+then
+    RUST_ARG='--enable-rust=no'
+    shift
+fi
+
 TRIPLET=`./depends/config.guess`
 PREFIX="$(pwd)/depends/$TRIPLET"
 
-make "$@" -C ./depends/ V=1 NO_QT=1
+NO_RUST="$RUST_ARG" make "$@" -C ./depends/ V=1 NO_QT=1
 
 ./autogen.sh
 CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
 CXXFLAGS='-arch x86_64 -I/usr/local/Cellar/gcc5/5.4.0/include/c++/5.4.0 -I$PREFIX/include -fwrapv -fno-strict-aliasing -Werror -g -Wl,-undefined -Wl,dynamic_lookup' \
-./configure --prefix="${PREFIX}" --with-gui=no "$HARDENING_ARG" "$LCOV_ARG"
+./configure --prefix="${PREFIX}" --with-gui=no "$HARDENING_ARG" "$LCOV_ARG" "$RUST_ARG" "$MINING_ARG"
 
 make "$@" V=1 NO_GTEST=0 STATIC=1
--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -6,11 +6,42 @@ PREFIX="$(pwd)/depends/$HOST"
 
 set -eu -o pipefail
 
+# If --enable-lcov is the first argument, enable lcov coverage support:
+LCOV_ARG=''
+HARDENING_ARG='--enable-hardening'
+TEST_ARG=''
+if [ "x${1:-}" = 'x--enable-lcov' ]
+then
+    LCOV_ARG='--enable-lcov'
+    HARDENING_ARG='--disable-hardening'
+    shift
+elif [ "x${1:-}" = 'x--disable-tests' ]
+then
+    TEST_ARG='--enable-tests=no'
+    shift
+fi
+
+# If --disable-mining is the next argument, disable mining code:
+MINING_ARG=''
+if [ "x${1:-}" = 'x--disable-mining' ]
+then
+    MINING_ARG='--enable-mining=no'
+    shift
+fi
+
+# If --disable-rust is the next argument, disable Rust code:
+RUST_ARG=''
+if [ "x${1:-}" = 'x--disable-rust' ]
+then
+    RUST_ARG='--enable-rust=no'
+    shift
+fi
+
 set -x
 cd "$(dirname "$(readlink -f "$0")")/.."
 
 cd depends/ && make HOST=$HOST V=1 NO_QT=1 && cd ../
 ./autogen.sh
-CXXFLAGS="-DPTW32_STATIC_LIB -DCURVE_ALT_BN128 -fopenmp -pthread" CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix="${PREFIX}" --with-gui=no --host="${HOST}" --enable-static --disable-shared
+CXXFLAGS="-DPTW32_STATIC_LIB -DCURVE_ALT_BN128 -fopenmp -pthread" CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix="${PREFIX}" --with-gui=no --host="${HOST}" --enable-static --disable-shared "$RUST_ARG" "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG"
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 CC="${CC}" CXX="${CXX}" make V=1
