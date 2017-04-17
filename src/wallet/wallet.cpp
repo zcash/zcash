e9e7e410efcaf7f864b6a3d3f2343ed3fbb44bca
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-04-17 17:33:52 +0300

    Test

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -635,6 +635,7 @@ void CWallet::ClearNoteWitnessCache()
         }
     }
     nWitnessCacheSize = 0;
+    fprintf(stderr,"Clear witness cache\n");
 }
 
 void CWallet::IncrementNoteWitnesses(const CBlockIndex* pindex,
@@ -669,6 +670,7 @@ void CWallet::IncrementNoteWitnesses(const CBlockIndex* pindex,
             }
         }
         if (nWitnessCacheSize < WITNESS_CACHE_SIZE) {
+            fprintf(stderr,"increment nWitnesscache -> %d\n",(int32_t)nWitnessCacheSize);
             nWitnessCacheSize += 1;
         }
 
@@ -782,6 +784,7 @@ void CWallet::DecrementNoteWitnesses(const CBlockIndex* pindex)
                 }
             }
         }
+        fprintf(stderr,"decrement witness cache -> %d\n",(int32_t)nWitnessCacheSize);
         nWitnessCacheSize -= 1;
         for (std::pair<const uint256, CWalletTx>& wtxItem : mapWallet) {
             for (mapNoteData_t::value_type& item : wtxItem.second.mapNoteData) {
