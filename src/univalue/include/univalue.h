375c84114be258595c07959015b7ff8453b4b739
kozyilmaz <kazim@monolytic.com>
2017-04-20 09:27:28 +0300

    Revert "[macOS] fixes “push_back” and “Pair” ambiguities"
    This reverts commit 0d19ca48e7f0b9a761f16a51ef9ac6e0bcab509f.

--- src/univalue/include/univalue.h
+++ src/univalue/include/univalue.h
@@ -25,9 +25,6 @@ public:
         typ = initialType;
         val = initialStr;
     }
-   UniValue(size_t val_) {
-        setInt(val_);
-    }
     UniValue(uint64_t val_) {
         setInt(val_);
     }
@@ -57,7 +54,6 @@ public:
     bool setNull();
     bool setBool(bool val);
     bool setNumStr(const std::string& val);
-    bool setInt(size_t val_);
     bool setInt(uint64_t val);
     bool setInt(int64_t val);
     bool setInt(int val_) { return setInt((int64_t)val_); }
@@ -95,10 +91,6 @@ public:
     bool push_back(const char *val_) {
         std::string s(val_);
         return push_back(s);
-    }
-    bool push_back(size_t val_) {
-        UniValue tmpVal(val_);
-        return push_back(tmpVal);
     }
     bool push_back(uint64_t val_) {
         UniValue tmpVal(val_);
@@ -195,13 +187,6 @@ static inline std::pair<std::string,UniValue> Pair(const char *cKey, std::string
 {
     std::string key(cKey);
     UniValue uVal(strVal);
-    return std::make_pair(key, uVal);
-}
-
-static inline std::pair<std::string,UniValue> Pair(const char *cKey, size_t sizeVal)
-{
-    std::string key(cKey);
-    UniValue uVal(sizeVal);
     return std::make_pair(key, uVal);
 }
 
--- src/univalue/lib/univalue.cpp
+++ src/univalue/lib/univalue.cpp
@@ -119,15 +119,6 @@ bool UniValue::setNumStr(const string& val_)
     return true;
 }
 
-bool UniValue::setInt(size_t val_)
-{
-    ostringstream oss;
-
-    oss << val_;
-
-    return setNumStr(oss.str());
-}
-
 bool UniValue::setInt(uint64_t val_)
 {
     ostringstream oss;
