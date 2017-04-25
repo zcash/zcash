843071611596bf78bf173c59a979eb9a570e6da3
joshuayabut <yabut.joshua@gmail.com>
2017-04-25 01:20:01 -0400

    Add block checkpoint at 96577

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,11 +126,12 @@ public:
         checkpointData = (Checkpoints::CCheckpointData) {
             boost::assign::map_list_of
             ( 0, consensus.hashGenesisBlock)
-            ( 30000, uint256S("0x000000005c2ad200c3c7c8e627f67b306659efca1268c9bb014335fdadc0c392")),
-            1482914970,     // * UNIX timestamp of last checkpoint block
-            82372,          // * total number of transactions between genesis and last checkpoint
+            ( 30000, uint256S("0x000000005c2ad200c3c7c8e627f67b306659efca1268c9bb014335fdadc0c392"))
+            ( 96577, uint256S("0x0000000177751545bd1af3ccf276ec2920d258453ab01f3d2f8f7fcc5f3a37b8")),
+            1493090861,     // * UNIX timestamp of last checkpoint block
+            241684,         // * total number of transactions between genesis and last checkpoint
                             //   (the tx=... number in the SetBestChain debug.log lines)
-            1581            // * estimated number of transactions per day after checkpoint
+            1441            // * estimated number of transactions per day after checkpoint
                             //   total number of tx / (checkpoint block height / (24 * 24))
         };
 
