3e5136cee92cfb797abca9495bcaee02f9bb80f4
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-04-26 22:52:10 +0300

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1565,7 +1565,7 @@ bool IsInitialBlockDownload()
     if ( ASSETCHAINS_SYMBOL[0] == 0 )
         state = ((chainActive.Height() < ptr->nHeight - 24*60) ||
                     ptr->GetBlockTime() < (GetTime() - chainParams.MaxTipAge()));
-    else state = (chainActive.Height() < ptr->nHeight - 3);
+    else state = (chainActive.Height() < ptr->nHeight - 99);
     //fprintf(stderr,"state.%d  ht.%d vs %d, t.%u %u\n",state,(int32_t)chainActive.Height(),(uint32_t)ptr->nHeight,(int32_t)ptr->GetBlockTime(),(uint32_t)(GetTime() - chainParams.MaxTipAge()));
     if (!state)
     {
