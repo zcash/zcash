zcash/zcash

commit 3290567bbd54e01cb6fe6b1d04c659abca983af2

torcontrol: Check for reading errors in ReadBinaryFile
This ensures that ReadBinaryFile never returns exactly TOR_COOKIE_SIZE bytes if
the file was larger than that.

src/torcontrol.cpp
@@ -324,6 +324,10 @@ static std::pair<bool,std::string> ReadBinaryFile(const std::string &filename, s
     char buffer[128];
     size_t n;
     while ((n=fread(buffer, 1, sizeof(buffer), f)) > 0) {
+        // Check for reading errors so we don't return any data if we couldn't
+        // read the entire file (or up to maxsize)
+        if (ferror(f))
+            return std::make_pair(false,"");
         retval.append(buffer, buffer+n);
         if (retval.size() > maxsize)
             break;

commit 12407174315a14ecaaeb99c8fe912775c18679d9

Net: Fix resource leak in ReadBinaryFile(...)
Introduced in 3290567bbd54e01cb6fe6b1d04c659abca983af2 via PR #2177.

src/torcontrol.cpp
@@ -376,8 +376,10 @@ static std::pair<bool,std::string> ReadBinaryFile(const std::string &filename, s
     while ((n=fread(buffer, 1, sizeof(buffer), f)) > 0) {
         // Check for reading errors so we don't return any data if we couldn't
         // read the entire file (or up to maxsize)
-        if (ferror(f))
+        if (ferror(f)) {
+            fclose(f);
             return std::make_pair(false,"");
+        }
         retval.append(buffer, buffer+n);
         if (retval.size() > maxsize)
             break;