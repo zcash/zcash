d753a0c7901727b51ab9b3fa7c84d147275662b2
joshuayabut <yabut.joshua@gmail.com>
2017-07-09 15:54:05 -0400

    Revert "Merge pull request #96 from dkaidalov/Fix_replay_protection"
    This reverts commit a1739f1a411e549285773d1ee1858e656bc85563, reversing
    changes made to 7348fcce47723062c6e41f8a9fa2b1bc64803a42.

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -380,8 +380,7 @@ bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, un
                 }
                 case OP_NOP5:
                 {
-                    // If it is OP_CHECKBLOCKATHEIGHT then there are should be 3 args in stack: 2 params and 1 ret value from the prev opcode
-                    if (stack.size() == 3) {
+                    if (stack.size() == 2) {
                         popstack(stack);
                         popstack(stack);
                     }
