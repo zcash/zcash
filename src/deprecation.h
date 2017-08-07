d6252a9473c0ffea402c352c6a54fddf39974140
Jason Davies <jason@jasondavies.com>
2017-08-07 20:15:57 +0100

    Fix deprecation policy comment.
    Thanks to @daira for spotting this and suggesting the correct policy.

--- src/deprecation.h
+++ src/deprecation.h
@@ -5,7 +5,9 @@
 #ifndef ZCASH_DEPRECATION_H
 #define ZCASH_DEPRECATION_H
 
-// Deprecation policy is 4th third-Tuesday after a release
+// Deprecation policy:
+// * Shut down 18 weeks' worth of blocks after the estimated release block height.
+// * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
 static const int APPROX_RELEASE_HEIGHT = 167200;
 static const int WEEKS_UNTIL_DEPRECATION = 18;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 24);
