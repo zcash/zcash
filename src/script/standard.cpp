3c8c8009db282e4d86719a2ea681ed1c2fb5cfab
Jim Posen <jimpo@coinbase.com>
2017-08-15 17:55:26 -0700

    Comments: More comments on functions/globals in standard.h.
    Zcash: Excludes comments for variables and functions we don't have:
    
    - GetScriptForRawPubKey (bitcoin/bitcoin#6415)
    - GetScriptForWitness (bitcoin/bitcoin#8149)

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -35,10 +35,7 @@ const char* GetTxnOutputType(txnouttype t)
     return NULL;
 }
 
-/**
- * Return public keys or hashes from scriptPubKey, for 'standard' transaction types.
- */
-bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, vector<vector<unsigned char> >& vSolutionsRet)
+bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet)
 {
     // Templates
     static multimap<txnouttype, CScript> mTemplates;
