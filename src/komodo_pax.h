ee0c53e50cebc1594ea0e49497125036b144fb3e
jl777 <jameslee777@yahoo.com>
2017-08-24 17:46:36 +0200

    Test

--- src/komodo_pax.h
+++ src/komodo_pax.h
@@ -617,7 +617,7 @@ uint64_t komodo_paxprice(uint64_t *seedp,int32_t height,char *base,char *rel,uin
     if ( ASSETCHAINS_SYMBOL[0] == 0 && chainActive.Tip() != 0 && height > chainActive.Tip()->nHeight )
     {
         if ( height < 100000000 )
-            printf("height.%d vs tip.%d\n",height,chainActive.Tip()->nHeight);
+            printf("komodo_paxprice height.%d vs tip.%d\n",height,chainActive.Tip()->nHeight);
         return(0);
     }
     *seedp = komodo_seed(height);
