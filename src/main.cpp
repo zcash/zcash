7e24efd429bf3c07882fecad4de7e0b8a6401e46
jl777 <jameslee777@yahoo.com>
2017-09-18 15:09:53 +0200

    Print sync messages

--- src/main.cpp
+++ src/main.cpp
@@ -4583,6 +4583,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 {
     const CChainParams& chainparams = Params();
     LogPrint("net", "received: %s (%u bytes) peer=%d\n", SanitizeString(strCommand), vRecv.size(), pfrom->id);
+    fprintf(stderr, "recv: %s (%u bytes) peer=%s\n", SanitizeString(strCommand), vRecv.size(), pfrom->addr.ToString());
     if (mapArgs.count("-dropmessagestest") && GetRand(atoi(mapArgs["-dropmessagestest"])) == 0)
     {
         LogPrintf("dropmessagestest DROPPING RECV MESSAGE\n");
