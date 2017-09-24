7ed38a865814a376b80e15d4f930b9e4c3aead7e
Tomas M <tomas@slax.org>
2017-09-24 18:47:28 +0200

    Use multiOS libsnark version to make it work in Win32

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -1,10 +1,9 @@
 package=libsnark
-$(package)_version=0.1
-$(package)_download_path=https://github.com/zcash/$(package)/archive/
+$(package)_download_path=https://github.com/ZencashOfficial/$(package)/releases/download/v20170131/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
-$(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=dad153fe46e2e1f33557a195cbe7d69aed8b19ed9befc08ddcb8c6d3c025941f
-$(package)_git_commit=9ada3f84ab484c57b2247c2f41091fd6a0916573
+
+$(package)_sha256_hash=0c243deec42b133948cd5d77848f7988a79e82e5bf570c39d9a96ee7a42f5302
+$(package)_git_commit=3854b20c25e8bc567aab2b558dec84d45f4a3e73
 
 define $(package)_set_vars
     $(package)_build_env=CC="$($(package)_cc)" CXX="$($(package)_cxx)"
@@ -14,9 +13,9 @@ endef
 $(package)_dependencies=libgmp libsodium
 
 define $(package)_build_cmds
-  $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
+    $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) install STATIC=1 DEPINST=$(host_prefix) PREFIX=$($(package)_staging_dir)$(host_prefix) CURVE=ALT_BN128 NO_SUPERCOP=1
+    $(MAKE) install STATIC=1 DEPINST=$(host_prefix) PREFIX=$($(package)_staging_dir)$(host_prefix) CURVE=ALT_BN128 NO_SUPERCOP=1
 endef
