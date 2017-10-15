db402e48fc2dc33365e69646d40e63d241ef5c1b
ca333 <ca333@users.noreply.github.com>
2017-10-15 23:51:22 +0200

    linux build os detection

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,7 @@
 rust_packages := rust librustzcash
 zcash_packages := libsnark libgmp libsodium
 
-ifeq ($(host_os),linux)
+ifeq ($(build_os),linux)
 	packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock
 else
 	packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock libcurl
