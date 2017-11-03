a86cf6435afa68210d8918801bb632beb67ada51
Tomas M <tomas@slax.org>
2017-11-04 00:08:09 +0100

    vot.bat script update

--- zcutil/vot.bat
+++ zcutil/vot.bat
@@ -39,16 +39,23 @@ if "%1" == "totals" (
 
 :help
 
+   cls
    echo.
-   echo Helper script to make VoteCoin operations easier
-   echo Supported syntax:
+   echo ------------------------------------------------------------------------
+   echo   This is helper script to make VoteCoin operations easier.
+   echo   Make sure votecoind.exe is running so we can communicate with it.
+   echo   Supported commands:
    echo.
    echo    vot addr ... generate new T address
    echo    vot zaddr  ... generate new Z address
-   echo    vot send FROM TO AMOUNT ... send coins FROM address to TO address of given AMOUNT, with zero fee
-   echo    vot sendto TO AMOUNT ... send coins from any address to TO address of given AMOUNT
-   echo    vot status ... show status of last transaction. Empty status means transaction still in progress
+   echo    vot send FROM TO AMOUNT ... send coins FROM address to TO address
+   echo    vot sendto TO AMOUNT ... send coins from any address to TO address
+   echo    vot status ... show status of last transaction.
    echo    vot totals ... show total balances in your entire wallet
+   echo ------------------------------------------------------------------------
+   echo.
+   echo.
+   cmd.exe
    goto exit
 
 :exit
