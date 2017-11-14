e7264d3de2c839c437ee8e9c26dcc98462ced39c
practicalswift <practicalswift@users.noreply.github.com>
2017-11-14 23:35:42 +0100

    Fix missing locking in CTxMemPool::check(const CCoinsViewCache *pcoins)
    * reading variable 'mapTx' requires holding mutex 'cs'
    * reading variable 'mapNextTx' requires holding mutex 'cs'
    * reading variable 'nCheckFrequency' requires holding mutex 'cs'
    
    zcash: cherry picked from commit 6bc5b7100b1182c4c060c045df9a58dada3d3eab
    zcash: https://github.com/bitcoin/bitcoin/pull/11689

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -491,6 +491,7 @@ void CTxMemPool::clear()
 
 void CTxMemPool::check(const CCoinsViewCache *pcoins) const
 {
+    LOCK(cs);
     if (nCheckFrequency == 0)
         return;
 
@@ -505,7 +506,6 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const
     CCoinsViewCache mempoolDuplicate(const_cast<CCoinsViewCache*>(pcoins));
     const int64_t nSpendHeight = GetSpendHeight(mempoolDuplicate);
 
-    LOCK(cs);
     std::list<const CTxMemPoolEntry*> waitingOnDependants;
     for (indexed_transaction_set::const_iterator it = mapTx.begin(); it != mapTx.end(); it++) {
         unsigned int i = 0;
