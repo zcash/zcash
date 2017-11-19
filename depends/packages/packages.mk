24902602f2f695a85dd364836b041ce5ec0a2565
syd <sydo@protonmail.com>
2017-11-19 14:08:17 -0500

    Upgrade googletest to 1.8.0

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,7 @@
 rust_packages := rust librustzcash
 proton_packages := proton
 zcash_packages := libgmp libsodium
-packages := boost openssl libevent zeromq $(zcash_packages) googletest googlemock
+packages := boost openssl libevent zeromq $(zcash_packages) googletest
 native_packages := native_ccache
 
 wallet_packages=bdb
