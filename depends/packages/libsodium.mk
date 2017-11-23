0c177bc585ca96029a606249c6bd09e6e6f8c6c2
syd <sydo@protonmail.com>
2017-11-22 23:16:51 -0500

    Update libsodium from 1.0.11 to 1.0.15

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,8 +1,8 @@
 package=libsodium
-$(package)_version=1.0.11
+$(package)_version=1.0.15
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
-$(package)_file_name=libsodium-1.0.11.tar.gz
-$(package)_sha256_hash=a14549db3c49f6ae2170cbbf4664bd48ace50681045e8dbea7c8d9fb96f9c765
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe9e178f4
 $(package)_dependencies=
 $(package)_config_opts=
 
