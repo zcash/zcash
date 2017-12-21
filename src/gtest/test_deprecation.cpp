bf69507cb579f88c36c80d29379c050fb28aaad6
kozyilmaz <kazim@monolytic.com>
2017-12-21 12:35:16 +0300

    Fix test/gtest bugs caught by latest macOS clang

--- src/gtest/test_deprecation.cpp
+++ src/gtest/test_deprecation.cpp
@@ -24,7 +24,7 @@ static bool ThreadSafeMessageBox(MockUIInterface *mock,
                                  const std::string& caption,
                                  unsigned int style)
 {
-    mock->ThreadSafeMessageBox(message, caption, style);
+    return mock->ThreadSafeMessageBox(message, caption, style);
 }
 
 class DeprecationTest : public ::testing::Test {
