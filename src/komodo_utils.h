f6d8f2cce9755fc9d44f3788293472b5a3cd8af1
jl777 <jameslee777@yahoo.com>
2018-01-28 19:58:35 +0400

    rpcworkqueue=64

--- src/komodo_utils.h
+++ src/komodo_utils.h
@@ -1364,7 +1364,7 @@ void komodo_configfile(char *symbol,uint16_t port)
         {
             if ( (fp= fopen(fname,"wb")) != 0 )
             {
-                fprintf(fp,"rpcuser=user%u\nrpcpassword=pass%s\nrpcport=%u\nserver=1\ntxindex=1\n\n",crc,password,port);
+                fprintf(fp,"rpcuser=user%u\nrpcpassword=pass%s\nrpcport=%u\nserver=1\ntxindex=1\nrpcworkqueue=64\n",crc,password,port);
                 fclose(fp);
                 printf("Created (%s)\n",fname);
             } else printf("Couldnt create (%s)\n",fname);
