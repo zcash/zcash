dc8e0ee81ff9bb8aad5763fb6ec14fd75da1c5b8
Jon Layton <jon@seequ.com>
2018-02-18 15:32:01 -0600

    Revert "fix make files to build for windows"
    This reverts commit 33b4077522bb9df67c02c07993817c1d86075c8f.

--- depends/packages/librustzcash.mk
+++ depends/packages/librustzcash.mk
@@ -7,14 +7,13 @@ $(package)_sha256_hash=a5760a90d4a1045c8944204f29fa2a3cf2f800afee400f88bf89bbfe2
 $(package)_git_commit=91348647a86201a9482ad4ad68398152dc3d635e
 $(package)_dependencies=rust
 
-#  cargo build --release
 define $(package)_build_cmds
-    cargo  build --release --lib --target="x86_64-pc-windows-gnu"
+  cargo build --release
 endef
 
 define $(package)_stage_cmds
   mkdir $($(package)_staging_dir)$(host_prefix)/lib/ && \
   mkdir $($(package)_staging_dir)$(host_prefix)/include/ && \
-  cp target/x86_64-pc-windows-gnu/release/rustzcash.lib $($(package)_staging_dir)$(host_prefix)/lib/librustzcash.a && \
+  cp target/release/librustzcash.a $($(package)_staging_dir)$(host_prefix)/lib/ && \
   cp include/librustzcash.h $($(package)_staging_dir)$(host_prefix)/include/
 endef
