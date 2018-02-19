531f933d3f9b620cd1e7fae9161da843c7a8ef42
zahidmadeel <zahidmadeel@gmail.com>
2018-02-19 18:35:49 +0500

    more changes in contrib compiling=yes

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -5,13 +5,13 @@ SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
 animecoind=${animecoind:-$SRCDIR/animecoind}
-ZCASHCLI=${ZCASHCLI:-$SRCDIR/animecoin-cli}
-ZCASHTX=${ZCASHTX:-$SRCDIR/animecoin-tx}
+ANIMECOINCLI=${ANIMECOINCLI:-$SRCDIR/animecoin-cli}
+ANIMECOINTX=${ANIMECOINTX:-$SRCDIR/animecoin-tx}
 
 [ ! -x $animecoind ] && echo "$animecoind not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-ZECVERSTR=$($ZCASHCLI --version | head -n1 | awk '{ print $NF }')
+ZECVERSTR=$($ANIMECOINCLI --version | head -n1 | awk '{ print $NF }')
 ZECVER=$(echo $ZECVERSTR | awk -F- '{ OFS="-"; NF--; print $0; }')
 ZECCOMMIT=$(echo $ZECVERSTR | awk -F- '{ print $NF }')
 
@@ -21,7 +21,7 @@ ZECCOMMIT=$(echo $ZECVERSTR | awk -F- '{ print $NF }')
 echo "[COPYRIGHT]" > footer.h2m
 $animecoind --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $animecoind $ZCASHCLI $ZCASHTX; do
+for cmd in $animecoind $ANIMECOINCLI $ANIMECOINTX; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=$ZECVER --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-$ZECCOMMIT//g" ${MANDIR}/${cmdname}.1
