41572034cb0854a04a87cecf17b6bc2f46609481
Jonathan "Duke" Leto <jonathan@leto.net>
2018-03-03 16:49:58 -0800

    Fix the port in curl examples shown at the bottom of all RPC method help texts

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -863,7 +863,7 @@ std::string HelpExampleCli(const std::string& methodname, const std::string& arg
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
-        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8232/\n";
+        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:7771/\n";
 }
 
 void RPCRegisterTimerInterface(RPCTimerInterface *iface)
