8b53992182b73330e52044ea998e8ef5f2700e6c
jl777 <jameslee777@yahoo.com>
2018-03-26 15:45:00 +0300

    Test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -924,6 +924,7 @@ uint64_t komodo_accrued_interest(int32_t *txheightp,uint32_t *locktimep,uint256 
     uint64_t value; uint32_t tiptime=0,txheighttimep; CBlockIndex *pindex;
     if ( (pindex= chainActive[tipheight]) != 0 )
         tiptime = (uint32_t)pindex->nTime;
+    else printf("cant find height[%d]\n",tipheight);
     if ( (*locktimep= komodo_interest_args(&txheighttimep,txheightp,&tiptime,&value,hash,n)) != 0 )
     {
         if ( (checkvalue == 0 || value == checkvalue) && (checkheight == 0 || *txheightp == checkheight) )
