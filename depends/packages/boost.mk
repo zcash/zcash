1c4a74d40b8999af3231db7bb2c13444a5e9dd86
Jack Grigg <jack@z.cash>
2018-03-26 16:23:37 +0200

    depends: Remove -mt suffix from Boost libraries
    Requires disabling --build-type=complete, because in Boost 1.66 it appears to be
    broken (only the tagged libraries are built).

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -7,7 +7,7 @@ $(package)_sha256_hash=5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad7581
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
+$(package)_config_opts=--layout=system --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
 $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
 $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
