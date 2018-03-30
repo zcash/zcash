0e400c88547273319762fe1308766751a96c2fea
Jack Grigg <jack@z.cash>
2018-03-30 19:06:51 +0100

    depends: Fix regex bugs in cargo-checksum.sh

--- depends/cargo-checksum.sh
+++ depends/cargo-checksum.sh
@@ -1,8 +1,8 @@
 echo "{\"files\":{$(
 find . -type f |  	# Get list of file paths
 grep -v $1 |		# Exclude Makefile hashes
-grep -v .stamp_ |	# Exclude Makefile stamps
-sed 's|^./||' |		# Remove leading ./
+grep -v '[.]stamp_' |	# Exclude Makefile stamps
+sed 's|^[.]/||' |	# Remove leading ./
 sort |			# Sort (for uniformity)
 xargs $2 |		# Get SHA256 hashes (assumes standard 'H(A) A' format)
 awk -v OFS='":"' '{print $2, $1}' |	#   'H(A) A'  ->  'A":"H(A)'
