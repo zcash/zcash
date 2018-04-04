7cc423522f3497c374670eaccd11433fd9db11f9
Simon <simon@bitcartel.com>
2018-04-04 10:16:06 -0700

    Bump MIT Licence copyright header.

--- src/test/dbwrapper_tests.cpp
+++ src/test/dbwrapper_tests.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2012-2013 The Bitcoin Core developers
+// Copyright (c) 2018 The Zcash developers
+// Copyright (c) 2012-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
