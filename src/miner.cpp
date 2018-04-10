13691369bf9993e120183ad0dd561ef2a2f0c8f5
jl777 <jameslee777@yahoo.com>
2018-04-10 17:10:41 +0300

    Test

--- src/miner.cpp
+++ src/miner.cpp
@@ -654,6 +654,7 @@ static bool ProcessBlockFound(CBlock* pblock)
 
 int32_t komodo_baseid(char *origbase);
 int32_t komodo_eligiblenotary(uint8_t pubkeys[66][33],int32_t *mids,int32_t *nonzpkeysp,int32_t height);
+arith_uint256 komodo_PoWtarget(int32_t *percPoSp,arith_uint256 target,int32_t height,int32_t goalperc);
 int32_t FOUND_BLOCK,KOMODO_MAYBEMINED;
 extern int32_t KOMODO_LASTMINED;
 int32_t roundrobin_delay;
