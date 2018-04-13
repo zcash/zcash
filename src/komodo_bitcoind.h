178618db31059e10d9184adb3e6b418fdd7fb0da
jl777 <jameslee777@yahoo.com>
2018-04-13 21:54:33 +0300

    Test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -816,7 +816,7 @@ void komodo_index2pubkey33(uint8_t *pubkey33,CBlockIndex *pindex,int32_t height)
 
 int32_t komodo_eligiblenotary(uint8_t pubkeys[66][33],int32_t *mids,int32_t *nonzpkeysp,int32_t height)
 {
-    int32_t i,j,n,duplicate; CBlock block; CBlockIndex *pindex; uint8_t pubkey33[33],notarypubs[64][33];
+    int32_t i,j,n,duplicate; CBlock block; CBlockIndex *pindex; uint8_t notarypubs33[64][33];
     memset(mids,-1,sizeof(*mids)*66);
     n = komodo_notaries(notarypubs,height,0);
     for (i=duplicate=0; i<66; i++)
@@ -828,14 +828,14 @@ int32_t komodo_eligiblenotary(uint8_t pubkeys[66][33],int32_t *mids,int32_t *non
                 komodo_block2pubkey33(pubkeys[i],&block);
                 for (j=0; j<n; j++)
                 {
-                    if ( memcmp(pubkey33,pubkeys[j],33) == 0 )
+                    if ( memcmp(notarypubs33[j],pubkeys[i],33) == 0 )
                     {
                         mids[i] = j;
                         (*nonzpkeysp)++;
                         break;
                     }
                 }
-            }
+            } else fprintf("couldnt load block.%d\n",height);
             /*if ( pindex->didinit != 0 && (pindex->notaryid >= 64 || pindex->notaryid < -1) )
             {
                 fprintf(stderr,"unexpected notaryid.%d at ht.%d\n",pindex->notaryid,height-i);
