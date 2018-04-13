c8f73c1d211a6777999e83c9116c910374713bde
jl777 <jameslee777@yahoo.com>
2018-04-13 13:34:51 +0300

    Test

--- src/pow.cpp
+++ src/pow.cpp
@@ -141,7 +141,7 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
     if ( height == 0 )
     {
         height = komodo_currentheight() + 1;
-        fprintf(stderr,"set height to %d\n",height);
+        //fprintf(stderr,"set height to %d\n",height);
     }
     if ( height > 34000 && ASSETCHAINS_SYMBOL[0] == 0 ) // 0 -> non-special notary
     {
@@ -158,7 +158,7 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
         }
         flag = komodo_eligiblenotary(pubkeys,mids,&nonzpkeys,height);
         special2 = komodo_is_special(height,pubkey33,timestamp);
-        fprintf(stderr,"ht.%d notaryid.%d special.%d flag.%d special2.%d\n",height,notaryid,special,flag,special2);
+        //fprintf(stderr,"ht.%d notaryid.%d special.%d flag.%d special2.%d\n",height,notaryid,special,flag,special2);
         if ( notaryid >= 0 )
         {
             if ( height > 10000 && height < 80000 && (special != 0 || special2 > 0) )
@@ -174,7 +174,7 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
         }
         if ( flag != 0 || special2 > 0 )
         {
-            fprintf(stderr,"EASY MINING ht.%d\n",height);
+            //fprintf(stderr,"EASY MINING ht.%d\n",height);
             bnTarget.SetCompact(KOMODO_MINDIFF_NBITS,&fNegative,&fOverflow);
         }
     }
