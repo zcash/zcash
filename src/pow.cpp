d0dc7ea822701dfbef4a641a0fba10f57a14c281
jl777 <jameslee777@yahoo.com>
2018-04-13 20:23:03 +0300

    Test

--- src/pow.cpp
+++ src/pow.cpp
@@ -164,7 +164,6 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
         }
         flag = komodo_eligiblenotary(pubkeys,mids,&nonzpkeys,height);
         special2 = komodo_is_special(pubkeys,mids,height,pubkey33,timestamp);
-        //fprintf(stderr,"ht.%d notaryid.%d special.%d flag.%d special2.%d\n",height,notaryid,special,flag,special2);
         if ( notaryid >= 0 )
         {
             if ( height > 10000 && height < 80000 && (special != 0 || special2 > 0) )
@@ -175,8 +174,12 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
                 flag = ((height % KOMODO_ELECTION_GAP) > 64 || (height % KOMODO_ELECTION_GAP) == 0);
             else if ( height == 790833 )
                 flag = 1;
-            else if ( special2 < 0 && height > 792000 )
-                flag = 0;
+            else if ( special2 < 0 )
+            {
+                if ( height > 792000 )
+                    flag = 0;
+                else fprintf(stderr,"ht.%d notaryid.%d special.%d flag.%d special2.%d\n",height,notaryid,special,flag,special2);
+            }
             if ( flag != 0 || special2 > 0 )
             {
                 //fprintf(stderr,"EASY MINING ht.%d\n",height);
@@ -208,7 +211,8 @@ bool CheckProofOfWork(int32_t height,uint8_t *pubkey33,uint256 hash, unsigned in
         for (i=0; i<66; i++)
             printf("%d ",mids[i]);
         printf(" minerids from ht.%d pindex.%p\n",height,pindex);
-        return false;
+        if ( height > 792000 )
+            return false;
     }
     return true;
 }
