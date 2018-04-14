9f0535143b944fac1c96b5b188d7b9c309b6ac16
ca333 <ca333@safe-mail.net>
2018-04-14 18:00:06 +0200

    test

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_62_0
-$(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.62.0
+$(package)_version=1_66_0
+$(package)_download_path=https://sourceforge.net/projects/boost/files/boost/1.66.0/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=36c96b0f6155c98404091d8ceb48319a28279ca0333fba1ad8611eb90afb2ca0
+$(package)_sha256_hash=5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9
 $(package)_patches=deprecated_auto_ptr.patch include_poll.patch
 
 define $(package)_set_vars
