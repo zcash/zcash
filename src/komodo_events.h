774680ba81cd0c28c489f682dc9a356c13c7d7ca
jl777 <jameslee777@yahoo.com>
2018-04-14 15:13:39 +0300

    Test

--- src/komodo_events.h
+++ src/komodo_events.h
@@ -91,7 +91,7 @@ void komodo_eventadd_pricefeed(struct komodo_state *sp,char *symbol,int32_t heig
 void komodo_eventadd_opreturn(struct komodo_state *sp,char *symbol,int32_t height,uint256 txid,uint64_t value,uint16_t vout,uint8_t *buf,uint16_t opretlen)
 {
     struct komodo_event_opreturn O; uint8_t *opret;
-    opretlen = (uint8_t *)calloc(1,sizeof(*O) + opretlen + 16);
+    opretlen = (uint8_t *)calloc(1,sizeof(O) + opretlen + 16);
     O.txid = txid;
     O.value = value;
     O.vout = vout;
