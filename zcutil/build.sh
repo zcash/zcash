f2d3715727acc3215f001405d54427fba163c6a0
Jack Grigg <jack@z.cash>
2018-04-14 11:39:32 -0600

    zcutil/build.sh: Remove --enable-werror from default configuration
    Windows cross-compile has currently-unavoidable warnings. Upstream Bitcoin Core
    has them as well. For now, let's remove this from the default configuration, and
    add it to the Linux and OSX CI builders (so we effectively still enforce it for
    merged PRs).

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -103,5 +103,5 @@ ld -v
 
 HOST="$HOST" BUILD="$BUILD" NO_PROTON="$PROTON_ARG" "$MAKE" "$@" -C ./depends/ V=1
 ./autogen.sh
-CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" $CONFIGURE_FLAGS --enable-werror CXXFLAGS='-g'
+CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" $CONFIGURE_FLAGS CXXFLAGS='-g'
 "$MAKE" "$@" V=1
