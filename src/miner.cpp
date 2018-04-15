14c144030af73a5315a6da9fc211d689abd24c13
jl777 <jameslee777@yahoo.com>
2018-04-15 22:33:44 +0300

    Fix non-static n -> n0/n1

--- src/miner.cpp
+++ src/miner.cpp
@@ -689,7 +689,6 @@ void static BitcoinMiner()
         if ( komodo_baseid(ASSETCHAINS_SYMBOL) < 0 )
             break;
     }
-    //sleep(60);
     komodo_chosennotary(&notaryid,chainActive.Tip()->nHeight,NOTARY_PUBKEY33,(uint32_t)chainActive.Tip()->GetBlockTime());
 
     std::string solver;
