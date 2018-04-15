c8a89440acc7da19570b278063764726fcb44229
jl777 <jameslee777@yahoo.com>
2018-04-15 22:14:35 +0300

    Syntax

--- src/komodo_notary.h
+++ src/komodo_notary.h
@@ -200,7 +200,7 @@ const char *Notaries_elected1[][2] =
 int32_t komodo_notaries(uint8_t pubkeys[64][33],int32_t height,uint32_t timestamp)
 {
     static uint8_t elected_pubkeys0[64][33],elected_pubkeys1[64][33],did0,did1,n0,n1;
-    int32_t i,htind; uint64_t mask = 0; struct knotary_entry *kp,*tmp;
+    int32_t i,htind,n; uint64_t mask = 0; struct knotary_entry *kp,*tmp;
     if ( timestamp == 0 && ASSETCHAINS_SYMBOL[0] != 0 )
         timestamp = komodo_heightstamp(height);
     else if ( ASSETCHAINS_SYMBOL[0] == 0 )
