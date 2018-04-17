e8fa58ecb575b42290bd1f28b8384b3cc758119f
jl777 <jameslee777@yahoo.com>
2018-04-17 16:43:03 +0300

    Default JUMBLR_PAUSE.1

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -205,6 +205,7 @@ UniValue jumblr_deposit(const UniValue& params, bool fHelp)
         if ( (retval= Jumblr_depositaddradd((char *)addr.c_str())) >= 0 )
             result.push_back(Pair("result", retval));
         else result.push_back(Pair("error", retval));
+        JUMBLR_PAUSE = 0;
     } else result.push_back(Pair("error", "invalid address"));
     return(result);
 }
@@ -222,6 +223,7 @@ UniValue jumblr_secret(const UniValue& params, bool fHelp)
         retval = Jumblr_secretaddradd((char *)addr.c_str());
         result.push_back(Pair("result", "success"));
         result.push_back(Pair("num", retval));
+        JUMBLR_PAUSE = 0;
     } else result.push_back(Pair("error", "invalid address"));
     return(result);
 }
