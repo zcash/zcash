7d65dcf4d9e1c11b58428bd231ef368c510d2f4a
Jack Grigg <jack@z.cash>
2018-04-18 04:25:29 +0100

    depends: Pass correct compiler, linker, and flags into googletest

--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -5,9 +5,14 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
 
+define $(package)_set_vars
+$(package)_cxxflags+=-std=c++11
+$(package)_cxxflags_linux=-fPIC
+endef
+
 define $(package)_build_cmds
-  $(MAKE) -C googlemock/make CXXFLAGS=-fPIC gmock.a && \
-  $(MAKE) -C googletest/make CXXFLAGS=-fPIC gtest.a
+  $(MAKE) -C googlemock/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gmock.a && \
+  $(MAKE) -C googletest/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gtest.a
 endef
 
 define $(package)_stage_cmds
