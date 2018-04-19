96df006a14a8b5c594bd2dbb1acb44e8e6aa675d
jl777 <jameslee777@yahoo.com>
2018-04-19 21:11:09 +0300

    Test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -1145,6 +1145,8 @@ arith_uint256 komodo_PoWtarget(int32_t *percPoSp,arith_uint256 target,int32_t he
 {
     CBlockIndex *pindex; arith_uint256 bnTarget,hashval,sum,ave; bool fNegative,fOverflow; int32_t i,n,ht,percPoS,diff,val;
     *percPoSp = percPoS = 0;
+    if ( height < 3 )
+        return(target);
     sum = arith_uint256(0);
     ave = sum;
     for (i=n=0; i<100; i++)
