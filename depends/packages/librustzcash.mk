62ba80e7665568410a199a612fe02990b62e8932
Sean Bowe <ewillbefull@gmail.com>
2018-04-19 13:00:28 -0600

    Update librustzcash hash

--- depends/packages/librustzcash.mk
+++ depends/packages/librustzcash.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/zcash/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=fd57825e51a49a435a0a456e16bb2e9ddff6578cfd7cb0a80fc96a2a019eb3bd
-$(package)_git_commit=170397b5a5ebfa2f6823bb631ca7b248f17823ac
+$(package)_sha256_hash=b63ba98d569d332764f27706038c04d03ac7e2c836dc15dc4eaa24b04b8c7f4a
+$(package)_git_commit=6cc1813ae3bb1e42224fd8ca0a8977b95c576738
 $(package)_dependencies=rust $(rust_crates)
 $(package)_patches=cargo.config
 
