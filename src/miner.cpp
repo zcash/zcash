563d507c6156afcca5f6d984f43ba1c783748c0e
jl777 <jameslee777@yahoo.com>
2018-04-20 13:30:54 +0300

    Test

--- src/miner.cpp
+++ src/miner.cpp
@@ -404,16 +404,15 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn)
                 pblock->nTime = blocktime;
                 if ( GetAdjustedTime() < pblock->nTime )//|| pblock->GetBlockTime() > GetAdjustedTime() + 60)
                 {
-                    printf("need to wait %d seconds to submit: ",(int32_t)(pblock->nTime - GetAdjustedTime()));
-                    /*while ( GetAdjustedTime()+30 < pblock->nTime )
+                    fprintf(stderr,"need to wait %d seconds to mine:\n",(int32_t)(pblock->nTime - GetAdjustedTime()));
+                    while ( GetAdjustedTime()+30 < pblock->nTime )
                     {
                         sleep(30);
                         fprintf(stderr,"%d ",(int32_t)(pblock->nTime - GetAdjustedTime()));
                     }
-                    fprintf(stderr,"finished waiting\n");*/
+                    fprintf(stderr,"finished waiting\n");
                     //sleep(pblock->nTime - GetAdjustedTime());
                 }
-                
             } else return(0); //fprintf(stderr,"no utxos eligible for staking\n");
         }
         
