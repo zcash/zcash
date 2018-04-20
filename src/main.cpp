519578c9c93d2d8cbe2c9ebbd28cc03021c3bcb3
jl777 <jameslee777@yahoo.com>
2018-04-20 12:17:02 +0300

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -4112,7 +4112,7 @@ CBlockIndex *komodo_ensure(CBlock *pblock,uint256 hash)
     {
         if ( (pindex= miSelf->second) == 0 ) // create pindex so first Accept block doesnt fail
         {
-            pindex = AddToBlockIndex(*pblock);
+            miSelf->second = AddToBlockIndex(*pblock);
             fprintf(stderr,"Block header %s is already known, but without pindex -> ensured %p\n",hash.ToString().c_str(),pindex);
         }
     }
