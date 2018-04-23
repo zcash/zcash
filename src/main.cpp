43ef3068d139d449c198f7a8598288a1dd8bbc8e
jl777 <jameslee777@yahoo.com>
2018-04-23 23:47:48 +0300

    10 second window for notary mining

--- src/main.cpp
+++ src/main.cpp
@@ -3761,10 +3761,10 @@ bool CheckBlockHeader(int32_t height,CBlockIndex *pindex, const CBlockHeader& bl
     if (blockhdr.GetBlockTime() > GetAdjustedTime() + 60)
     {
         CBlockIndex *tipindex;
-        fprintf(stderr,"future block %u vs time.%u + 60\n",(uint32_t)blockhdr.GetBlockTime(),(uint32_t)GetAdjustedTime());
+        fprintf(stderr,"ht.%d future block %u vs time.%u + 60\n",height,(uint32_t)blockhdr.GetBlockTime(),(uint32_t)GetAdjustedTime());
         if ( (tipindex= chainActive.Tip()) != 0 && tipindex->GetBlockHash() == blockhdr.hashPrevBlock && blockhdr.GetBlockTime() < GetAdjustedTime() + 60*2 )
         {
-            fprintf(stderr,"it is the next block, let's wait a bit\n");
+            fprintf(stderr,"it is the next block, let's wait for %d seconds\n",GetAdjustedTime() + 60 - blockhdr.GetBlockTime());
             while ( blockhdr.GetBlockTime() > GetAdjustedTime() + 60 )
                 sleep(1);
             fprintf(stderr,"now its valid\n");
