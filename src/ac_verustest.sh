e329959f32de20f0658f5367b1b20157d75f1ecd
miketout <michaeltoutonghi@gmail.com>
2018-04-30 22:02:33 -0700

    moved verus (testnet) cli script to fiat directory

--- src/ac_verustest.sh
+++
@@ -1,2 +0,0 @@
-./komodo-cli -ac_name=VERUSTEST -ac_algo=verushash -ac_supply=0 -ac_eras=3 -ac_reward=0,38400000000,2400000000 -ac_halving=2,5,10 -ac_decay=100000000,0,0 -ac_end=14,44,0 -addnode=10.1.10.81 -ac_timelockgte=19200000000 -ac_timeunlockfrom=720 -ac_timeunlockto=1440 "$@"
-
