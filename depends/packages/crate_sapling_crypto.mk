fcbe7eef8dd5be61bd1275022c8316c3a6d61549
Sean Bowe <ewillbefull@gmail.com>
2018-05-07 18:56:55 -0600

    Update sapling-crypto and librustzcash

--- depends/packages/crate_sapling_crypto.mk
+++ depends/packages/crate_sapling_crypto.mk
@@ -3,8 +3,8 @@ $(package)_crate_name=sapling-crypto
 $(package)_download_path=https://github.com/zcash-hackworks/$($(package)_crate_name)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=5eb4040bc223a689341b3f1a1fc53d6064c4c032b23ae0c2c653b063e1da24db
-$(package)_git_commit=e554b473dd10885d232f42237c13282f5b6fee43
+$(package)_sha256_hash=5062b9e752066ad959f14063d496b0a156ce96004a13a6823494249793c01f96
+$(package)_git_commit=7beeb52730e24724ee10ea2458ecf7776cb59c58
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
