84e3a2212f9c4d0dcd9d9c6d2c0a24bec4a76fac
Jay Graber <jay@z.cash>
2018-05-17 06:13:52 -0700

    Change librustzcash dependency hash to work for new Sapling classes

--- depends/packages/librustzcash.mk
+++ depends/packages/librustzcash.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/zcash/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=1fb331a92b63da41e95ef9db671982d243a13bcd6d25570760c9ca83b8996887
-$(package)_git_commit=36d7acf3f37570f499fc8fe79fda372e5eb873ca
+$(package)_sha256_hash=ed8de6f820f4441a80457a25db022168df09ca945302d27004975b0d71c32718
+$(package)_git_commit=0ef61e6d460e49e70202f8599bbdb2d39e41d061
 $(package)_dependencies=rust $(rust_crates)
 $(package)_patches=cargo.config
 
--- src/zcash/prf.cpp
+++ src/zcash/prf.cpp
@@ -8,7 +8,7 @@
 const unsigned char ZCASH_EXPANDSEED_PERSONALIZATION[crypto_generichash_blake2b_PERSONALBYTES] = {'Z','c','a','s','h','_','E','x','p','a','n','d','S','e','e','d'};
 
 // Sapling 
-std::array<unsigned char, 64> PRF_expand(const uint256& x, unsigned char t)
+std::array<unsigned char, 64> PRF_expand(const uint256& sk, unsigned char t)
 {
     std::array<unsigned char, 64> res;   
     unsigned char blob[33];
@@ -65,6 +65,8 @@ std::array<unsigned char, 11> default_diversifier(const uint256& sk)
         
         if (librustzcash_check_diversifier(res.data())) {
             break;
+        } else if (blob[33] > 255) {
+            throw std::runtime_error("librustzcash_check_diversifier did not return valid diversifier");
         }
         blob[33] += 1;
     }
