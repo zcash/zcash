8abe764b18013165d7cca27229dff0710bddd1dd
Michael Toutonghi <michaeltoutonghi@gmail.com>
2018-05-26 04:02:10 -0700

    Disabled test only state

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -181,7 +181,7 @@ public:
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
-        //fMiningRequiresPeers = true;
+        fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
