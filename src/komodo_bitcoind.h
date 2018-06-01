7e679101e1a2afab6893e82d582945c81b0920a2
jl777 <jameslee777@yahoo.com>
2018-06-01 06:33:50 -1100

    Print

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -1128,7 +1128,7 @@ uint32_t komodo_stake(int32_t validateflag,arith_uint256 bnTarget,int32_t nHeigh
             }
         }
         //fprintf(stderr,"iterated until i.%d winner.%d\n",i,winner);
-        if ( 0 )
+        if ( 1 )
         {
             for (i=31; i>=24; i--)
                 fprintf(stderr,"%02x",((uint8_t *)&hashval)[i]);
@@ -1138,7 +1138,7 @@ uint32_t komodo_stake(int32_t validateflag,arith_uint256 bnTarget,int32_t nHeigh
             fprintf(stderr," segid.%d iter.%d winner.%d coinage.%llu %d ht.%d t.%u %.8f diff.%d\n",segid,iter,winner,(long long)coinage,(int32_t)(blocktime - txtime),nHeight,blocktime,dstr(value),(int32_t)diff);
         }
     }
-    if ( nHeight < 2 )
+    if ( nHeight < 10 )
         return(blocktime);
     return(blocktime * winner);
 }
