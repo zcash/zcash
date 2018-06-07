584b784c57d80a8e3f41f6bb2a1756b0004bf485
jl777 <jameslee777@yahoo.com>
2018-06-07 09:13:38 -1100

    Fix

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -327,7 +327,7 @@ UniValue validateaddress(const UniValue& params, bool fHelp)
 
         CScript scriptPubKey = GetScriptForDestination(dest);
         ret.push_back(Pair("scriptPubKey", HexStr(scriptPubKey.begin(), scriptPubKey.end())));
-        ret.push_back(Pair("segid", komodo_segid(address.c_str()) & 3f));
+        ret.push_back(Pair("segid", komodo_segid(address.c_str()) & 0x3f));
 #ifdef ENABLE_WALLET
         isminetype mine = pwalletMain ? IsMine(*pwalletMain, dest) : ISMINE_NO;
         ret.push_back(Pair("ismine", (mine & ISMINE_SPENDABLE) ? true : false));
