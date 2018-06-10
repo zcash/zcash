f60687ca929b5e76924e046a3060f5b23d8485a9
David Dawes <davidd@remitly.com>
2018-06-10 00:00:49 -0700

    Undo makeRelase.sh changes. Hacked libs in directly.

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
-# find the dylibs to copy for komodo-cli
-DYLIBS=`otool -L src/komodo-cli | grep "/usr/local" | awk -F' ' '{ print $1 }'`
-echo "copying $DYLIBS to $src"
-# copy the dylibs to the srcdir
-for dylib in $DYLIBS; do cp -rf $dylib src/; done
+  
+binaries=("komodo-cli" "komodod")
 
-# modify komodo-cli to point to dylibs
-echo "modifying $binary to use local libraries"
-for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/komodo-cli; done;
-chmod +x src/komodo-cli
-#
-# find the dylibs to copy for komodod
-DYLIBS=`otool -L src/komodod | grep "/usr/local" | awk -F' ' '{ print $1 }'`
-echo "copying $DYLIBS to $src"
-# copy the dylibs to the srcdir
-for dylib in $DYLIBS; do cp -rf $dylib src/; done
+for binary in "${binaries[@]}";
+do
+    # find the dylibs to copy for komodod
+    DYLIBS=`otool -L src/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+    echo "copying $DYLIBS to $src"
+    # copy the dylibs to the srcdir
+    for dylib in $DYLIBS; do cp -rf $dylib src/; done
 
-# modify komodod to point to dylibs
-echo "modifying $binary to use local libraries"
-for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/komodod; done;
-chmod +x src/komodod
-
+    # modify komodod to point to dylibs
+    echo "modifying $binary to use local libraries"
+    for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/$binary; done;
+    chmod +x src/$binary
+done
