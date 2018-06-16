17eb369969b83c70a2d09c5816874208d5232b58
David Dawes <davidd@remitly.com>
2018-06-16 10:47:23 -0700

    Get dependencies of dependencies to the second generation.

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,54 +1,45 @@
 #!/bin/sh
- 
+
+KMD_DIR=kmd/mac/verus-cli
 binaries=("komodo-cli" "komodod")
 
 for binary in "${binaries[@]}";
 do
-    # find the dylibs to copy for komodod
+  # find the dylibs to copy for komodod
     DYLIBS=`otool -L src/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
-    echo "copying $DYLIBS to $src"
+    echo "copying $DYLIBS to $KMD_DIR"
     # copy the dylibs to the srcdir
-    for dylib in $DYLIBS; do cp -rf $dylib src/; done
+    for dylib in $DYLIBS; do cp -rf $dylib $KMD_DIR; done
 
     # modify komodod to point to dylibs
     echo "modifying $binary to use local libraries"
-    for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/$binary; done;
-    chmod +x src/$binary
+    for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` $KMD_DIR/$binary; done;
+    chmod +x $KMD_DIR/$binary
 done
 
-libaries=("libgcc_s.1.dylib" "libgomp.1.dylib" "libidn2.0.dylib" "ibstdc++.6.dylib")
+libraries=("libgcc_s.1.dylib" "libgomp.1.dylib" "libidn2.0.dylib" "libstdc++.6.dylib")
 
 for binary in "${libraries[@]}";
 do
     # Need to undo this for the dylibs when we are done
-    chmod 755 src/$binary
-    # find the dylibs to copy for komodod
-    DYLIBS=`otool -L src/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
-    echo "copying $DYLIBS to $src"
+    chmod 755 $KMD_DIR/$binary
+  # find the dylibs to copy for komodod
+    DYLIBS=`otool -L $KMD_DIR/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+    echo "copying $DYLIBS to $KMD_DIR"
     # copy the dylibs to the srcdir
-    for dylib in $DYLIBS; do cp -rf $dylib src/; done
-
-    # modify binary or dylib to point to dylibs
-    echo "modifying $binary to use local libraries"
-    for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/$binary; done;
-    chmod +x src/$binary
+    for dylib in $DYLIBS; do cp -rf $dylib $KMD_DIR; done
 done
 
-indirectlibaries=("libintl.8.dylib" "libunistring.2.dylib")
+indirectlibraries=("libintl.8.dylib" "libunistring.2.dylib")
 
 for binary in "${indirectlibraries[@]}";
 do
     # Need to undo this for the dylibs when we are done
     chmod 755 src/$binary
     # find the dylibs to copy for komodod
-    DYLIBS=`otool -L src/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
-    echo "copying $DYLIBS to $src"
+    DYLIBS=`otool -L $KMD_DIR/$binary | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+    echo "copying $DYLIBS to $KMD_DIR"
     # copy the dylibs to the srcdir
-    for dylib in $DYLIBS; do cp -rf $dylib src/; done
-
-    # modify binary or dylib to point to dylibs
-    echo "modifying $binary to use local libraries"
-    for dylib in $DYLIBS; do install_name_tool -change $dylib @executable_path/`basename $dylib` src/$binary; done;
-    chmod +x src/$binary
+    for dylib in $DYLIBS; do cp -rf $dylib $KMD_DIR; done
 done
 
