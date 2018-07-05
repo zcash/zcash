20a1f68495067de1150a6d315c2351239c3857d2
jl777 <jameslee777@yahoo.com>
2018-07-05 04:01:37 -1100

    +print

--- src/main.cpp
+++ src/main.cpp
@@ -6826,8 +6826,10 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
         if (pindexBestHeader == NULL)
             pindexBestHeader = chainActive.Tip();
         bool fFetch = state.fPreferredDownload || (nPreferredDownload == 0 && !pto->fClient && !pto->fOneShot); // Download if this is a nice peer, or we have no nice peers and this one might do.
+        fprintf(stderr,"see if can request\n");
         if (!state.fSyncStarted && !pto->fClient && !fImporting && !fReindex)
         {
+            fprintf(stderr,"check for asking initial headers: nSyncStarted.%d fFetch.%d\n",(int32_t)nSyncStarted,(int32_t)fFetch);
             // Only actively request headers from a single peer, unless we're close to today.
             if ((nSyncStarted == 0 && fFetch) )//|| pindexBestHeader->GetBlockTime() > GetAdjustedTime() - 24 * 60 * 60)
             {
