1cb7611fc9254216319434abf0b22d0fa0c00a1f
jl777 <jameslee777@yahoo.com>
2018-07-10 03:58:38 -1100

    Test

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -1207,7 +1207,7 @@ uint32_t komodo_newstake(int32_t validateflag,arith_uint256 bnTarget,int32_t nHe
         }
         if ( validateflag != 0 )
         {
-            for (i=31; i>=0; i--)
+            /*for (i=31; i>=0; i--)
                 fprintf(stderr,"%02x",((uint8_t *)&ratio)[i]);
             fprintf(stderr," ratio -> ");
             for (i=31; i>=24; i--)
@@ -1215,13 +1215,16 @@ uint32_t komodo_newstake(int32_t validateflag,arith_uint256 bnTarget,int32_t nHe
             fprintf(stderr," vs target ");
             for (i=31; i>=24; i--)
                 fprintf(stderr,"%02x",((uint8_t *)&bnTarget)[i]);
-            fprintf(stderr," segid.%d iter.%d winner.%d coinage.%llu %d ht.%d gap.%d %.8f diff.%d\n",segid,iter,winner,(long long)coinage,(int32_t)(blocktime - txtime),nHeight,(int32_t)(blocktime - prevtime),dstr(value),(int32_t)diff);
+            fprintf(stderr," segid.%d iter.%d winner.%d coinage.%llu %d ht.%d gap.%d %.8f diff.%d\n",segid,iter,winner,(long long)coinage,(int32_t)(blocktime - txtime),nHeight,(int32_t)(blocktime - prevtime),dstr(value),(int32_t)diff);*/
             break;
         }
     }
     //fprintf(stderr,"iterated until i.%d winner.%d\n",i,winner);
-    if ( 0 )
+    if ( 1 )
     {
+        for (i=31; i>=0; i--)
+            fprintf(stderr,"%02x",((uint8_t *)&ratio)[i]);
+        fprintf(stderr," ratio -> ");
         for (i=31; i>=24; i--)
             fprintf(stderr,"%02x",((uint8_t *)&hashval)[i]);
         fprintf(stderr," vs ");
