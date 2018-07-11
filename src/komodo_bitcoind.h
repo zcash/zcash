ce2b6b330c64fecb79bef0c9608c8c8b3e3690e9
jl777 <jameslee777@yahoo.com>
2018-07-11 07:41:16 -1100

    Steepen PoW diff easement

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -1332,7 +1332,10 @@ arith_uint256 komodo_PoWtarget(int32_t *percPoSp,arith_uint256 target,int32_t he
     }
     else if ( percPoS > goalperc ) // decrease PoW diff -> raise bnTarget
     {
-        bnTarget = ((ave * arith_uint256(goalperc)) + (easydiff * arith_uint256(percPoS))) / arith_uint256(percPoS + goalperc);
+        //bnTarget = ((ave * arith_uint256(goalperc)) + (easydiff * arith_uint256(percPoS))) / arith_uint256(percPoS + goalperc);
+        bnTarget = (ave * arith_uint256(percPoS * percPoS * percPoS)) / arith_uint256(goalperc * goalperc);
+        if ( bnTarget > easydiff )
+            bnTarget = easydiff;
         if ( 1 )
         {
             for (i=31; i>=24; i--)
