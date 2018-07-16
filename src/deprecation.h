8d487e52a7f01b137e3704315e0eedcf8e6b001c
jl777 <jameslee777@yahoo.com>
2018-07-15 21:41:16 -1100

    Change deprecation height for 1400000

--- src/deprecation.h
+++ src/deprecation.h
@@ -6,14 +6,14 @@
 #define ZCASH_DEPRECATION_H
 
 // Deprecation policy:
-// * Shut down 16 weeks' worth of blocks after the estimated release block height.
-// * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 800000;
-static const int WEEKS_UNTIL_DEPRECATION = 52;
-static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 24);
+// * Shut down WEEKS_UNTIL_DEPRECATION weeks' worth of blocks after the estimated release block height.
+// * A warning is shown during the WEEKS_UNTIL_DEPRECATION worth of blocks prior to shut down.
+//static const int APPROX_RELEASE_HEIGHT = 800000;
+//static const int WEEKS_UNTIL_DEPRECATION = 52;
+static const int DEPRECATION_HEIGHT = 1400000; //APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 60);
 
 // Number of blocks before deprecation to warn users
-static const int DEPRECATION_WARN_LIMIT = 60 * 24 * 60; // 2 months
+static const int WEEKS_UNTIL_DEPRECATION = 60 * 24 * 60; // 2 months
 
 /**
  * Checks whether the node is deprecated based on the current block height, and
