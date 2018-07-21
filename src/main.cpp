b72f0608509ec793bb5ace2a615d252709e1fd14
jl777 <jameslee777@yahoo.com>
2018-07-21 08:38:37 -1100

    Dust exemption

--- src/main.cpp
+++ src/main.cpp
@@ -748,7 +748,7 @@ bool IsStandardTx(const CTransaction& tx, string& reason, const int nHeight)
         else if ((whichType == TX_MULTISIG) && (!fIsBareMultisigStd)) {
             reason = "bare-multisig";
             return false;
-        } else if (txout.IsDust(::minRelayTxFee)) {
+        } else if (txout.scriptPubKey.IsPayToCryptoCondition() == 0 && txout.IsDust(::minRelayTxFee)) {
             reason = "dust";
             return false;
         }
