e0509cb521f15a33b10848d3ff2cc8de54457dec
jl777 <jameslee777@yahoo.com>
2018-07-21 03:04:17 -1100

    Return valid vSolution

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -74,6 +74,8 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, vector<vector<unsi
         if (scriptPubKey.IsPayToCryptoCondition()) {
             if (scriptPubKey.MayAcceptCryptoCondition()) {
                 typeRet = TX_CRYPTOCONDITION;
+                vector<unsigned char> hashBytes(scriptPubKey.begin(), scriptPubKey.end());
+                vSolutionsRet.push_back(hashBytes);
                 return true;
             }
             return false;
