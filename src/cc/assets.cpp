9ecbb923977a3d7cdc983ec58e431dd7ae680ed2
jl777 <jameslee777@yahoo.com>
2018-07-22 11:21:53 -1100

    test

--- src/cc/assets.cpp
+++ src/cc/assets.cpp
@@ -325,10 +325,9 @@ bool ProcessAssets(Eval* eval, std::vector<uint8_t> paramsNull,const CTransactio
     static uint256 zero,prevtxid;
     CTransaction createTx; uint256 txid,assetid,assetid2,hashBlock; uint8_t funcid; int32_t i,n; uint64_t amount; std::vector<uint8_t> origpubkey;
     txid = ctx.GetHash();
-    if ( txid == prevtxid )
+    //if ( txid == prevtxid )
         return(true);
     fprintf(stderr,"ProcessAssets\n");
-    //CTransaction tx = *(CTransaction *)&ctx;
     if ( paramsNull.size() != 0 ) // Don't expect params
         return eval->Invalid("Cannot have params");
     else if ( (n= ctx.vout.size()) == 0 )
