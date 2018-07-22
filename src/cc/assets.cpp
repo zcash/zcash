a465e89d5adb581e5a2d7feee66f2631249f0f99
jl777 <jameslee777@yahoo.com>
2018-07-22 11:36:05 -1100

    Test

--- src/cc/assets.cpp
+++ src/cc/assets.cpp
@@ -338,6 +338,7 @@ bool ProcessAssets(Eval* eval, std::vector<uint8_t> paramsNull,const CTransactio
         return eval->Invalid("cant find asset create txid");
     if ( assetid2 != zero && eval->GetTxUnconfirmed(assetid2,createTx,hashBlock) == 0 )
         return eval->Invalid("cant find asset2 create txid");
+    return(true);
     if ( AssetValidate(eval,ctx,n,funcid,assetid,assetid2,amount,origpubkey) != 0 )
     {
         prevtxid = txid;
