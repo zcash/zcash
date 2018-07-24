14cfb1b72c6df7f5e14ad757d81d98939163760a
jl777 <jameslee777@yahoo.com>
2018-07-23 18:54:03 -1100

    Test

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -224,10 +224,10 @@ extern UniValue tokenfillswap(const UniValue& params, bool fHelp);
 extern UniValue faucetfund(const UniValue& params, bool fHelp);
 extern UniValue faucetget(const UniValue& params, bool fHelp);
 extern UniValue faucetaddress(const UniValue& params, bool fHelp);
-extern UniValue rewardsaddress(const UniValue& params, bool fHelp)
-extern UniValue rewardsfund(const UniValue& params, bool fHelp)
-extern UniValue rewardslock(const UniValue& params, bool fHelp)
-extern UniValue rewardsunlock(const UniValue& params, bool fHelp)
+extern UniValue rewardsaddress(const UniValue& params, bool fHelp);
+extern UniValue rewardsfund(const UniValue& params, bool fHelp);
+extern UniValue rewardslock(const UniValue& params, bool fHelp);
+extern UniValue rewardsunlock(const UniValue& params, bool fHelp);
 
 extern UniValue getnewaddress(const UniValue& params, bool fHelp); // in rpcwallet.cpp
 //extern UniValue getnewaddress64(const UniValue& params, bool fHelp); // in rpcwallet.cpp
