c9bab2a8d2e8c4956faa58df052c8518ed23eee3
jl777 <jameslee777@yahoo.com>
2018-07-24 07:18:26 -1100

    Fix

--- src/cc/CCrewards.h
+++ src/cc/CCrewards.h
@@ -24,7 +24,7 @@
 bool RewardsValidate(struct CCcontract_info *cp,Eval* eval,const CTransaction &tx);
 
 std::string RewardsFund(uint64_t txfee,char *planstr,uint64_t funds,uint64_t APR,uint64_t minseconds,uint64_t maxseconds,uint64_t mindeposit);
-std::string RewardsLock(uint64_t txfee,char *planstr,uint64_t amount)
+std::string RewardsLock(uint64_t txfee,char *planstr,uint64_t amount);
 std::string RewardsUnlock(uint64_t txfee);
 
 #endif
