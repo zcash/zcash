075a1114eb11d73b23c50fe7f85a2997aa38401e
jl777 <jameslee777@yahoo.com>
2018-07-27 01:48:25 -1100

    strcpy(cp->normaladdr,AssetsNormaladdr);

--- src/cc/CCcustom.cpp
+++ src/cc/CCcustom.cpp
@@ -46,7 +46,8 @@
 // Assets, aka Tokens
 #define FUNCNAME IsAssetsInput
 #define EVALCODE EVAL_ASSETS
-const char *AssetsCCaddr = "RGKRjeTBw4LYFotSDLT6RWzMHbhXri6BG6";  //"RFYE2yL3KknWdHK6uNhvWacYsCUtwzjY3u";
+const char *AssetsCCaddr = "RGKRjeTBw4LYFotSDLT6RWzMHbhXri6BG6";
+const char *AssetsNormaladdr = "RFYE2yL3KknWdHK6uNhvWacYsCUtwzjY3u";
 char AssetsCChexstr[67] = { "02adf84e0e075cf90868bd4e3d34a03420e034719649c41f371fc70d8e33aa2702" };
 uint8_t AssetsCCpriv[32] = { 0x9b, 0x17, 0x66, 0xe5, 0x82, 0x66, 0xac, 0xb6, 0xba, 0x43, 0x83, 0x74, 0xf7, 0x63, 0x11, 0x3b, 0xf0, 0xf3, 0x50, 0x6f, 0xd9, 0x6b, 0x67, 0x85, 0xf9, 0x7a, 0xf0, 0x54, 0x4d, 0xb1, 0x30, 0x77 };
 
@@ -57,7 +58,8 @@ uint8_t AssetsCCpriv[32] = { 0x9b, 0x17, 0x66, 0xe5, 0x82, 0x66, 0xac, 0xb6, 0xb
 // Faucet
 #define FUNCNAME IsFaucetInput
 #define EVALCODE EVAL_FAUCET
-const char *FaucetCCaddr = "R9zHrofhRbub7ER77B7NrVch3A63R39GuC";  //"RKQV4oYs4rvxAWx1J43VnT73rSTVtUeckk";
+const char *FaucetCCaddr = "R9zHrofhRbub7ER77B7NrVch3A63R39GuC";
+const char *FaucetNormaladdr = "RKQV4oYs4rvxAWx1J43VnT73rSTVtUeckk";
 char FaucetCChexstr[67] = { "03682b255c40d0cde8faee381a1a50bbb89980ff24539cb8518e294d3a63cefe12" };
 uint8_t FaucetCCpriv[32] = { 0xd4, 0x4f, 0xf2, 0x31, 0x71, 0x7d, 0x28, 0x02, 0x4b, 0xc7, 0xdd, 0x71, 0xa0, 0x39, 0xc4, 0xbe, 0x1a, 0xfe, 0xeb, 0xc2, 0x46, 0xda, 0x76, 0xf8, 0x07, 0x53, 0x3d, 0x96, 0xb4, 0xca, 0xa0, 0xe9 };
 
@@ -68,7 +70,8 @@ uint8_t FaucetCCpriv[32] = { 0xd4, 0x4f, 0xf2, 0x31, 0x71, 0x7d, 0x28, 0x02, 0x4
 // Rewards
 #define FUNCNAME IsRewardsInput
 #define EVALCODE EVAL_REWARDS
-const char *RewardsCCaddr = "RJCqA4jQTFEZ841dZgxko8aYgUU3FRNGNm";  //"RYRJGMAYEfLCZ6ZddbpxPiUZ1sens8vPYK";
+const char *RewardsCCaddr = "RJCqA4jQTFEZ841dZgxko8aYgUU3FRNGNm";
+const char *RewardsNormaladdr = "RYRJGMAYEfLCZ6ZddbpxPiUZ1sens8vPYK";
 char RewardsCChexstr[67] = { "026f00fdc2f1ed0006d66e2ca1787633590581c2fc90e7cb7b01a6c1131b40e94d" };
 uint8_t RewardsCCpriv[32] = { 0x9f, 0x0c, 0x57, 0xdc, 0x6f, 0x78, 0xae, 0xb0, 0xc7, 0x62, 0x9e, 0x7d, 0x2b, 0x90, 0x6b, 0xbd, 0x40, 0x78, 0x19, 0x5b, 0x3c, 0xb8, 0x82, 0x2d, 0x29, 0x84, 0x72, 0x7a, 0x59, 0x5a, 0x4b, 0x69 };
 #include "CCcustom.inc"
@@ -78,7 +81,8 @@ uint8_t RewardsCCpriv[32] = { 0x9f, 0x0c, 0x57, 0xdc, 0x6f, 0x78, 0xae, 0xb0, 0x
 // Dice
 #define FUNCNAME IsDiceInput
 #define EVALCODE EVAL_DICE
-const char *DiceCCaddr = "REabWB7KjFN5C3LFMZ5odExHPenYzHLtVw";  //"RLEe8f7Eg3TDuXii9BmNiiiaVGraHUt25c";
+const char *DiceCCaddr = "REabWB7KjFN5C3LFMZ5odExHPenYzHLtVw";
+const char *DiceNormaladdr = "RLEe8f7Eg3TDuXii9BmNiiiaVGraHUt25c";
 char DiceCChexstr[67] = { "039d966927cfdadab3ee6c56da63c21f17ea753dde4b3dfd41487103e24b27e94e" };
 uint8_t DiceCCpriv[32] = { 0x0e, 0xe8, 0xf5, 0xb4, 0x3d, 0x25, 0xcc, 0x35, 0xd1, 0xf1, 0x2f, 0x04, 0x5f, 0x01, 0x26, 0xb8, 0xd1, 0xac, 0x3a, 0x5a, 0xea, 0xe0, 0x25, 0xa2, 0x8f, 0x2a, 0x8e, 0x0e, 0xf9, 0x34, 0xfa, 0x77 };
 #include "CCcustom.inc"
@@ -88,7 +92,8 @@ uint8_t DiceCCpriv[32] = { 0x0e, 0xe8, 0xf5, 0xb4, 0x3d, 0x25, 0xcc, 0x35, 0xd1,
 // Lotto
 #define FUNCNAME IsLottoInput
 #define EVALCODE EVAL_LOTTO
-const char *LottoCCaddr = "RNXZxgyWSAE6XS3qGnTaf5dVNCxnYzhPrg";  //"RLW6hhRqBZZMBndnyPv29Yg3krh6iBYCyg";
+const char *LottoCCaddr = "RNXZxgyWSAE6XS3qGnTaf5dVNCxnYzhPrg";
+const char *LottoNormaladdr = "RLW6hhRqBZZMBndnyPv29Yg3krh6iBYCyg";
 char LottoCChexstr[67] = { "03f72d2c4db440df1e706502b09ca5fec73ffe954ea1883e4049e98da68690d98f" };
 uint8_t LottoCCpriv[32] = { 0xb4, 0xac, 0xc2, 0xd9, 0x67, 0x34, 0xd7, 0x58, 0x80, 0x4e, 0x25, 0x55, 0xc0, 0x50, 0x66, 0x84, 0xbb, 0xa2, 0xe7, 0xc0, 0x39, 0x17, 0xb4, 0xc5, 0x07, 0xb7, 0x3f, 0xca, 0x07, 0xb0, 0x9a, 0xeb };
 #include "CCcustom.inc"
@@ -98,7 +103,8 @@ uint8_t LottoCCpriv[32] = { 0xb4, 0xac, 0xc2, 0xd9, 0x67, 0x34, 0xd7, 0x58, 0x80
 // Ponzi
 #define FUNCNAME IsPonziInput
 #define EVALCODE EVAL_PONZI
-const char *PonziCCaddr = "RUKTbLBeKgHkm3Ss4hKZP3ikuLW1xx7B2x";  //"RWSHRbxnJYLvDjpcQ2i8MekgP6h2ctTKaj";
+const char *PonziCCaddr = "RUKTbLBeKgHkm3Ss4hKZP3ikuLW1xx7B2x";
+const char *PonziNormaladdr = "RWSHRbxnJYLvDjpcQ2i8MekgP6h2ctTKaj";
 char PonziCChexstr[67] = { "039b52d294b413b07f3643c1a28c5467901a76562d8b39a785910ae0a0f3043810" };
 uint8_t PonziCCpriv[32] = { 0x11, 0xe1, 0xea, 0x3e, 0xdb, 0x36, 0xf0, 0xa8, 0xc6, 0x34, 0xe1, 0x21, 0xb8, 0x02, 0xb9, 0x4b, 0x12, 0x37, 0x8f, 0xa0, 0x86, 0x23, 0x50, 0xb2, 0x5f, 0xe4, 0xe7, 0x36, 0x0f, 0xda, 0xae, 0xfc };
 #include "CCcustom.inc"
@@ -108,7 +114,8 @@ uint8_t PonziCCpriv[32] = { 0x11, 0xe1, 0xea, 0x3e, 0xdb, 0x36, 0xf0, 0xa8, 0xc6
 // Auction
 #define FUNCNAME IsAuctionInput
 #define EVALCODE EVAL_AUCTION
-const char *AuctionCCaddr = "RL4YPX7JYG3FnvoPqWF2pn3nQknH5NWEwx";  //"RFtVDNmdTZBTNZdmFRbfBgJ6LitgTghikL";
+const char *AuctionCCaddr = "RL4YPX7JYG3FnvoPqWF2pn3nQknH5NWEwx";
+const char *AuctionNormaladdr = "RFtVDNmdTZBTNZdmFRbfBgJ6LitgTghikL";
 char AuctionCChexstr[67] = { "037eefe050c14cb60ae65d5b2f69eaa1c9006826d729bc0957bdc3024e3ca1dbe6" };
 uint8_t AuctionCCpriv[32] = { 0x8c, 0x1b, 0xb7, 0x8c, 0x02, 0xa3, 0x9d, 0x21, 0x28, 0x59, 0xf5, 0xea, 0xda, 0xec, 0x0d, 0x11, 0xcd, 0x38, 0x47, 0xac, 0x0b, 0x6f, 0x19, 0xc0, 0x24, 0x36, 0xbf, 0x1c, 0x0a, 0x06, 0x31, 0xfb };
 #include "CCcustom.inc"
@@ -122,6 +129,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
     {
         case EVAL_ASSETS:
             strcpy(cp->unspendableCCaddr,AssetsCCaddr);
+            strcpy(cp->normaladdr,AssetsNormaladdr);
             strcpy(cp->CChexstr,AssetsCChexstr);
             memcpy(cp->CCpriv,AssetsCCpriv,32);
             cp->validate = AssetsValidate;
@@ -129,6 +137,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_FAUCET:
             strcpy(cp->unspendableCCaddr,FaucetCCaddr);
+            strcpy(cp->normaladdr,FaucetNormaladdr);
             strcpy(cp->CChexstr,FaucetCChexstr);
             memcpy(cp->CCpriv,FaucetCCpriv,32);
             cp->validate = FaucetValidate;
@@ -136,6 +145,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_REWARDS:
             strcpy(cp->unspendableCCaddr,RewardsCCaddr);
+            strcpy(cp->normaladdr,RewardsNormaladdr);
             strcpy(cp->CChexstr,RewardsCChexstr);
             memcpy(cp->CCpriv,RewardsCCpriv,32);
             cp->validate = RewardsValidate;
@@ -143,6 +153,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_DICE:
             strcpy(cp->unspendableCCaddr,DiceCCaddr);
+            strcpy(cp->normaladdr,DiceNormaladdr);
             strcpy(cp->CChexstr,DiceCChexstr);
             memcpy(cp->CCpriv,DiceCCpriv,32);
             cp->validate = DiceValidate;
@@ -150,6 +161,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_LOTTO:
             strcpy(cp->unspendableCCaddr,LottoCCaddr);
+            strcpy(cp->normaladdr,LottoNormaladdr);
             strcpy(cp->CChexstr,LottoCChexstr);
             memcpy(cp->CCpriv,LottoCCpriv,32);
             cp->validate = LottoValidate;
@@ -157,6 +169,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_PONZI:
             strcpy(cp->unspendableCCaddr,PonziCCaddr);
+            strcpy(cp->normaladdr,PonziNormaladdr);
             strcpy(cp->CChexstr,PonziCChexstr);
             memcpy(cp->CCpriv,PonziCCpriv,32);
             cp->validate = PonziValidate;
@@ -164,6 +177,7 @@ struct CCcontract_info *CCinit(struct CCcontract_info *cp,uint8_t evalcode)
             break;
         case EVAL_AUCTION:
             strcpy(cp->unspendableCCaddr,AuctionCCaddr);
+            strcpy(cp->normaladdr,AuctionNormaladdr);
             strcpy(cp->CChexstr,AuctionCChexstr);
             memcpy(cp->CCpriv,AuctionCCpriv,32);
             cp->validate = AuctionValidate;
