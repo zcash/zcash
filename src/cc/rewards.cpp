e426fab9b6a17090d1a966b384ec0ccff645923f
jl777 <jameslee777@yahoo.com>
2018-07-27 06:45:33 -1100

    Test

--- src/cc/rewards.cpp
+++ src/cc/rewards.cpp
@@ -47,7 +47,7 @@ uint64_t RewardsCalc(uint64_t amount,uint256 txid,uint64_t APR,uint64_t minsecon
     }
     else if ( duration > maxseconds )
         maxseconds = duration;
-    reward = ((amount * APR) / (COIN * 365*24*3600));// * duration;
+    reward = ((amount * APR) / COIN);// * duration;
     fprintf(stderr,"amount %.8f %.8f %llu -> duration.%llu reward %.8f\n",(double)amount/COIN,((double)amount * APR)/COIN,(long long)((amount * APR) / (COIN * 365*24*3600)),(long long)duration,(double)reward/COIN);
     return(reward);
 }
