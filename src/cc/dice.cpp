494b64e58ee20820bbfaa8889149a0918f4689f5
jl777 <jameslee777@yahoo.com>
2018-07-29 22:40:38 -1100

    -print

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -45,27 +45,12 @@ uint256 DiceHashEntropy(uint256 &entropy,uint256 _txidpriv) // max 1 vout per tx
     memset(&hentropy,0,32);
     endiancpy(txidpriv.bytes,(uint8_t *)&_txidpriv,32);
     txidpriv.bytes[0] &= 0xf8, txidpriv.bytes[31] &= 0x7f, txidpriv.bytes[31] |= 0x40;
-    for (i=0; i<32; i++)
-        fprintf(stderr,"%02x",txidpriv.bytes[i]);
-    fprintf(stderr," txidpriv\n");
     txidpub = curve25519(txidpriv,curve25519_basepoint9());
-    for (i=0; i<32; i++)
-        fprintf(stderr,"%02x",txidpub.bytes[i]);
-    fprintf(stderr," txidpub\n");
 
     Myprivkey(tmp256.bytes);
-    for (i=0; i<32; i++)
-        fprintf(stderr,"%02x",tmp256.bytes[i]);
-    fprintf(stderr," tmp256\n");
     vcalc_sha256(0,mypriv.bytes,tmp256.bytes,32);
     mypriv.bytes[0] &= 0xf8, mypriv.bytes[31] &= 0x7f, mypriv.bytes[31] |= 0x40;
-    for (i=0; i<32; i++)
-        fprintf(stderr,"%02x",mypriv.bytes[i]);
-    fprintf(stderr," mypriv\n");
     mypub = curve25519(mypriv,curve25519_basepoint9());
-    for (i=0; i<32; i++)
-        fprintf(stderr,"%02x",mypub.bytes[i]);
-    fprintf(stderr," mypub\n");
 
     ssecret = curve25519(mypriv,txidpub);
     ssecret2 = curve25519(txidpriv,mypub);
