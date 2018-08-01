b97fa94e72092da1feaf0bb73e8cf14e3c6b027f
jl777 <jameslee777@yahoo.com>
2018-07-31 22:54:59 -1100

    Test

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -887,6 +887,8 @@ std::string DiceWinLoseTimeout(int32_t *resultp,uint64_t txfee,char *planstr,uin
                 if ( winlosetimeout != 0 )
                     hentropy = DiceHashEntropy(entropy,mtx.vin[0].prevout.hash);
                 *resultp = 1;
+                char str[65],str2[65];
+                fprintf(stderr,"iswin.%d house entropy %s vs bettor %s\n",iswin,uint256_str(str,hentropyproof),uin256_str(str2,bettorentropy));
                 return(FinalizeCCTx(0,cp,mtx,mypk,txfee,EncodeDiceOpRet(funcid,sbits,fundingtxid,hentropy,hentropyproof)));
             } else fprintf(stderr,"iswin.%d does not match.%d\n",iswin,winlosetimeout);
         } else return("0");
