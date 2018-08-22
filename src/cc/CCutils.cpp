558b29ac15af73f5be55b97181de5d6d14ce3304
jl777 <jameslee777@yahoo.com>
2018-08-22 06:47:06 -1100

    -print

--- src/cc/CCutils.cpp
+++ src/cc/CCutils.cpp
@@ -242,7 +242,7 @@ bool Myprivkey(uint8_t myprivkey[])
             if ( pwalletMain->GetKey(keyID,vchSecret) != 0 )
             {
                 memcpy(myprivkey,vchSecret.begin(),32);
-                if ( 1 )
+                if ( 0 )
                 {
                     for (i=0; i<32; i++)
                         fprintf(stderr,"0x%02x, ",myprivkey[i]);
