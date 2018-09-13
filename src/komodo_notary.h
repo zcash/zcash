42ce922b972d6f84e7519918acfe39be40eff6d8
blackjok3r <blackjok3r@gmail.com>
2018-09-13 13:50:47 +0800

    fix stupid mistake

--- src/komodo_notary.h
+++ src/komodo_notary.h
@@ -247,7 +247,7 @@ int32_t komodo_notaries(uint8_t pubkeys[64][33],int32_t height,uint32_t timestam
         {
             if ( did1 == 0 )
             {
-	       if ( (strlen(symbol) >= 4 && strncmp(symbol, "STKD", 4) == 0) || (strlen(symbol) >= 6 && strncmp(symbol, "STAKED", 6) == 0) )
+	       if ( (strlen(ASSETCHAINS_SYMBOL) >= 4 && strncmp(ASSETCHAINS_SYMBOL, "STKD", 4) == 0) || (strlen(ASSETCHAINS_SYMBOL) >= 6 && strncmp(ASSETCHAINS_SYMBOL, "STAKED", 6) == 0) )
                {
                   n1 = (int32_t)(sizeof(notaries_STAKED)/sizeof(*notaries_STAKED));
                   for (i=0; i<n1; i++)
