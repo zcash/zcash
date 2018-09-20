3330881171f284e9edc2b578e681237f551aedd3
blackjok3r <blackjok3r@gmail.com>
2018-09-20 22:44:41 +0800

    Use min sigs = 3 for staked chains, so we can add more pubkeys.

--- src/crosschain_authority.cpp
+++ src/crosschain_authority.cpp
@@ -73,7 +73,7 @@ bool CheckTxAuthority(const CTransaction &tx, CrosschainAuthority auth)
 const CrosschainAuthority auth_STAKED = [&](){
     CrosschainAuthority auth;
     auth.size = (int32_t)(sizeof(notaries_STAKEDcc)/sizeof(*notaries_STAKEDcc));
-    auth.requiredSigs = (auth.size/5);
+    auth.requiredSigs = 3;
     for (int n=0; n<auth.size; n++)
         for (size_t i=0; i<33; i++)
             sscanf(notaries_STAKEDcc[n][1]+(i*2), "%2hhx", auth.notaries[n]+i);
