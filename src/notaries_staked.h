3b210ee00cbd89399088c3dadb4eda43b4740b0f
blackjok3r <blackjok3r@gmail.com>
2018-09-21 15:57:30 +0800

    WTFTWTFEWIDlknPI

--- src/notaries_staked.h
+++ src/notaries_staked.h
@@ -2,7 +2,7 @@
 #ifndef NOTARIES_STAKED
 #define NOTARIES_STAKED
 
-extern const char *notaries_STAKED[17][2];
+extern const char *notaries_STAKED[][2];
 extern int num_notaries_STAKED;
 
 bool is_STAKED();
