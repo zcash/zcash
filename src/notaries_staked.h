158d7befbc384c5d45ecada1c35d5e373ecb4858
blackjok3r <blackjok3r@gmail.com>
2018-09-21 13:11:12 +0800

    WTF!??!?

--- src/notaries_staked.h
+++ src/notaries_staked.h
@@ -1,6 +1,3 @@
-#ifndef NOTARIES_STAKED_H
-#define NOTARIES_STAKED_H
-
 extern const char *notaries_STAKED[][2];
 extern const int num_notaries_STAKED;
 
@@ -29,4 +26,3 @@ bool is_STAKED();
 
 const int num_notaries_STAKED = (int32_t)(sizeof(notaries_STAKED)/sizeof(*notaries_STAKED));
 */
-#endif
