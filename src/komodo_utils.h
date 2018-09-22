c4e1eb67262be9a92230d09e04ba88a69cbf8868
jl777 <jameslee777@yahoo.com>
2018-09-22 00:43:19 -1100

    PRATE halving 5x

--- src/komodo_utils.h
+++ src/komodo_utils.h
@@ -1704,6 +1704,8 @@ void komodo_args(char *argv0)
     {
         BITCOIND_RPCPORT = GetArg("-rpcport", ASSETCHAINS_RPCPORT);
         //fprintf(stderr,"(%s) port.%u chain params initialized\n",ASSETCHAINS_SYMBOL,BITCOIND_RPCPORT);
+        if ( strcmp("PIRATE",ASSETCHAINS_SYMBOL) == 0 && ASSETCHAINS_HALVING == 77777 )
+            ASSETCHAINS_HALVING *= 5;
     } else BITCOIND_RPCPORT = GetArg("-rpcport", BaseParams().RPCPort());
 }
 
