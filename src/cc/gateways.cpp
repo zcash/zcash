c15c1ad2ef71a4ff991fc914a9ecb3d6a5f3fb38
jl777 <jameslee777@yahoo.com>
2018-09-23 23:25:22 -1100

    Test

--- src/cc/gateways.cpp
+++ src/cc/gateways.cpp
@@ -317,7 +317,7 @@ int64_t AddGatewaysInputs(struct CCcontract_info *cp,CMutableTransaction &mtx,CP
             if ( E_UNMARSHAL(vopret,ss >> evalcode; ss >> funcid; ss >> assetid) != 0 )
             {
                 assetid = revuint256(assetid);
-                char str[65]; fprintf(stderr,"%d:%d (%c) check for refassetid.%s vs %s %.8f\n",evalcode,cp->evalcode,funcid,uint256_str(str,refassetid),uint256_str(str2,assetid),(double)nValue/COIN);
+                char str[65],str2[65]; fprintf(stderr,"%d:%d (%c) check for refassetid.%s vs %s %.8f\n",evalcode,cp->evalcode,funcid,uint256_str(str,refassetid),uint256_str(str2,assetid),(double)nValue/COIN);
                 if ( evalcode == cp->evalcode && assetid == refassetid && funcid == 't' && (nValue= vintx.vout[vout].nValue) > 0 && myIsutxo_spentinmempool(txid,vout) == 0 )
                 {
                     fprintf(stderr,"total %llu maxinputs.%d %.8f\n",(long long)total,maxinputs,(double)it->second.satoshis/COIN);
