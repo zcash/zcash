56ac817fb80bfefa0d8fd7ed5e0bd212425a48e6
miketout <michaeltoutonghi@gmail.com>
2018-10-12 22:44:52 -0700

    Fix height or greater check

--- src/cheatcatcher.cpp
+++ src/cheatcatcher.cpp
@@ -49,7 +49,7 @@ bool CCheatList::IsHeightOrGreaterInList(uint32_t height)
 {
     auto range = orderedCheatCandidates.equal_range(height);
     //printf("IsHeightOrGreaterInList: %s\n", range.second == orderedCheatCandidates.end() ? "false" : "true");
-    return (range.second != orderedCheatCandidates.end());
+    return (range.first != orderedCheatCandidates.end() || range.second != orderedCheatCandidates.end());
 }
 
 bool CCheatList::IsCheatInList(const CTransaction &tx, CTransaction *cheatTx)
