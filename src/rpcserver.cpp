0440e8a805aec3354f68ba378ecdee580800e052
jl777 <jameslee777@yahoo.com>
2018-10-20 08:08:12 -1100

    %s/Komodo server stopping

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -250,7 +250,7 @@ UniValue stop(const UniValue& params, bool fHelp)
             "\nStop Komodo server.");
     // Shutdown will take long enough that the response should get back
     StartShutdown();
-    sprintf(buf,"%s Komodo server stopping",ASSETCHAINS_SYMBOL);
+    sprintf(buf,"%s server stopping",ASSETCHAINS_SYMBOL[0] != 0 ? ASSETCHAINS_SYMBOL : "Komodo");
     return buf;
 }
 
