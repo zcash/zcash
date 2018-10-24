4e989f5d0ed25909ad31b4c9dad6bc5f90377537
blackjok3r <blackjok3r@gmail.com>
2018-10-24 14:30:30 +0800

    fix?

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4982,7 +4982,8 @@ UniValue setpubkey(const UniValue& params, bool fHelp)
         decode_hex(NOTARY_PUBKEY33,33,(char *)NOTARY_PUBKEY.c_str());
         pubkey2addr((char *)address,(uint8_t *)NOTARY_PUBKEY33);
         printf("%s\n",address);
-        result.push_back(Pair("R-address", address));
+        std::string str(address);
+        result.push_back(Pair("R-address", str));
     } else {
         result.push_back(Pair("error", "Can only set pubkey once, to change it you need to restart your daemon."));
     }
