db0fa8d71cacf5ded3d1f137e3f19b5377cdf50f
blackjok3r <blackjok3r@gmail.com>
2018-10-26 23:41:49 +0800

    try?

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -430,8 +430,8 @@ UniValue getdatafromblock(const UniValue& params, bool fHelp)
           uint256 firsttxid_256(uint256S(firsttxid));
           if (GetTransaction(firsttxid_256,firsttx,hash,false)) {
               std::string firststreamid = HexStr(firsttx.vout[2].scriptPubKey.begin(), firsttx.vout[2].scriptPubKey.end());
-              printf("first stream id changed to: %s\n", firststreamid.c_str());
               streamid.append(firststreamid.substr (8,64));
+              printf("first stream id changed to: %s\n", streamid.c_str());
               BlockMap::iterator mi = mapBlockIndex.find(hash);
               if (mi != mapBlockIndex.end() && (*mi).second) {
                   CBlockIndex* pindex = (*mi).second;
