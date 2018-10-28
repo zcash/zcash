5d7c92006eb5ccedcdfa5100c9d500dd49cebd67
blackjok3r <blackjok3r@gmail.com>
2018-10-29 00:05:19 +0800

    lol

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -962,7 +962,7 @@ std::string DiceBet(uint64_t txfee,char *planstr,uint256 fundingtxid,int64_t bet
     int32_t entropytxs=0,emptyvar=0;
     funding = DicePlanFunds(entropyval,entropytxid,sbits,cp,dicepk,fundingtxid,entropytxs,false);
     DicePlanFunds(entropyval,entropytxid,sbits,cp,dicepk,fundingtxid,emptyvar,true);
-    if ( ( funding >= 2*bet*odds+txfee && entropyval != 0 )
+    if ( ( funding >= 2*bet*odds+txfee && entropyval != 0 ) )
     {
         if ( entropytxs < 100 ) {
             error = "Your dealer is broke, find a new casino.";
