7a4eb1733243bc0d702614ae19b53dfd8855b15d
blackjok3r <blackjok3r@gmail.com>
2018-10-29 01:22:27 +0800

    w

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -413,7 +413,7 @@ int32_t DiceIsWinner(uint256 &entropy,uint256 txid,CTransaction tx,CTransaction 
             hentropy2 = DiceHashEntropy(entropy,vinTx.vin[0].prevout.hash);
             if ( hentropy == hentropy2 )
             {
-                char str[65];
+                //char str[65];
                 printf("%s\n",uint256_str(str,entropy));
                 winnings = DiceCalc(tx.vout[1].nValue,tx.vout[2].nValue,minbet,maxbet,maxodds,timeoutblocks,entropy,bettorentropy);
                 char str[65]; fprintf(stderr,"%s winnings %.8f bet %.8f at odds %d:1\n",uint256_str(str,tx.GetHash()),(double)winnings/COIN,(double)tx.vout[1].nValue/COIN,(int32_t)(tx.vout[2].nValue-10000));
