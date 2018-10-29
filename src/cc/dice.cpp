cdd74f5bb3da7450f3f842cd1915fafeb2e87fec
blackjok3r <blackjok3r@gmail.com>
2018-10-29 12:44:09 +0800

    a

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -682,6 +682,7 @@ int64_t DicePlanFunds(uint64_t &entropyval,uint256 &entropytxid,uint64_t refsbit
     SetCCunspents(unspentOutputs,coinaddr);
     entropyval = 0;
     int loops = 0;
+    fprintf(stderr, "number of unspents: %ld\n",unspentOutputs.size());
     for (std::vector<std::pair<CAddressUnspentKey, CAddressUnspentValue> >::const_iterator it=unspentOutputs.begin(); it!=unspentOutputs.end(); it++)
     {
         loops++;
