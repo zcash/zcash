55d887fa757f21f6f243e928ec8914f9af90b001
Mihailo Milenkovic <mihailo.milenkovic84@gmail.com>
2018-10-31 16:07:26 +0100

    Fix

--- src/cc/dapps/oraclefeed.c
+++ src/cc/dapps/oraclefeed.c
@@ -690,7 +690,7 @@ char *get_gatewaysmultisig(char *refcoin,char *acname,char *txidaddr,int32_t *K)
     {
         if ((hexstr=jstr(retjson,"hex")) != 0 )
         {            
-            if (strlen(hex)>0) hex = clonestr(hexstr);
+            if (strlen(hexstr)>0) hex = clonestr(hexstr);
         }
         *K=jint(retjson,"number_of_signs");   
         free_json(retjson);
