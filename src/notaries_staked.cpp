72bbdaef489b2dead6fc066dd7856d526110df9d
blackjok3r <blackjok3r@gmail.com>
2018-11-03 19:32:54 +0800

    add print

--- src/notaries_staked.cpp
+++ src/notaries_staked.cpp
@@ -156,6 +156,7 @@ int8_t StakedNotaryID(std::string &notaryname, char *Raddress) {
 
 int8_t ScanStakedArray(const char *notaries_chosen[][2],int num_notaries,char *Raddress,std::string &notaryname) {
     for (size_t i = 0; i < num_notaries; i++) {
+        fprintf(stderr, "%s\n", NOTARYADDRS[i]);
         if ( strcmp(Raddress,NOTARYADDRS[i]) == 0 ) {
             notaryname.assign(notaries_chosen[i][0]);
             return(i);
