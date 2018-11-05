156e471707bc04124ce50cf4e567ce0b59e1b786
blackjok3r <blackjok3r@gmail.com>
2018-11-05 18:27:08 +0800

    too slow

--- src/notaries_staked.h
+++ src/notaries_staked.h
@@ -9,7 +9,7 @@ static const int STAKED_ERA_GAP = 777;
 
 static const int STAKED_NOTARIES_TIMESTAMP1 = 1541412157;
 static const int STAKED_NOTARIES_TIMESTAMP2 = 1541413657;
-static const int STAKED_NOTARIES_TIMESTAMP3 = 1604233333;
+static const int STAKED_NOTARIES_TIMESTAMP3 = 1541414977;
 static const int STAKED_NOTARIES_TIMESTAMP4 = 1604244444;
 
 extern const char *notaries_STAKED1[][2];
