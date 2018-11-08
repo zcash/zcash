b7d5bb40f1e7d6b47148ad6eb6739f57c90252a9
jl777 <jameslee777@yahoo.com>
2018-11-08 06:05:42 -1100

    scripts

--- src/cc/dice.cpp
+++ src/cc/dice.cpp
@@ -1530,6 +1530,7 @@ double DiceStatus(uint64_t txfee,char *planstr,uint256 fundingtxid,uint256 bettx
                 printf("%s\n",cmd);*/
                 for (i=0; i<=(mintxs - entropytxs)/100; i++)
                 {
+                    fprintf(stderr,"sendmany.%d of %d\n",i,1 + (mintxs - entropytxs)/100);
                     if ( system("./sendmany") != 0 )
                     {
                         fprintf(stderr,"system error issuing.(./sendmany)\n");
