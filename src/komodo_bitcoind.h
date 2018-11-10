0424f2f3d1eb6c4ab5618053d956daa63f818aad
jl777 <jameslee777@yahoo.com>
2018-11-10 07:34:43 -1100

    Apply percentage

--- src/komodo_bitcoind.h
+++ src/komodo_bitcoind.h
@@ -1112,7 +1112,7 @@ uint64_t komodo_commission(const CBlock *pblock,int32_t height)
     txn_count = pblock->vtx.size();
     if ( ASSETCHAINS_FOUNDERS != 0 )
     {
-        return(GetBlockSubsidy(height,Params().GetConsensus()));
+        return((GetBlockSubsidy(height,Params().GetConsensus()) * ASSETCHAINS_COMMISSION) / COIN);
         n = pblock->vtx[0].vout.size();
         for (j=0; j<n; j++)
             if ( j != 1 )
