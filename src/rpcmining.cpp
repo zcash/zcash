b6f711b69d3e0a0d702537a50b66c84ed5b03b9b
blackjok3r <blackjok3r@gmail.com>
2018-11-11 06:26:36 +0800

    fix

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -526,9 +526,10 @@ UniValue getblocktemplate(const UniValue& params, bool fHelp)
     {
         const UniValue& oparam = params[0].get_obj();
         const UniValue& modeval = find_value(oparam, "mode");
-        if (modeval.isStr())
+        if (modeval.isStr()) {
             strMode = modeval.get_str();
-        fprintf(stderr, "%s\n",strMode.c_str());
+            fprintf(stderr, "%s\n",strMode.c_str());
+        }
         else if (modeval.isNull())
         {
             strMode = "template";
