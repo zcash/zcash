07d7f7e0e60bfc1839642a84359d16a09ba5cdb9
jl777 <jameslee777@yahoo.com>
2018-11-25 04:11:55 -1100

    Enable Dec 15th activation

--- src/main.cpp
+++ src/main.cpp
@@ -3870,9 +3870,9 @@ int32_t komodo_activate_sapling(CBlockIndex *pindex)
     }
     if ( activation != 0 )
     {
-#if KOMODO_SAPLING_ACTIVATION != 1544832000
+//#if KOMODO_SAPLING_ACTIVATION != 1544832000
         komodo_setactivation(activation);
-#endif
+//#endif
         fprintf(stderr,"%s sapling activation at %d\n",ASSETCHAINS_SYMBOL,activation);
         ASSETCHAINS_SAPLING = activation;
     }
