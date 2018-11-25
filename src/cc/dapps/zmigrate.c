2f385de2657026317464cedc4ae0a2ae34a8477a
jl777 <jameslee777@yahoo.com>
2018-11-25 05:13:56 -1100

    -print and 10,000 limit for migrate

--- src/cc/dapps/zmigrate.c
+++ src/cc/dapps/zmigrate.c
@@ -906,7 +906,7 @@ int32_t main(int32_t argc,char **argv)
     zsaddr = clonestr(argv[2]);
     printf("%s: %s %s\n",REFCOIN_CLI,coinstr,zsaddr);
     char coinaddr[64],zcaddr[128],opidstr[128]; int32_t alldone,finished; int64_t amount,stdamount,txfee;
-    stdamount = 1000 * SATOSHIDEN;
+    stdamount = 10000 * SATOSHIDEN;
     txfee = 10000;
 again:
     printf("start processing zmigrate\n");
