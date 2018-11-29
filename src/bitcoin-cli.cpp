95fbd857070b9a3688d28915f35b0362df6e98b6
blackjok3r <blackjok3r@gmail.com>
2018-11-29 23:59:48 +0800

    try

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -74,8 +74,8 @@ public:
 #include "komodo_globals.h"
 #include "komodo_utils.h"
 #include "komodo_cJSON.c"
-#include "komodo_notary.h"
-#include "notaries_staked.cpp"
+//#include "komodo_notary.h"
+//#include "notaries_staked.cpp"
 
 
 
