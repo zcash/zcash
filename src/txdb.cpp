ab5447324f16c9496a465ea5b9888b8271dd1cb6
blackjok3r <blackjok3r@gmail.com>
2018-12-11 11:52:39 +0800

    fix

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -526,7 +526,7 @@ UniValue CBlockTreeDB::Snapshot(int top)
       	char amount[32];
       	sprintf(amount, "%.8f", (double) it->first / COIN);
       	obj.push_back( make_pair("amount", amount) );
-        obj.push_back( make_pair("segid",((int32_t)komodo_segid32((char *)it->second.c_str() & 0x3f)) );
+        obj.push_back( make_pair("segid",(int32_t)komodo_segid32((char *)it->second.c_str()) & 0x3f) );
       	total += it->first;
       	addressesSorted.push_back(obj);
       	topN++;
