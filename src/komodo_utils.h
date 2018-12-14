9d9974442de11a7ee8d0483159c80dc6a6cfe150
blackjok3rtt <30971146+blackjok3rtt@users.noreply.github.com>
2018-12-14 17:49:23 +0800

    fix formatting again...

--- src/komodo_utils.h
+++ src/komodo_utils.h
@@ -1872,11 +1872,11 @@ void komodo_args(char *argv0)
             val = ASSETCHAINS_COMMISSION | (((uint64_t)ASSETCHAINS_STAKED & 0xff) << 32) | (((uint64_t)ASSETCHAINS_CC & 0xffff) << 40) | ((ASSETCHAINS_PUBLIC != 0) << 7) | ((ASSETCHAINS_PRIVATE != 0) << 6) | ASSETCHAINS_TXPOW;
             extralen += iguana_rwnum(1,&extraptr[extralen],sizeof(val),(void *)&val);
             if ( ASSETCHAINS_FOUNDERS != 0 )
-						{
+	    {
                 extralen += iguana_rwnum(1,&extraptr[extralen],sizeof(ASSETCHAINS_FOUNDERS),(void *)&ASSETCHAINS_FOUNDERS);
-								if ( ASSETCHAINS_FOUNDERS_PERIOD != 0 )
-										extralen += iguana_rwnum(1,&extraptr[extralen],sizeof(ASSETCHAINS_FOUNDERS_PERIOD),(void *)&ASSETCHAINS_FOUNDERS_PERIOD);
-						}
+		if ( ASSETCHAINS_FOUNDERS_PERIOD != 0 )
+		    extralen += iguana_rwnum(1,&extraptr[extralen],sizeof(ASSETCHAINS_FOUNDERS_PERIOD),(void *)&ASSETCHAINS_FOUNDERS_PERIOD);
+	    }
             if ( ASSETCHAINS_SCRIPTPUB.size() > 1 )
               	extralen += iguana_rwnum(1,&extraptr[extralen],(int32_t)ASSETCHAINS_SCRIPTPUB.size(),(void *)ASSETCHAINS_SCRIPTPUB.c_str());
         }
