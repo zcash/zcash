425bba692c6fd335734945ed9cb6e7d9bde503e3
jl777 <jameslee777@yahoo.com>
2018-12-28 03:41:26 -1100

    Fix -port

--- src/komodo_utils.h
+++ src/komodo_utils.h
@@ -1911,7 +1911,13 @@ void komodo_args(char *argv0)
             MAX_MONEY = 10000100000LL*SATOSHIDEN;
         //fprintf(stderr,"MAX_MONEY %llu %.8f\n",(long long)MAX_MONEY,(double)MAX_MONEY/SATOSHIDEN);
         //printf("baseid.%d MAX_MONEY.%s %.8f\n",baseid,ASSETCHAINS_SYMBOL,(double)MAX_MONEY/SATOSHIDEN);
-        ASSETCHAINS_P2PPORT = komodo_port(ASSETCHAINS_SYMBOL,ASSETCHAINS_SUPPLY,&ASSETCHAINS_MAGIC,extraptr,extralen);
+        uint16_t tmpport = komodo_port(ASSETCHAINS_SYMBOL,ASSETCHAINS_SUPPLY,&ASSETCHAINS_MAGIC,extraptr,extralen);
+        if ( GetArg("-port",0) != 0 )
+        {
+            ASSETCHAINS_P2PPORT = GetArg("-port",0);
+            fprintf(stderr,"set p2pport.%u\n",ASSETCHAINS_P2PPORT);
+        } else ASSETCHAINS_P2PPORT = tmpport;
+
         while ( (dirname= (char *)GetDataDir(false).string().c_str()) == 0 || dirname[0] == 0 )
         {
             fprintf(stderr,"waiting for datadir\n");
