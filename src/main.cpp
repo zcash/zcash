f52cbb1b069f228b1d6747a0ef25345e61b88d39
jl777 <jameslee777@yahoo.com>
2019-01-08 23:25:42 -1100

    Jan 15th activation

--- src/main.cpp
+++ src/main.cpp
@@ -3180,7 +3180,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         //fprintf(stderr,"checkblock failure in connectblock futureblock.%d\n",futureblock);
         return false;
     }
-    if ( fCheckPOW != 0 && pindex->nTime > 1546028567 && !ContextualCheckBlock(block, state, pindex->pprev) )
+    if ( fCheckPOW != 0 && pindex->nTime > 1547510400 && !ContextualCheckBlock(block, state, pindex->pprev) ) // Activate Jan 15th, 2019
     {
         fprintf(stderr,"ContextualCheckBlock failed ht.%d\n",(int32_t)pindex->GetHeight());
         return false;
