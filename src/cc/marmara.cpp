8d87e118df09455489d7bbd47ec953df2bbdf51e
jl777 <jameslee777@yahoo.com>
2019-01-16 23:17:12 -1100

    batontxid = spenttxid;

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -171,6 +171,7 @@ int32_t MarmaraGetbatontxid(std::vector<uint256> &creditloop,uint256 &batontxid,
             }
             else if ( value > 0 )
             {
+                batontxid = spenttxid;
                 fprintf(stderr,"n.%d got false baton %s/v%d %.8f\n",n,batontxid.GetHex().c_str(),vout,(double)value/COIN);
                 return(n);
             }
