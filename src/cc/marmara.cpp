dce284a1d18da00a07c4455a30309b91b75d2d9c
jl777 <jameslee777@yahoo.com>
2019-01-18 01:52:05 -1100

    _

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -417,7 +417,7 @@ UniValue MarmaraLock(uint64_t txfee,int64_t amount,int32_t height)
     mypk = pubkey2pk(Mypubkey());
     Marmarapk = GetUnspendable(cp,0);
     Getscriptaddress(coinaddr,CScript() << ParseHex(HexStr(mypk)) << OP_CHECKSIG);
-    val = AddNormalinputs2(tmpmtx,CCaddressbalance(coinaddr),MARMARA_VINS);
+    val = AddNormalinputs2(tmpmtx,CCaddress_balance(coinaddr),MARMARA_VINS);
     fprintf(stderr,"normal inputs %.8f numvins.%d\n",(double)val/COIN,(int32_t)tmpmtx.vin.size());
     inputsum = AddNormalinputs2(mtx,val,tmpmtx.vin.size() < MARMARA_VINS ? tmpmtx.vin.size() : MARMARA_VINS);
     mtx.vout.push_back(MakeCC1of2vout(EVAL_MARMARA,amount,Marmarapk,mypk));
