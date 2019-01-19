c7002513e383907de706e1e6459370d6ac448afe
jl777 <jameslee777@yahoo.com>
2019-01-19 03:37:28 -1100

    Height and from_mempool in club

--- src/cc/cclib.cpp
+++ src/cc/cclib.cpp
@@ -33,11 +33,21 @@ char *CClib_name() { return(MYCCLIBNAME); }
 
 bool CClib_Dispatch(const CC *cond,Eval *eval,std::vector<uint8_t> paramsNull,const CTransaction &txTo,unsigned int nIn)
 {
-    uint8_t evalcode;
+    uint8_t evalcode; int32_t height,from_mempool;
     if ( ASSETCHAINS_CCLIB != MYCCLIBNAME )
     {
         fprintf(stderr,"-ac_cclib=%s vs myname %s\n",ASSETCHAINS_CCLIB.c_str(),MYCCLIBNAME);
         return eval->Invalid("-ac_cclib name mismatches myname");
+    }
+    height = KOMODO_CONNECTING;
+    if ( KOMODO_CONNECTING < 0 ) // always comes back with > 0 for final confirmation
+        return(true);
+    if ( ASSETCHAINS_CC == 0 || (height & ~(1<<30)) < KOMODO_CCACTIVATE )
+        return eval->Invalid("CC are disabled or not active yet");
+    if ( (KOMODO_CONNECTING & (1<<30)) != 0 )
+    {
+        from_mempool = 1;
+        height &= ((1<<30) - 1);
     }
     evalcode = cond->code[0];
     if ( evalcode >= EVAL_FIRSTUSER && evalcode <= EVAL_LASTUSER )
