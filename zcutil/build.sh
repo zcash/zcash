63eda9498a8d6f03e57ba0bc5d7fc314aeb0e74f
blackjok3r <blackjok3r@gmail.com>
2019-01-25 16:50:15 +0800

    test fix

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -101,25 +101,12 @@ eval "$MAKE" --version
 as --version
 ld -v
 
-../src/cc/makecclib
+#BUILD CCLIB
+cd src/cc/ 
+./makecclib
 
 HOST="$HOST" BUILD="$BUILD" NO_PROTON="$PROTON_ARG" "$MAKE" "$@" -C ./depends/ V=1
 ./autogen.sh
-
-#BUILD CCLIB
-
-WD=$PWD
-cd src/cc
-echo $PWD
-
-if make "$@"; then
-  echo CCLIB BUILD SUCCESSFUL
-else
-  echo CCLIB BUILD FAILED
-  exit 1
-fi
-
-cd $WD
 
 CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" $CONFIGURE_FLAGS CXXFLAGS='-g'
 "$MAKE" "$@" V=1
