b399529ec84274978dcb01c1154f141630caa69f
jl777 <jameslee777@yahoo.com>
2019-01-29 08:08:29 -1100

    Ignore

--- src/cc/sudoku.cpp
+++ src/cc/sudoku.cpp
@@ -803,9 +803,9 @@ UniValue sudoku_solution(uint64_t txfee,struct CCcontract_info *cp,cJSON *params
                     {
                         result.push_back(Pair("result","error"));
                         result.push_back(Pair("error","captcha failure"));
-                        return(result);
+                        //return(result);
                     }
-                    else
+                    //else
                     {
                         if ( (txidstr= jstri(params,0)) != 0 )
                         {
