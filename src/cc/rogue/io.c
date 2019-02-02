8aec87e0204fa157d075bf2ee29e9c149cdc0f0c
jl777 <jameslee777@yahoo.com>
2019-02-02 07:21:14 -1100

    Sleep

--- src/cc/rogue/io.c
+++ src/cc/rogue/io.c
@@ -177,7 +177,11 @@ readchar(struct rogue_state *rs)
             {
                 rs->buffered[rs->num++] = ch;
                 if ( rs->num > (sizeof(rs->buffered)*9)/10 && rs->needflush == 0 )
+                {
                     rs->needflush = (uint32_t)time(NULL);
+                    fprintf(stderr,"needflush.%u %d of %d\n",rs->needflush,rs->num,(int32_t)sizeof(rs->buffered));
+                    sleep(3);
+                }
             } else fprintf(stderr,"buffer filled without flushed\n");
         }
     } else fprintf(stderr,"readchar rs.%p non-gui error?\n",rs);
