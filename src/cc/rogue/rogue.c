7a05f1eef3d0482651e23e65505087f5a4d9bb3c
jl777 <jameslee777@yahoo.com>
2019-02-11 01:49:56 -1100

    Disable auto bailout

--- src/cc/rogue/rogue.c
+++ src/cc/rogue/rogue.c
@@ -213,6 +213,7 @@ void rogue_bailout(struct rogue_state *rs)
     char cmd[512];
     //flushkeystrokes(rs);
     //sleep(5);
+    return;
     fprintf(stderr,"bailing out\n");
     sprintf(cmd,"./komodo-cli -ac_name=ROGUE cclib bailout 17 \\\"[%%22%s%%22]\\\" >> bailout.log",Gametxidstr);
     if ( system(cmd) != 0 )
