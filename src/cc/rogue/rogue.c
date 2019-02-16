593ec0e34092997f12d528d51f45882d057277c7
jl777 <jameslee777@yahoo.com>
2019-02-16 10:25:49 -1100

    Pause

--- src/cc/rogue/rogue.c
+++ src/cc/rogue/rogue.c
@@ -335,8 +335,9 @@ int32_t rogue_replay(uint64_t seed,int32_t sleeptime)
             fclose(fp);
         }
         rogue_replay2(0,seed,keystrokes,num,player,50);
-        mvaddstr(LINES - 2, 0, (char *)"replay completed");
         sleep(4);
+
+        //mvaddstr(LINES - 2, 0, (char *)"replay completed");
         endwin();
     }
     if ( keystrokes != 0 )
