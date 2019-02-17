7d50f06c7613cf74f56d23768919c7f52b114922
ca333 <ca333@users.noreply.github.com>
2019-02-17 18:21:18 +0100

    add ZMQ to WIN

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -49,6 +49,6 @@ wallet_packages=bdb
 ifeq ($(host_os),linux)
 	packages := boost openssl libevent zeromq $(zcash_packages) googletest #googlemock
 else
-	packages := boost openssl libevent $(zcash_packages) libcurl googletest #googlemock
+	packages := boost openssl libevent zeromq $(zcash_packages) libcurl googletest #googlemock
 endif
 
