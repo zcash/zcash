4f5ba23dfcf01bd3d272da05a004b60af3a8b40e
ca333 <ca333@keemail.me>
2019-02-17 16:05:19 +0100

    set static ZMQ flag

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -9,7 +9,7 @@ define $(package)_set_vars
   $(package)_build_env+=
   $(package)_config_opts=--enable-shared=false --enable-static --host=x86_64-w64-mingw32
   $(package)_config_opts_mingw32=--enable-shared=false --enable-static --host=x86_64-w64-mingw32
-  $(package)_cflags=-Wno-error -Wall -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -lws2_32 -liphlpapi -lrpcrt4
+  $(package)_cflags=-Wno-error -Wall -Wno-pedantic-ms-format -DZMQ_STATIC -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -lws2_32 -liphlpapi -lrpcrt4
   $(package)_conf_tool=./configure
 endef
 else
@@ -31,7 +31,8 @@ define $(package)_preprocess_cmds
   cd $($(package)_build_subdir); ./autogen.sh
 endef
 define $(package)_config_cmds
-  $($(package)_conf_tool) $($(package)_config_opts) CFLAGS="-Wno-error -Wall -Wno-pedantic-ms-format -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -lws2_32 -liphlpapi -lrpcrt4"
+  $($(package)_conf_tool) $($(package)_config_opts)
+	CFLAGS="-Wno-error -Wall -Wno-pedantic-ms-format -DZMQ_STATIC -DLIBCZMQ_EXPORTS -DZMQ_DEFINED_STDINT -lzmq -lws2_32 -liphlpapi -lrpcrt4"
 endef
 else
 define $(package)_config_cmds
