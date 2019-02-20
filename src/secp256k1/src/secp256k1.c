fb69ddb0c66c37f949b3e007720162344c7e4b8b
jl777 <jameslee777@yahoo.com>
2019-02-20 08:12:36 -1100

    Add modules

--- src/secp256k1/src/secp256k1.c
+++ src/secp256k1/src/secp256k1.c
@@ -580,6 +580,12 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 # include "modules/ecdh/main_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_MUSIG
+#include "modules/schnorrsig/main_impl.h"
+#include "modules/musig/main_impl.h"
+#endif
+
+
 #ifdef ENABLE_MODULE_RECOVERY
 # include "modules/recovery/main_impl.h"
 #endif
