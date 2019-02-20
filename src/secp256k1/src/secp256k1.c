38e67bca763fed2b6d2b618d5089019ac1b145fd
jl777 <jameslee777@yahoo.com>
2019-02-20 08:45:05 -1100

    ../include

--- src/secp256k1/src/secp256k1.c
+++ src/secp256k1/src/secp256k1.c
@@ -4,7 +4,7 @@
  * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
  **********************************************************************/
 
-#include "include/secp256k1.h"
+#include "../include/secp256k1.h"
 
 #include "util.h"
 #include "num_impl.h"
