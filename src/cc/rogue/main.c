4dcef6624bc8f2c1da09a9e8e78bca8357872028
jl777 <jameslee777@yahoo.com>
2019-02-24 08:17:20 -1100

    Delay after keystrokes

--- src/cc/rogue/main.c
+++ src/cc/rogue/main.c
@@ -726,9 +726,10 @@ void rogue_progress(struct rogue_state *rs,uint64_t seed,char *keystrokes,int32_
             sprintf(params,"[\"keystrokes\",\"17\",\"[%%22%s%%22,%%22%s%%22]\"]",Gametxidstr,hexstr);
             if ( (retstr= komodo_issuemethod(USERPASS,"cclib",params,ROGUE_PORT)) != 0 )
             {
-                //fprintf(stderr,"KEYSTROKES.(%s)\n",retstr);
+                fprintf(stderr,"KEYSTROKES.(%s)\n",retstr);
                 free(retstr);
             }
+            sleep(1);
         }
     }
 }
