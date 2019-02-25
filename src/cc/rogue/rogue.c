c5930804ab2789402fa7c0a0d73460a1aa32488b
jl777 <jameslee777@yahoo.com>
2019-02-24 18:24:33 -1100

    Flush keystrokes every 1000

--- src/cc/rogue/rogue.c
+++ src/cc/rogue/rogue.c
@@ -560,7 +560,7 @@ playit(struct rogue_state *rs)
         }
         else
         {
-            if ( rs->needflush != 0 && rs->num > 8000 )
+            if ( rs->needflush != 0 && rs->num > 1000 )
             {
                 if ( flushkeystrokes(rs) == 0 )
                     rs->needflush = 0;
