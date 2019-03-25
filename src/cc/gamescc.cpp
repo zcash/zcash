23cd6f2cbef1aab8af7b2606358085dddfbecb4f
jl777 <jameslee777@yahoo.com>
2019-03-25 09:58:56 -1100

    Mv

--- src/cc/gamescc.cpp
+++ src/cc/gamescc.cpp
@@ -1679,13 +1679,13 @@ void games_packitemstr(char *packitemstr,struct games_packitem *item)
 #include <memory.h>
 #include <string.h>
 #include <unistd.h>
-#include "../core_io.h"
-#include "../wallet/wallet.h"
-#include <univalue.h>
 
 #define inline
 #include <curl/curl.h>
 #include <curl/easy.h>
+#include "../core_io.h"
+#include "../wallet/wallet.h"
+#include <univalue.h>
 #undef inline
 
 char USERPASS[8192]; uint16_t GAMES_PORT;
