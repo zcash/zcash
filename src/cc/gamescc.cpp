fc3753d23b96782459aab4663a8543f6713b6a66
jl777 <jameslee777@yahoo.com>
2019-03-26 03:50:45 -1100

    +print

--- src/cc/gamescc.cpp
+++ src/cc/gamescc.cpp
@@ -1457,6 +1457,11 @@ UniValue games_fund(uint64_t txfee,struct CCcontract_info *cp,cJSON *params)
             if ( (inputsum= AddNormalinputs(mtx,mypk,amount+GAMES_TXFEE,64)) >= amount+GAMES_TXFEE )
             {
                 mtx.vout.push_back(MakeTokensCC1vout(cp->evalcode,amount,gamespk));
+                {
+                    char destaddr[64];
+                    GetScriptaddress(destaddr,mtx.vout[0].scriptPubKey);
+                    fprintf(stderr,"destaddr.(%s) %d\n",destaddr,cp->evalcode);
+                }
                 rawtx = FinalizeCCTx(0,cp,mtx,mypk,GAMES_TXFEE,opret);
                 return(games_rawtxresult(result,rawtx,1));
             }
