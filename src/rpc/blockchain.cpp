bc66f9f707cb6ae6bb91a64edea613206b07bb28
jl777 <jameslee777@yahoo.com>
2019-04-02 04:31:40 -1100

    Test

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1203,7 +1203,7 @@ UniValue prices(const UniValue& params, bool fHelp)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "illegal numpricefeeds");
     prices = (uint32_t *)calloc(sizeof(*prices),width*numpricefeeds);
     correlated = (int64_t *)calloc(sizeof(*correlated),width);
-    prices2 = (int64_t *)calloc(sizeof(*prices2),width);
+    prices2 = (uint32_t *)calloc(sizeof(*prices2),width);
     for (ht=nextheight-1,i=0; i<width&&ht>2*daywindow+2; i++,ht--)
     {
         if ( ht < 0 || ht > chainActive.Height() )
