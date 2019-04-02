f5354b767d07eaba696aa8167877bb659e1d9fa3
jl777 <jameslee777@yahoo.com>
2019-04-02 02:17:35 -1100

    Again

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1245,8 +1245,8 @@ UniValue prices(const UniValue& params, bool fHelp)
                 offset = j*width + i;
                 smoothed = komodo_pricesmoothed(&correlated[i],daywindow);
                 UniValue parr(UniValue::VARR);
-                parr.push_back(ValueFromAmount((int64_t)prices[offset]));
-                parr.push_back(ValueFromAmount(correlated[i]));
+                parr.push_back(ValueFromAmount((int64_t)prices[offset]*10000));
+                parr.push_back(ValueFromAmount(correlated[i]*ind<36?10000:1));
                 parr.push_back(ValueFromAmount(smoothed));
                 p.push_back(parr);
             }
