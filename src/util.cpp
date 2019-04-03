5579964235f20592857d93490b9b7b8678cb12eb
jl777 <jameslee777@yahoo.com>
2019-04-03 06:37:57 -1100

    Remove ,

--- src/util.cpp
+++ src/util.cpp
@@ -404,6 +404,10 @@ void SplitStr(const std::string& strVal, std::vector<std::string> &outVals)
     
     while ( ss >> str )
     {
+        if ( str.size() == 0 )
+            continue;
+        if ( str[str.size()-1] == ',' )
+            str.resize(str.size()-1);
         outVals.push_back(str);
         while ( ss.peek() == ' ' )
             ss.ignore();
