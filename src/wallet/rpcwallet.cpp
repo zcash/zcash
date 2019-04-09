0aba8374f1e7096ac672bb0e0d50ae185e40bc94
dimxy <dimxy@komodoplatform.com>
2019-04-09 21:40:23 +0500

    rpc height added

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -6931,8 +6931,8 @@ UniValue priceslist(const UniValue& params, bool fHelp)
 UniValue pricesinfo(const UniValue& params, bool fHelp)
 {
     uint256 bettxid; int32_t height;
-    if ( fHelp || params.size() != 1 )
-        throw runtime_error("pricesinfo fundingtxid\n");
+    if ( fHelp || params.size() != 2 )
+        throw runtime_error("pricesinfo fundingtxid height\n");
     if ( ensure_CCrequirements(EVAL_PRICES) < 0 )
         throw runtime_error("to use CC contracts, you need to launch daemon with valid -pubkey= for an address in your wallet\n");
     bettxid = Parseuint256((char *)params[0].get_str().c_str());
