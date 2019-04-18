84dfe684656d0955e5318908d6c7c27c0d952558
Carl Dong <accounts@carldong.me>
2019-04-18 13:49:11 -0400

    depends: Purge libtool archives
    We use pkg-config where we can, which generally replaces libtool at a
    higher level and does not have the same downsides as libtool. These
    archives sit in our depends tree with no purpose and pollute the final
    bitcoin build with massive overlinking.

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -29,4 +29,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
+  rm lib/*.la
 endef
--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -24,5 +24,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf bin share
+  rm -rf bin share lib/*.la
 endef
