b4ad109167a34a6bfcb3cfa89a53de9543cde2c4
jl777 <jameslee777@yahoo.com>
2019-04-18 21:59:31 -1100

    send_curl

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -2017,7 +2017,7 @@ int32_t get_stockprices(uint32_t now,uint32_t *prices,std::vector<std::string> s
 {
     char url[32768],*symbol,*timestr; cJSON *json,*obj; int32_t i,n=0,retval=-1; uint32_t uprice,timestamp;
     sprintf(url,"https://api.iextrading.com/1.0/tops/last?symbols=%s",GetArg("-ac_stocks","").c_str());
-    if ( (json= get_urljson(url)) != 0 ) //if ( (json= send_curl(url,(char *)"iex")) != 0 ) //
+    if ( (json= send_curl(url,(char *)"iex")) != 0 ) //if ( (json= get_urljson(url)) != 0 ) // //
     {
         if ( (n= cJSON_GetArraySize(json)) > 0 )
         {
@@ -2056,7 +2056,7 @@ uint32_t get_dailyfx(uint32_t *prices)
     //{"base":"USD","rates":{"BGN":1.74344803,"NZD":1.471652701,"ILS":3.6329113924,"RUB":65.1997682296,"CAD":1.3430201462,"USD":1.0,"PHP":52.8641469068,"CHF":0.9970582992,"AUD":1.4129078267,"JPY":110.6792654662,"TRY":5.6523444464,"HKD":7.8499732573,"MYR":4.0824567659,"HRK":6.6232840078,"CZK":22.9862720628,"IDR":14267.4986628633,"DKK":6.6551078624,"NOK":8.6806917454,"HUF":285.131039401,"GBP":0.7626582278,"MXN":19.4183455161,"THB":31.8702085933,"ISK":122.5708682475,"ZAR":14.7033339276,"BRL":3.9750401141,"SGD":1.3573720806,"PLN":3.8286682118,"INR":69.33187734,"KRW":1139.1602781244,"RON":4.2423783206,"CNY":6.7387234801,"SEK":9.3385630237,"EUR":0.8914244963},"date":"2019-03-28"}
     char url[512],*datestr; cJSON *json,*rates; int32_t i; uint32_t datenum=0,price = 0;
     sprintf(url,"https://api.openrates.io/latest?base=USD");
-    if ( (json= get_urljson(url)) != 0 ) //if ( (json= send_curl(url,(char *)"dailyfx")) != 0 )
+    if ( (json= send_curl(url,(char *)"dailyfx")) != 0 ) //if ( (json= get_urljson(url)) != 0 ) //
     {
         if ( (rates= jobj(json,(char *)"rates")) != 0 )
         {
