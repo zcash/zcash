9129c28d9056b0f0d453ac289352891e122e7e5f
jl777 <jameslee777@yahoo.com>
2019-04-18 22:01:07 -1100

    Test

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -2079,7 +2079,7 @@ uint32_t get_binanceprice(const char *symbol)
 {
     char url[512]; cJSON *json; uint32_t price = 0;
     sprintf(url,"https://api.binance.com/api/v1/ticker/price?symbol=%sBTC",symbol);
-    if ( (json= get_urljson(url)) != 0 ) //if ( (json= send_curl(url,(char *)"bnbprice")) != 0 )
+    if ( (json= send_curl(url,(char *)"bnbprice")) != 0 )//if ( (json= get_urljson(url)) != 0 ) //
     {
         price = jdouble(json,(char *)"price")*SATOSHIDEN + 0.0000000049;
         free_json(json);
