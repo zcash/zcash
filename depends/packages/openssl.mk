dc0c977906d2d3b8da343a0baf9bb263ef432a1c
Jack Grigg <jack@z.cash>
2019-05-22 11:03:42 +0000

    depends: Add FreeBSD support to OpenSSL

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -87,6 +87,8 @@ $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
+$(package)_config_opts_x86_64_freebsd=BSD-x86_64
+$(package)_config_opts_i686_freebsd=BSD-generic32
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
 endef
