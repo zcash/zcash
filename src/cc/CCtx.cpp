b550734e5a6cc77350d3c9bf1d3f0d1cbba2b327
dimxy <dimxy@komodoplatform.com>
2019-05-31 01:52:38 +0500

    changed to Getscriptaddr

--- src/cc/CCtx.cpp
+++ src/cc/CCtx.cpp
@@ -312,11 +312,12 @@ UniValue FinalizeCCTxExt(bool remote, uint64_t CCmask, struct CCcontract_info *c
                     {
                         // use vector of dest addresses and conds
                         for (auto t : cp->vintxconds) {
-                            //char coinaddr[64];
-                            //cond = t.wcond.get();
-                            //Getscriptaddress(coinaddr, CCPubKey(cond));
-                            if (strcmp(destaddr, t.coinaddr) == 0) {
-                                cond = t.wcond.get();
+                            char coinaddr[64];
+                            cond = t.wcond.get();
+                            Getscriptaddress(coinaddr, CCPubKey(cond));
+                            std::cerr << __func__ << " destaddr=" << destaddr << " coinaddr=" << coinaddr << " t.coinaddr=" << t.coinaddr << std::endl;
+                            if (strcmp(destaddr, coinaddr) == 0) {
+                                //cond = t.wcond.get();
                                 if (t.CCpriv[0])
                                     privkey = t.CCpriv;
                                 else
