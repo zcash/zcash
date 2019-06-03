cfabd1bc616b3d44c648967b547fc375e199ad50
adityapk <zcash@adityapk.com>
2019-06-03 12:40:54 -0700

    Win build

---
+++ mkwinrelease.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building Windows"
+make clean > /dev/null
+HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) >/dev/null
+strip src/ycashd.exe
+strip src/ycash-cli.exe
+cp src/ycashd.exe ../ycash/artifacts/
+cp src/ycash-cli.exe ../ycash/artifacts/
+
