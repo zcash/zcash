2ede72b27776c51c47a09029c11b1e223904a437
Larry Ruane <larry@z.cash>
2019-06-20 09:16:07 -0600

    copyright 2019

--- qa/rpc-tests/already_spent.py
+++ qa/rpc-tests/already_spent.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# Copyright (c) 2018 The Zcash developers
+# Copyright (c) 2019 The Zcash developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
