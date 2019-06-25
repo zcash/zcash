2a47986a0024931b13484365f7779a728db5489d
Jack Grigg <jack@z.cash>
2019-06-25 17:29:32 +0200

    Remove libgmp

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -34,7 +34,7 @@ rust_crates := \
   crate_winapi_x86_64_pc_windows_gnu
 rust_packages := rust $(rust_crates) librustzcash
 proton_packages := proton
-zcash_packages := libgmp libsodium
+zcash_packages := libsodium
 packages := boost openssl libevent zeromq $(zcash_packages) googletest
 native_packages := native_ccache
 
