0301d92db2156e6ab9f682915ce74ee37c6d6a2c
dimxy <dimxy@komodoplatform.com>
2019-07-04 11:01:37 +0500

    added loggin stake tx in MarmaraPosCheck

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -578,7 +578,8 @@ bool MarmaraPoScheck(char *destaddr, CScript inOpret, CTransaction staketx)  // 
         }
     }
     
-    LOGSTREAMFN("marmara", CCLOG_ERROR, stream  <<  "incorrect stake tx vout or opret, returning false, txid=" << staketx.GetHash().ToString() << " inOpret=" << inOpret.ToString() << std::endl);
+    LOGSTREAMFN("marmara", CCLOG_ERROR, stream  << "incorrect stake tx vout or opret, returning false, txid=" << staketx.GetHash().ToString() << " inOpret=" << inOpret.ToString() << std::endl);
+    LOGSTREAMFN("marmara", CCLOG_ERROR, stream << "bad stake tx=" << staketx.ToString() << std::endl);
     return(false);
 }
 
