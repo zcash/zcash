cd504f92a436f89e6e85c6a643ab38c1effba056
Kulcsár Béla Tibor <kulcsarbela@gmail.com>
2019-07-08 18:51:57 +0200

    modify fetch_params to handle accents and whitespaces in user folder

--- zcutil/fetch-params.bat
+++ zcutil/fetch-params.bat
@@ -1,28 +1,28 @@
 @echo off
 call :GET_CURRENT_DIR
 cd %THIS_DIR%
-IF NOT EXIST %APPDATA%\ZcashParams (
-MKDIR %APPDATA%\ZcashParams
+IF NOT EXIST "%APPDATA%"\ZcashParams (
+MKDIR "%APPDATA%"\ZcashParams
 )
-IF NOT EXIST %APPDATA%\ZcashParams\sprout-proving.key (
+IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-proving.key (
     ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-proving.key -O %APPDATA%\ZcashParams\sprout-proving.key
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-proving.key -O "%APPDATA%"\ZcashParams\sprout-proving.key
 )
-IF NOT EXIST %APPDATA%\ZcashParams\sprout-verifying.key (
+IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-verifying.key (
     ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-verifying.key -O %APPDATA%\ZcashParams\sprout-verifying.key
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-verifying.key -O "%APPDATA%"\ZcashParams\sprout-verifying.key
 )
-IF NOT EXIST %APPDATA%\ZcashParams\sapling-spend.params (
+IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-spend.params (
     ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-spend.params -O %APPDATA%\ZcashParams\sapling-spend.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-spend.params -O "%APPDATA%"\ZcashParams\sapling-spend.params
 )
-IF NOT EXIST %APPDATA%\ZcashParams\sapling-output.params (
+IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-output.params (
     ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-output.params -O %APPDATA%\ZcashParams\sapling-output.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-output.params -O "%APPDATA%"\ZcashParams\sapling-output.params
 )
-IF NOT EXIST %APPDATA%\ZcashParams\sprout-groth16.params (
+IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-groth16.params (
     ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-groth16.params -O %APPDATA%\ZcashParams\sprout-groth16.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-groth16.params -O "%APPDATA%"\ZcashParams\sprout-groth16.params
 )
 goto :EOF
 :GET_CURRENT_DIR
