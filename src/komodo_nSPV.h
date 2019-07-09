b75259a0e296a7fcfaf6fc8878dc7472c7012378
jl777 <jameslee777@yahoo.com>
2019-07-08 21:54:30 -1100

    vchSig

--- src/komodo_nSPV.h
+++ src/komodo_nSPV.h
@@ -435,7 +435,7 @@ bool NSPV_SignTx(CMutableTransaction &mtx,int32_t vini,int64_t utxovalue,const C
 
 int32_t NSPV_newnotariescount(CTransaction tx,uint8_t elected[64][33])
 {
-    CPubKey pubkeys[64]; uint8_t sig[512]; CScript scriptPubKeys[64]; CMutableTransaction mtx(tx); int32_t vini,j,siglen,numsigs = 0; char *str;
+    CPubKey pubkeys[64]; uint8_t sig[512]; CScript scriptPubKeys[64]; CMutableTransaction mtx(tx); int32_t vini,j,siglen,numsigs = 0; char *str; std::vector<uint8_t> vchSig;
     for (j=0; j<64; j++)
     {
         pubkeys[j] = buf2pk(elected[j]);
