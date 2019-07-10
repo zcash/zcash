15eb217c46f8294007cdd95f1ef514b1653698fe
jl777 <jameslee777@yahoo.com>
2019-07-10 02:32:13 -1100

    Tweak sleeps

--- src/komodo_nSPV.h
+++ src/komodo_nSPV.h
@@ -561,7 +561,7 @@ int32_t NSPV_notarizationextract(int32_t verifyntz,int32_t *ntzheightp,uint256 *
         GetOpReturnData(tx.vout[1].scriptPubKey,opret);
         if ( opret.size() >= 32*2+4 )
         {
-            sleep(1); // needed to avoid no pnodes error
+            //sleep(1); // needed to avoid no pnodes error
             *desttxidp = NSPV_opretextract(ntzheightp,blockhashp,symbol,opret,tx.GetHash());
             nTime = NSPV_blocktime(*ntzheightp);
             komodo_notaries(elected,*ntzheightp,nTime);
