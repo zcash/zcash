b41983c09c8dde782b88f4386c6f4f0a2ec05cf9
jl777 <jameslee777@yahoo.com>
2019-07-11 05:15:13 -1100

    Bypass verify

--- src/script/sign.cpp
+++ src/script/sign.cpp
@@ -447,6 +447,8 @@ bool ProduceSignature(const BaseSignatureCreator& creator, const CScript& fromPu
     
     sigdata.scriptSig = PushAll(result);
     // Test solution
+    if ( KOMODO_NSPV != 0 )
+        return(solved);
     return solved && VerifyScript(sigdata.scriptSig, fromPubKey, STANDARD_SCRIPT_VERIFY_FLAGS, creator.Checker(), consensusBranchId);
 }
 
