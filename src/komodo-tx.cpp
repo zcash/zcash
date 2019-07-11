18a58c1c946ca7d8d908796952e4e54fc2b4e9aa
jl777 <jameslee777@yahoo.com>
2019-07-10 22:25:41 -1100

    Declare

--- src/komodo-tx.cpp
+++ src/komodo-tx.cpp
@@ -47,6 +47,7 @@ using namespace std;
 
 #include "komodo_interest.h"
 
+CKey NSPV_key;
 uint64_t komodo_accrued_interest(int32_t *txheightp,uint32_t *locktimep,uint256 hash,int32_t n,int32_t checkheight,uint64_t checkvalue,int32_t tipheight)
 {
     return(0);
