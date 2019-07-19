9e55b41979287c2989a436a800f610cda23dd0b0
jl777 <jameslee777@yahoo.com>
2019-07-19 05:51:56 -1100

    -print

--- src/komodo_nSPV_fullnode.h
+++ src/komodo_nSPV_fullnode.h
@@ -513,14 +513,14 @@ void komodo_nSPVreq(CNode *pfrom,std::vector<uint8_t> request) // received a req
             pfrom->prevtimes[ind] = 0;
         if ( request[0] == NSPV_INFO ) // info
         {
-            fprintf(stderr,"check info %u vs %u, ind.%d\n",timestamp,pfrom->prevtimes[ind],ind);
+            //fprintf(stderr,"check info %u vs %u, ind.%d\n",timestamp,pfrom->prevtimes[ind],ind);
             if ( timestamp > pfrom->prevtimes[ind] )
             {
                 struct NSPV_inforesp I;
                 if ( len == 1+sizeof(reqheight) )
                     iguana_rwnum(0,&request[1],sizeof(reqheight),&reqheight);
                 else reqheight = 0;
-                fprintf(stderr,"request height.%d\n",reqheight);
+                //fprintf(stderr,"request height.%d\n",reqheight);
                 memset(&I,0,sizeof(I));
                 if ( (slen= NSPV_getinfo(&I,reqheight)) > 0 )
                 {
@@ -529,7 +529,7 @@ void komodo_nSPVreq(CNode *pfrom,std::vector<uint8_t> request) // received a req
                     //fprintf(stderr,"slen.%d\n",slen);
                     if ( NSPV_rwinforesp(1,&response[1],&I) == slen )
                     {
-                        fprintf(stderr,"send info resp to id %d\n",(int32_t)pfrom->id);
+                        //fprintf(stderr,"send info resp to id %d\n",(int32_t)pfrom->id);
                         pfrom->PushMessage("nSPV",response);
                         pfrom->prevtimes[ind] = timestamp;
                     }
