4f788e8e1f8df6180fd15e30509b844c919b0c12
Arjun <denverbdr@protonmail.com>
2019-07-30 10:07:26 -0700

    Disable tests for release builds

--- mkwinrelease.sh
+++ mkwinrelease.sh
@@ -2,7 +2,7 @@
 
 echo "Building Windows"
 make clean > /dev/null
-HOST=x86_64-w64-mingw32 ./zcutil/build.sh -j$(nproc) >/dev/null
+HOST=x86_64-w64-mingw32 ./zcutil/build.sh --disable-tests -j8 >/dev/null
 strip src/ycashd.exe
 strip src/ycash-cli.exe
 cp src/ycashd.exe ../ycash/artifacts/
