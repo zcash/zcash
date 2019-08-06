2f007290613dc5d40d3646ee770590fecea8c399
Eirik Ogilvie-Wigley <eirik@z.cash>
2019-08-05 19:05:11 -0600

    Add copyright info

--- src/consensus/params.cpp
+++ src/consensus/params.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 The Zcash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
 #include "params.h"
 
 #include "upgrades.h"
