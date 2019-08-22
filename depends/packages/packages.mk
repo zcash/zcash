be43fb5e98f883c52124dcbebd0808315f038168
Jack Grigg <jack@z.cash>
2019-08-22 18:11:09 +0100

    Revert "Remove insecurely-downloaded dependencies that we don't currently use."
    This partially reverts commit 86551dcb182f5179475f3872ad153300d2feb248
    to bring back native_cctools for cross-compiling macOS.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -72,3 +72,7 @@ packages := boost openssl libevent zeromq $(zcash_packages) googletest
 native_packages := native_ccache
 
 wallet_packages=bdb
+
+ifneq ($(build_os),darwin)
+darwin_native_packages=native_cctools
+endif
