f3d677694e0f7d135cd7b39df7785213cfeaca52
dimxy <dimxy@komodoplatform.com>
2019-09-22 14:40:43 +0500

    more logging

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -2378,7 +2378,7 @@ void komodo_cbopretupdate(int32_t forceflag)
     }
     pending = 1;
     now = (uint32_t)time(NULL);
-    std::cerr << __func__ << " " << "called, time=" << now  <<std::endl;
+    std::cerr << __func__ << " " << "called, time=" << now  << " forceflag=" << forceflag << std::endl;
     if ( (ASSETCHAINS_CBOPRET & 1) != 0 )
     {
 //if ( komodo_nextheight() > 333 ) // for debug only!
