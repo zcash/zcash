3420f0171eed6676f0650d0c14e6d6ca01ab8227
fanquake <fanquake@gmail.com>
2019-09-24 11:01:19 +0800

    build: only pass --disable-dependency-tracking to packages that understand it
    By blanket passing --disable-dependency-tracking to all depends packages
    we end up with some warnings like:
    
    configure: WARNING: unrecognized options: --disable-dependency-tracking
    
    So instead, only pass it to packages that understand it.
    
    Related to https://github.com/bitcoin/bitcoin/issues/16354.

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -10,7 +10,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress
+  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-dependency-tracking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_freebsd=--with-pic
--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -7,7 +7,7 @@ $(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d83
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf --disable-drafts
   $(package)_config_opts += --without-libgssapi_krb5 --without-pgm --without-norm --without-vmci
-  $(package)_config_opts += --disable-libunwind --disable-radix-tree --without-gcov
+  $(package)_config_opts += --disable-libunwind --disable-radix-tree --without-gcov --disable-dependency-tracking
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_freebsd=--with-pic
   $(package)_cxxflags=-std=c++11
