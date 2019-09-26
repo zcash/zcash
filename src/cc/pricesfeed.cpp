4bc028ec190edae2035bf60d6ac5d5563a551fc3
dimxy <dimxy@komodoplatform.com>
2019-09-26 14:32:35 +0500

    more delay logging

--- src/cc/pricesfeed.cpp
+++ src/cc/pricesfeed.cpp
@@ -510,7 +510,10 @@ static uint32_t poll_one_feed(const CFeedConfigItem &citem, uint32_t *pricevalue
             return 0;
         }
         // pause to prevent ban by the web resource
+        std::cerr << "before sleep" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cerr << "after sleep" << std::endl;
+
     }
     LOGSTREAM("prices", CCLOG_INFO, stream << std::endl);
     return numadded;
