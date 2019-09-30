9f55215a264e60460e9feb0ef1afc1c304cdd55c
dimxy <dimxy@komodoplatform.com>
2019-09-30 16:28:09 +0500

    logging in rpc

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1241,6 +1241,7 @@ UniValue prices(const UniValue& params, bool fHelp, const CPubKey& mypk)
         {
             if ( (n= komodo_heightpricebits(0,rawprices,ht)) > 0 )
             {
+                std::cerr << __func__ << "\t" << "n=" << n << " numprices=" << numprices << std::endl;
                 if ( n != numpricefeeds )
                     throw JSONRPCError(RPC_INVALID_PARAMETER, "numprices != first numprices");
                 else
