116c7bd4605fb7a4f7131f150db45a74e79006f3
dimxy <dimxy@komodoplatform.com>
2019-10-04 01:24:58 +0500

    includes

--- src/cc/priceslibs/cjsonpointer.cpp
+++ src/cc/priceslibs/cjsonpointer.cpp
@@ -1,4 +1,28 @@
+/******************************************************************************
+* Copyright © 2014-2019 The SuperNET Developers.                             *
+*                                                                            *
+* See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+* the top-level directory of this distribution for the individual copyright  *
+* holder information and the developer policies on copyright and licensing.  *
+*                                                                            *
+* Unless otherwise agreed in a custom licensing agreement, no part of the    *
+* SuperNET software, including this file may be copied, modified, propagated *
+* or distributed except according to the terms contained in the LICENSE file *
+*                                                                            *
+* Removal or modification of this copyright notice is prohibited.            *
+*                                                                            *
+******************************************************************************/
 
+// cjsonpointer.c  
+// C-language RFC 6901 JSON pointer implementation for cJSON parser
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <list>
+#include <vector>
+#include <functional>
+#include <ctype.h>
 #include "cjsonpointer.h"
 
 template <typename T>
