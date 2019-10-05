e3a01d6faf887622883149aa2eb0c4437e025747
Za Wilcox <zancas@protonmail.com>
2019-10-05 16:59:18 -0600

    make packer_build be parameterized
    parameterize packer_build.sh and ignore buildlog.txt

--- contrib/devtools/packer/packer_build.sh
+++ contrib/devtools/packer/packer_build.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
 
-docker run -v `pwd`:`pwd` -w `pwd` registry.gitlab.com/zingo-labs/zcash/packer:1.0 build \
-    -var 'aws_access_key_id={YOURAWSKEYID}' \
-    -var 'aws_secret_key={YOURAWSSECRET}' \
-    -var 'ssh_pubkey={YOURAWSPUBKEYFILE}' $@ | cut -d':' -f2 | xargs > built_image_id.txt
+docker run -v `pwd`:`pwd` -w `pwd` \
+    registry.gitlab.com/zingo-labs/zcash/packer:1.0 $1 \
+                                                    -var $2 \
+                                                    -var $3 \
+                                                    -var $4 \
+                                                    $5 > buildlog.txt
