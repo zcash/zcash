92d8b582c81bf16bd3b186016bab1695a28a6406
Za Wilcox <zancas@protonmail.com>
2019-10-05 22:08:58 -0600

    change per str4d's suggestions

--- contrib/hashicorp/packer/packer_build.sh
+++ contrib/hashicorp/packer/packer_build.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env sh
 
-docker run -v `pwd`:`pwd` -w `pwd` \
+PWD=$(pwd)
+docker run -v PWD:PWD -w PWD \
     registry.gitlab.com/zingo-labs/zcash/packer:1.0 $1 \
                                                     -var $2 \
                                                     -var $3 \
