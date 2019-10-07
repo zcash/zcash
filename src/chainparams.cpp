3b9237b88384ca3f9c1360bf606e10d9dde64e08
zebambam <bambam@z.cash>
2019-10-07 13:17:00 -0700

    Removed stale seeder, fixing #4153

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -143,7 +143,6 @@ public:
         vSeeds.clear();
         vSeeds.push_back(CDNSSeedData("z.cash", "dnsseed.z.cash")); // Zcash
         vSeeds.push_back(CDNSSeedData("str4d.xyz", "dnsseed.str4d.xyz")); // @str4d
-        vSeeds.push_back(CDNSSeedData("znodes.org", "dnsseed.znodes.org")); // @bitcartel
 
         // guarantees the first 2 characters, when base58 encoded, are "t1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0xB8};
