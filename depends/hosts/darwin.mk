d6c3af5e848636f599aeba255a54f84d43e3ffd8
Carl Dong <contact@carldong.me>
2019-10-09 20:10:00 -0400

    depends: Decouple toolchain + binutils
    For now they remain the same, but in the next commit, we will assign
    them differently according to wether or not we're using system clang.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -20,4 +20,5 @@ darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 darwin_debug_CFLAGS=-O1
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
+darwin_native_binutils=native_cctools
 darwin_native_toolchain=native_cctools
