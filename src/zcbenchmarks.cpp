f270979acb72216156cc888a21638a778e3f9ed8
Jack Grigg <jack@z.cash>
2019-10-20 19:11:02 +1300

    Migrate to librustzcash 0.2.0 API

--- src/zcbenchmarks.cpp
+++ src/zcbenchmarks.cpp
@@ -656,6 +656,10 @@ double benchmark_create_sapling_output()
     auto enc = res.get();
     auto encryptor = enc.second;
 
+    CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+    ss << address;
+    std::vector<unsigned char> addressBytes(ss.begin(), ss.end());
+
     auto ctx = librustzcash_sapling_proving_ctx_init();
 
     struct timeval tv_start;
@@ -665,8 +669,7 @@ double benchmark_create_sapling_output()
     bool result = librustzcash_sapling_output_proof(
         ctx,
         encryptor.get_esk().begin(),
-        note.d.data(),
-        note.pk_d.begin(),
+        addressBytes.data(),
         note.r.begin(),
         note.value(),
         odesc.cv.begin(),
