0eec09684327091a29cf8a674020ef89ea5c60b5
Alfredo Garcia <oxarbitrage@gmail.com>
2019-10-22 14:16:53 -0300

    Consensus: Decouple pow.cpp from util.h

--- src/pow.cpp
+++ src/pow.cpp
@@ -12,7 +12,6 @@
 #include "primitives/block.h"
 #include "streams.h"
 #include "uint256.h"
-#include "util.h"
 
 #include "sodium.h"
 
@@ -71,9 +70,7 @@ unsigned int CalculateNextWorkRequired(arith_uint256 bnAvg,
     // Limit adjustment step
     // Use medians to prevent time-warp attacks
     int64_t nActualTimespan = nLastBlockTime - nFirstBlockTime;
-    LogPrint("pow", "  nActualTimespan = %d  before dampening\n", nActualTimespan);
     nActualTimespan = averagingWindowTimespan + (nActualTimespan - averagingWindowTimespan)/4;
-    LogPrint("pow", "  nActualTimespan = %d  before bounds\n", nActualTimespan);
 
     if (nActualTimespan < minActualTimespan) {
         nActualTimespan = minActualTimespan;
@@ -91,12 +88,6 @@ unsigned int CalculateNextWorkRequired(arith_uint256 bnAvg,
     if (bnNew > bnPowLimit) {
         bnNew = bnPowLimit;
     }
-
-    /// debug print
-    LogPrint("pow", "GetNextWorkRequired RETARGET\n");
-    LogPrint("pow", "params.AveragingWindowTimespan(%d) = %d    nActualTimespan = %d\n", nextHeight, averagingWindowTimespan, nActualTimespan);
-    LogPrint("pow", "Current average: %08x  %s\n", bnAvg.GetCompact(), bnAvg.ToString());
-    LogPrint("pow", "After:  %08x  %s\n", bnNew.GetCompact(), bnNew.ToString());
 
     return bnNew.GetCompact();
 }
@@ -138,11 +129,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
-        return error("CheckProofOfWork(): nBits below minimum work");
+        return false;
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
-        return error("CheckProofOfWork(): hash doesn't match nBits");
+        return false;
 
     return true;
 }
