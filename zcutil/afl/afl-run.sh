f189a5f09d7c31180cf045f0f65f6297a2f56f7c
Taylor Hornby <taylor@defuse.ca>
2019-10-28 18:28:54 -0600

    Pass AFL input file path to zcashd

--- zcutil/afl/afl-run.sh
+++ zcutil/afl/afl-run.sh
@@ -6,4 +6,4 @@ AFL_INSTALL_DIR="$1"
 FUZZ_CASE="$2"
 shift 2
 
-"$AFL_INSTALL_DIR/afl-fuzz" -i "./src/fuzzing/$FUZZ_CASE/input" -o "./src/fuzzing/$FUZZ_CASE/output" "$@" ./src/zcashd
+"$AFL_INSTALL_DIR/afl-fuzz" -i "./src/fuzzing/$FUZZ_CASE/input" -o "./src/fuzzing/$FUZZ_CASE/output" "$@" ./src/zcashd @@
