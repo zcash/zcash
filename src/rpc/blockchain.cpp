0f5ef52d2e25e8d7504d802717d93074000b23e8
Alfredo Garcia <oxarbitrage@gmail.com>
2019-11-26 19:42:29 -0300

    add estimatedheight to getblockchaininfo
    Co-authored-by: Aditya Kulkarni <adityapk@gmail.com>

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -10,6 +10,7 @@
 #include "consensus/validation.h"
 #include "key_io.h"
 #include "main.h"
+#include "metrics.h"
 #include "primitives/transaction.h"
 #include "rpc/server.h"
 #include "streams.h"
@@ -987,6 +988,7 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp)
             "  \"bestblockhash\": \"...\", (string) the hash of the currently best block\n"
             "  \"difficulty\": xxxxxx,     (numeric) the current difficulty\n"
             "  \"verificationprogress\": xxxx, (numeric) estimate of verification progress [0..1]\n"
+            "  \"estimatedheight\": xxxx,  (numeric) if syncing, the estimated height of the chain, else the current best height\n"
             "  \"chainwork\": \"xxxx\"     (string) total amount of work in active chain, in hexadecimal\n"
             "  \"size_on_disk\": xxxxxx,       (numeric) the estimated size of the block and undo files on disk\n"
             "  \"commitments\": xxxxxx,    (numeric) the current number of note commitments in the commitment tree\n"
@@ -1033,6 +1035,11 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp)
     obj.push_back(Pair("chainwork",             chainActive.Tip()->nChainWork.GetHex()));
     obj.push_back(Pair("pruned",                fPruneMode));
     obj.push_back(Pair("size_on_disk",          CalculateCurrentUsage()));
+
+    if (IsInitialBlockDownload(Params()))
+        obj.push_back(Pair("estimatedheight",       EstimateNetHeight(Params().GetConsensus(), (int)chainActive.Height(), chainActive.Tip()->GetMedianTimePast())));
+    else
+        obj.push_back(Pair("estimatedheight",       (int)chainActive.Height()));
 
     SproutMerkleTree tree;
     pcoinsTip->GetSproutAnchorAt(pcoinsTip->GetBestAnchor(SPROUT), tree);
