2c8b85f612968bd7c9edfeedaa735da73739df84
mdr0id <marshall@z.cash>
2019-12-04 08:07:25 -0800

    Update to py3, ISSUE with encoding

--- qa/rpc-tests/p2p_txexpiringsoon.py
+++ qa/rpc-tests/p2p_txexpiringsoon.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2018 The Zcash developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://www.opensource.org/licenses/mit-license.php .
-
-import sys; assert sys.version_info < (3,), ur"This script does not run under Python 3. Please use Python 2.7.x."
 
 from test_framework.authproxy import JSONRPCException
 from test_framework.mininode import NodeConn, NetworkThread, CInv, \
@@ -19,7 +17,7 @@ from binascii import hexlify
 class TxExpiringSoonTest(BitcoinTestFramework):
 
     def setup_chain(self):
-        print "Initializing test directory " + self.options.tmpdir
+        print("Initializing test directory " + self.options.tmpdir)
         initialize_chain_clean(self.options.tmpdir, 3)
 
     def setup_network(self):
