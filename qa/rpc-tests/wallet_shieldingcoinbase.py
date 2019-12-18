e77e8660e3965b7b8886eb7d264701c46003096b
Jack Grigg <jack@z.cash>
2019-12-18 15:05:04 -0600

    test: Add missing sync_all point
    This file was renamed in master, so it was missed in the first commit.

--- qa/rpc-tests/wallet_shieldingcoinbase.py
+++ qa/rpc-tests/wallet_shieldingcoinbase.py
@@ -47,6 +47,7 @@ class WalletShieldingCoinbaseTest (BitcoinTestFramework):
         print "Mining blocks..."
 
         self.nodes[0].generate(4)
+        self.sync_all()
 
         walletinfo = self.nodes[0].getwalletinfo()
         assert_equal(walletinfo['immature_balance'], 40)
