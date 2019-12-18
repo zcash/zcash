e83700acff4e069d9f4aa18e0e8e110a971efda6
jl777 <jameslee777@yahoo.com>
2019-12-18 04:01:13 -1100

    Revert "+comments"
    This reverts commit 0114490c2b3afe4e97f7a4537459b1bcd659638e.

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -15,39 +15,19 @@
 
 // included from komodo_nSPV_superlite.h
 
-// quote: bid/ask vol, pubkey, timestamp, sig -> shorthash; cached for one hour
-
-// shortquote: +/- price, vol, elapsed, shorthash
-
-// subscription: order pair, recent best shortquotes
-
-// DEX state: subscriptions[]
-
-// incoming shortquotes[] -> send all unexpired quotes that is better than incoming[]
-// quote -> update best shortquotes, send quote to any neighbor that needs it
-
 int32_t komodo_DEXprocess(CNode *pfrom,std::vector<uint8_t> &response,uint8_t *msg,int32_t len) // incoming message
 {
     // update DEX state and set response and return nonzero if there is a response
-    
-    // price subscription -> return list of peers with that subscription along with bestprice
-    // price quote heartbeat with best known external pricequote/timestamp
-    // if any of our peers has a price subscription, mark external subscription
     return(0);
 }
 
 void komodo_DEXpoll(CNode *pto) // from SendMessages polling
 {
     // determine what message to send to the other node
-    // for each ptr-> price subscription or external subscription, check to see if we have a better or fresher price and send if we do
 }
 
 cJSON *komodo_DEXrpc(cJSON *jsonargs) // from cli
 {
-    if ( strncmp(ASSETCHAINS_SYMBOL,"DEX",3) == 0 )
-    {
-        // price subscription
-        // setprice
-    }
+    // update DEX state
 }
 
