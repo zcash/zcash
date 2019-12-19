5cfaacf8a47605cd5138e04768307ecfa15f486e
Taylor Hornby <taylor@defuse.ca>
2019-12-19 15:48:18 -0700

    fix more

--- depends/packages/crate_bit_vec.mk
+++ depends/packages/crate_bit_vec.mk
@@ -1,9 +1,9 @@
 package=crate_bit_vec
 $(package)_crate_name=bit-vec
-$(package)_version=0.6.1
+$(package)_version=0.4.4
 $(package)_download_path=https://static.crates.io/crates/$($(package)_crate_name)
 $(package)_file_name=$($(package)_crate_name)-$($(package)_version).crate
-$(package)_sha256_hash=a4523a10839ffae575fb08aa3423026c8cb4687eef43952afb956229d4f246f7
+$(package)_sha256_hash=02b4ff8b16e6076c3e14220b39fbc1fabb6737522281a388998046859400895f
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
