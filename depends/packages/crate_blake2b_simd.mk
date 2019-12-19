7423c9aa01455ecf694eb380ac71fb729b48ded4
Taylor Hornby <taylor@defuse.ca>
2019-12-19 15:41:15 -0700

    fix blake2b

--- depends/packages/crate_blake2b_simd.mk
+++ depends/packages/crate_blake2b_simd.mk
@@ -1,9 +1,9 @@
 package=crate_blake2b_simd
-$(package)_crate_name=blake2b-simd
+$(package)_crate_name=blake2b_simd
 $(package)_version=0.5.9
 $(package)_download_path=https://static.crates.io/crates/$($(package)_crate_name)
 $(package)_file_name=$($(package)_crate_name)-$($(package)_version).crate
-$(package)_sha256_hash=1677911cb9f1a232c0689929731d854a879ca597807f9972b2dcbe406a58682e
+$(package)_sha256_hash=b83b7baab1e671718d78204225800d6b170e648188ac7dc992e9d6bddf87d0c0
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
