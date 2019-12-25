94d70bc1683e8b02729ae0fcb1348e13109acc10
dimxy <dimxy@komodoplatform.com>
2019-12-26 00:12:38 +0500

    removed lock logging define

--- src/rpc/marmararpc.cpp
+++ src/rpc/marmararpc.cpp
@@ -12,7 +12,6 @@
  * Removal or modification of this copyright notice is prohibited.            *
  *                                                                            *
  ******************************************************************************/
-#define DEBUG_LOCKCONTENTION
 
 #include <stdint.h>
 #include <string.h>
