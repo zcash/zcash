95a312b0d7bc34899be969dc3cc52b8337921397
jl777 <jameslee777@yahoo.com>
2020-01-01 20:23:21 -1100

    Tweaks

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -337,7 +337,7 @@ int32_t komodo_DEXmodval(uint32_t now,int32_t modval,CNode *peer)
                     {
                         if ( relay >= 0 && relay <= KOMODO_DEX_RELAYDEPTH && now <= t+KOMODO_DEX_LOCALHEARTBEAT*KOMODO_DEX_MAXHOPS )
                         {
-                            //SETBIT(ptr->peermask,peerpos);
+                            SETBIT(ptr->peermask,peerpos);
                             packet.resize(ptr->datalen);
                             memcpy(&packet[0],ptr->data,ptr->datalen);
                             //fprintf(stderr,"send packet.%08x to peerpos.%d\n",Hashtables[modval][i],peerpos);
@@ -476,7 +476,7 @@ int32_t komodo_DEXprocess(uint32_t now,CNode *pfrom,uint8_t *msg,int32_t len)
             {
                 if ( (ind= komodo_DEXfind(openind,modval,h)) >= 0 && (ptr= Datablobs[modval][ind]) != 0 )
                 {
-                    if ( GETBIT(ptr->peermask,peerpos) == 0 )
+                    //if ( GETBIT(ptr->peermask,peerpos) == 0 )
                     {
                         //fprintf(stderr,"send packet.%08x to peerpos.%d\n",h,peerpos);
                         SETBIT(ptr->peermask,peerpos);
