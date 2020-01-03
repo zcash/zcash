5ce2c8aa73f58676f3bc4f635707ff3e999818b7
jl777 <jameslee777@yahoo.com>
2020-01-02 23:06:21 -1100

    Syntax

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -513,7 +513,7 @@ void komodo_DEXmsg(CNode *pfrom,std::vector<uint8_t> request) // received a pack
 
 void komodo_DEXbroadcast(char *hexstr)
 {
-    std::vector<uint8_t> packet; bits256 hash; uint8_t quote[16]; int32_t i,len,modval,iter,openind; uint32_t shorthash,timestamp;
+    std::vector<uint8_t> packet; bits256 hash; uint8_t quote[16]; int32_t i,ind,len,modval,iter,openind; uint32_t shorthash,timestamp;
     //for (iter=0; iter<2500; iter++)
     {
         len = (int32_t)(sizeof(quote)/sizeof(*quote));
