7b2188c33f0d656b7600c45cf0214f326e7c2d48
jl777 <jameslee777@yahoo.com>
2020-01-02 22:04:46 -1100

    Delay broadcast

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -511,7 +511,7 @@ void komodo_DEXbroadcast(char *hexstr)
     std::vector<uint8_t> packet; bits256 hash; uint8_t quote[16]; int32_t i,len,iter; uint32_t shorthash,timestamp;
     timestamp = (uint32_t)time(NULL);
     srand(timestamp);
-    for (iter=0; iter<1000; iter++)
+    for (iter=0; iter<100; iter++)
     {
         len = (int32_t)(sizeof(quote)/sizeof(*quote));
         for (i=0; i<len; i++)
@@ -526,5 +526,6 @@ void komodo_DEXbroadcast(char *hexstr)
             fprintf(stderr," ERROR issue order %08x %08x %s!\n",shorthash,hash.uints[1],bits256_str(str,hash));
         }
     }
+    sleep(1);
 }
 
