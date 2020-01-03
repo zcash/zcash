af12b3d2d0ad3a03c365c126f1de40e8f2f8c941
jl777 <jameslee777@yahoo.com>
2020-01-02 21:44:30 -1100

    -print

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -520,11 +520,10 @@ void komodo_DEXbroadcast(char *hexstr)
             fprintf(stderr,"%02x",quote[i]);
         }
         komodo_DEXgenquote(hash,shorthash,packet,timestamp,quote,len);
-        char str[65]; fprintf(stderr," issue order %08x %08x %s!\n",shorthash,hash.uints[1],bits256_str(str,hash));
         // need to queue this and dequeue in the DEXpoll loop, remove std::vector
         if ( komodo_DEXadd(-1,timestamp,timestamp % KOMODO_DEX_PURGETIME,hash,shorthash,&packet[0],packet.size()) >= 0 )
         {
-            //fprintf(stderr," issue order %08x %08x!\n",shorthash,hash.uints[1]);
+            char str[65]; fprintf(stderr," issue order %08x %08x %s!\n",shorthash,hash.uints[1],bits256_str(str,hash));
         }
     }
 }
