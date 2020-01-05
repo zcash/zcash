8a7737c8bf716008412e41ec65e26d82ad71926c
jl777 <jameslee777@yahoo.com>
2020-01-04 23:43:06 -1100

    Syntax

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -968,11 +968,11 @@ UniValue komodo_DEXlist(int32_t minpriority,char *tagA,char *tagB,char *destpub3
                 {
                     for (i=ptr->offset; i<ptr->datalen; i++)
                         fprintf(stderr,"%02x",ptr->data[i]);
-                    fprintf(stderr," %p %u\n",ptr,ptr->shorthash);
+                    fprintf(stderr," %p %u\n",ptr,ptr->hash.uints[0]);
                     // implement min/max priority filtering
                     ptr = ptr->prevs[ind];
                     n++;
-                    a.push_back((int64_t)ptr->shorthash);
+                    a.push_back((int64_t)ptr->hash.uints[0]);
                 }
                 obj.push_back(a);
                 obj.push_back(Pair((char *)"n",n));
