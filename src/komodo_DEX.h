0e9d3e22f6a7223c93b86b642c4d5d4cf52e0f6b
jl777 <jameslee777@yahoo.com>
2020-01-05 23:23:50 -1100

    Fix

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -991,7 +991,7 @@ UniValue komodo_DEXbroadcast(char *hexstr,int32_t priority,char *tagA,char *tagB
         timestamp = (uint32_t)time(NULL);
         modval = (timestamp % KOMODO_DEX_PURGETIME);
         if ( komodo_DEXgenquote(priority + komodo_DEX_sizepriority(len + datalen + sizeof(uint32_t)),hash,shorthash,packet,timestamp,quote,len,payload,datalen) != (int32_t)(len + datalen + sizeof(uint32_t)) )
-            fprintf(stderr,"unexpected packetsize != %ld\n",(len + datalen + sizeof(uint32_t));
+            fprintf(stderr,"unexpected packetsize != %ld\n",(len + datalen + sizeof(uint32_t)));
         if ( payload != 0 )
         {
             if ( payload != (uint8_t *)hexstr )
