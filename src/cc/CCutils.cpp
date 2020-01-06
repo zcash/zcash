df1817faa26bebd9435cd29635a8d93d82e2da82
jl777 <jameslee777@yahoo.com>
2020-01-06 04:37:55 -1100

    cipherlen

--- src/cc/CCutils.cpp
+++ src/cc/CCutils.cpp
@@ -1011,7 +1011,7 @@ uint8_t *SuperNET_deciphercalc(uint8_t **ptrp,int32_t *msglenp,bits256 privkey,u
     char str[65],z;
     for (z=0; z<32; z++)
         fprintf(stderr,"%02x",privkey.bytes[z]);
-    printf(" priv, use attached pubkey.(%s)\n",bits256_str(str,srcpubkey));
+    printf(" priv, use attached pubkey.(%s) origlen.%d\n",bits256_str(str,srcpubkey),cipherlen);
     cipher += sizeof(srcpubkey);
     cipherlen -= sizeof(srcpubkey);
     nonce = cipher;
@@ -1025,7 +1025,10 @@ uint8_t *SuperNET_deciphercalc(uint8_t **ptrp,int32_t *msglenp,bits256 privkey,u
         for (z=0; z<32; z++)
             fprintf(stderr,"%02x",privkey.bytes[z]);
         fprintf(stderr," priv, cipherlen.%d\n",cipherlen);
-    }
+        for (z=0; z<cipherlen; z++)
+            fprintf(stderr,"%02x",cipher[z]);
+        fprintf(stderr," cipher[]\n");
+   }
     if ( (retptr= _SuperNET_decipher(nonce,cipher,message,cipherlen,srcpubkey,privkey)) == 0 )
     {
         *msglenp = -1;
@@ -1070,10 +1073,13 @@ uint8_t *SuperNET_ciphercalc(uint8_t **ptrp,int32_t *cipherlenp,bits256 privkey,
     }
     _SuperNET_cipher(nonce,cipher,data,datalen,*destpubkeyp,privkey,buf);
     {
+        int32_t z;
         uint8_t message[8192];
+        for (z=0; z<datalen+crypto_box_ZEROBYTES; z++)
+            fprintf(stderr,"%02x",cipher[z]);
+        fprintf(stderr," cipher[]\n");
         if ( _SuperNET_decipher(nonce,cipher,message,datalen+crypto_box_ZEROBYTES,*destpubkeyp,privkey) != 0 )
         {
-            int32_t z;
             for (z=0; z<datalen; z++)
                 fprintf(stderr,"%02x",message[z+crypto_box_ZEROBYTES]);
             fprintf(stderr," deciphered.%d\n",z);
@@ -1097,12 +1103,6 @@ uint8_t *komodo_DEX_decrypt(uint8_t **allocatedp,uint8_t *data,int32_t *datalenp
 {
     uint8_t space[1024]; int32_t msglen;
     msglen = *datalenp;
-    {
-        int32_t i;
-        for (i=0; i<*datalenp; i++)
-            fprintf(stderr,"%02x",data[i]);
-        fprintf(stderr," <- decrypt.%d that\n",*datalenp);
-    }
     if ( (data= SuperNET_deciphercalc(allocatedp,&msglen,privkey,data,*datalenp,space,sizeof(space))) == 0 )
     {
         printf("komodo_DEX_decrypt decrytion error\n");
