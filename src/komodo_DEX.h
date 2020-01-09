3726fd5adfc4c8676e0456fdbe9ae74170ea5135
jl777 <jameslee777@yahoo.com>
2020-01-08 20:29:23 -1100

    1MB is 1<<20

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -73,7 +73,7 @@ void komodo_DEX_privkey(bits256 &priv0);
 #define KOMODO_DEX_MAXINDEX 64
 #define KOMODO_DEX_MAXINDICES 4 // [0] destpub, [1] tagA, [2] tagB, [3] two tags order dependent
 
-#define KOMODO_DEX_MAXPACKETSIZE (1 << 10)
+#define KOMODO_DEX_MAXPACKETSIZE (1 << 20)
 #define KOMODO_DEX_MAXPRIORITY 20 // a millionX should be enough, but can be as high as 64 - KOMODO_DEX_TXPOWBITS
 #define KOMODO_DEX_TXPOWBITS 1    // should be 17 for approx 1 sec per tx
 #define KOMODO_DEX_TXPOWDIVBITS 10 // each doubling of size of datalen, increases minpriority
