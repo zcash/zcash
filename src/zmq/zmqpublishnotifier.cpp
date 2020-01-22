c01c3cf3e593679228d87eb891f777cecc9dc501
DeckerSU <support@decker.su>
2020-01-22 05:30:38 +0300

    [zmq] Call va_end() on va_start()ed args.
    https://github.com/bitcoin/bitcoin/commit/5ba61f0034597ffeedc8c44af5a6ec670b095d83
    
    Issue: https://github.com/bitcoin/bitcoin/pull/10056

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -28,6 +28,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
         if (rc != 0)
         {
             zmqError("Unable to initialize ZMQ msg");
+            va_end(args);
             return -1;
         }
 
@@ -41,6 +42,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
         {
             zmqError("Unable to send ZMQ msg");
             zmq_msg_close(&msg);
+            va_end(args);
             return -1;
         }
 
@@ -51,6 +53,7 @@ static int zmq_send_multipart(void *sock, const void* data, size_t size, ...)
 
         size = va_arg(args, size_t);
     }
+    va_end(args);
     return 0;
 }
 
