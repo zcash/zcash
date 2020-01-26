c1e979b5c8b3b0c5ff70239d650b44c8cf8bdd1c
jl777 <jameslee777@yahoo.com>
2020-01-26 03:35:31 -1100

    +prints

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -2135,6 +2135,7 @@ UniValue komodo_DEXsubscribe(char *fname,int32_t priority,uint32_t shorthash)
                         continue;
                     t = locator >> 32;
                     h = locator & 0xffffffff;
+                    fprintf(stderr,"t.%u h.%08x\n",t,h);
                     pthread_mutex_lock(&DEX_globalmutex);
                     fragptr = komodo_DEXfind(t % KOMODO_DEX_PURGETIME,h);
                     pthread_mutex_unlock(&DEX_globalmutex);
@@ -2214,7 +2215,7 @@ UniValue komodo_DEXsubscribe(char *fname,int32_t priority,uint32_t shorthash)
 UniValue komodo_DEXpublish(char *fname,int32_t priority,int32_t streamsize)
 {
     static uint8_t locators[KOMODO_DEX_MAXPACKETSIZE];
-    UniValue result(UniValue::VOBJ); FILE *fp; uint64_t locator,volA; long fsize; int32_t i,rlen,len,n,numlocators=0; bits256 filehash; uint8_t buf[KOMODO_DEX_FILEBUFSIZE]; char bufstr[sizeof(buf)*2+1],pubkeystr[67],volAstr[16],volBstr[16],*hexstr;
+    UniValue result(UniValue::VOBJ); FILE *fp; uint64_t locator,volA; long fsize; int32_t i,rlen,len=0,n,numlocators=0; bits256 filehash; uint8_t buf[KOMODO_DEX_FILEBUFSIZE]; char bufstr[sizeof(buf)*2+1],pubkeystr[67],volAstr[16],volBstr[16],*hexstr;
     if ( strlen(fname) >= KOMODO_DEX_TAGSIZE )
     {
         result.push_back(Pair((char *)"result",(char *)"error"));
@@ -2273,7 +2274,7 @@ UniValue komodo_DEXpublish(char *fname,int32_t priority,int32_t streamsize)
                     bufstr[i<<1] = 0;
                     sprintf(volAstr,"%0.8f",dstr(volA));
                     komodo_DEXbroadcast(&locator,'Q',bufstr,KOMODO_DEX_VIPLEVEL,fname,(char *)"data",pubkeystr,volAstr,(char *)"");
-                    iguana_rwnum(1,&locators[1 + volA * sizeof(uint64_t)],sizeof(locator),&locator);
+                    len += iguana_rwnum(1,&locators[len],sizeof(locator),&locator);
                     numlocators++;
                 }
                 else
