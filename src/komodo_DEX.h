f23d0feaa9ece024b5078051a6bb6928996ce46c
jl777 <jameslee777@yahoo.com>
2020-02-07 06:01:20 -1100

    uint8_t *

--- src/komodo_DEX.h
+++ src/komodo_DEX.h
@@ -3021,7 +3021,7 @@ UniValue komodo_DEX_notarize(char *coin,int32_t prevheight)
                 for (height=ntzheight+1; height<ntzheight+1440; height++)
                 {
                     sprintf(tagB,"%d",height);
-                    if ( (_DEX_updatetips(tips,0,0,lenA,(uint8_t *)coin,(int8_t)strlen(tagB),tagB,0,0) >> 16) != 0 )
+                    if ( (_DEX_updatetips(tips,0,0,lenA,(uint8_t *)coin,(int8_t)strlen(tagB),(uint8_t *)tagB,0,0) >> 16) != 0 )
                         fprintf(stderr,"error getting tips for ht.%d ntzheight.%d\n",height,ntzheight);
                     if ( tips[ind] != 0 )
                     {
