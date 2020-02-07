b6cd764d54c75461971cd8832df3af9e6f56ad66
Pieter Wuille <pieter.wuille@gmail.com>
2020-02-06 19:57:32 -0800

    Get rid of VARINT default argument
    This removes the need for the GNU C++ extension of variadic macros.
    
    (cherry picked from commit 0e0fa27acb74b4f0075afcf59a0dff51a21baddb)

--- src/chain.h
+++ src/chain.h
@@ -491,14 +491,14 @@ public:
     inline void SerializationOp(Stream& s, Operation ser_action) {
         int nVersion = s.GetVersion();
         if (!(s.GetType() & SER_GETHASH))
-            READWRITE(VARINT(nVersion, VarIntMode::NONNEGATIVE_SIGNED));
+            READWRITE(VARINT_MODE(nVersion, VarIntMode::NONNEGATIVE_SIGNED));
         nClientVersion = nVersion;
 
-        READWRITE(VARINT(nHeight, VarIntMode::NONNEGATIVE_SIGNED));
+        READWRITE(VARINT_MODE(nHeight, VarIntMode::NONNEGATIVE_SIGNED));
         READWRITE(VARINT(nStatus));
         READWRITE(VARINT(nTx));
         if (nStatus & (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO))
-            READWRITE(VARINT(nFile, VarIntMode::NONNEGATIVE_SIGNED));
+            READWRITE(VARINT_MODE(nFile, VarIntMode::NONNEGATIVE_SIGNED));
         if (nStatus & BLOCK_HAVE_DATA)
             READWRITE(VARINT(nDataPos));
         if (nStatus & BLOCK_HAVE_UNDO)
