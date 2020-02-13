7f7141d97b982a9f3db471b82bdbd340a9172bdb
jl777 <jameslee777@yahoo.com>
2020-02-13 07:09:21 -1100

    Test

--- src/cc/dapps/subatomic.c
+++ src/cc/dapps/subatomic.c
@@ -492,14 +492,10 @@ int32_t subatomic_approved(struct msginfo *mp,cJSON *approval,cJSON *msgjson,cha
         }
         if ( get_createmultisig2(coin,acname,mp->msigaddr,mp->redeemscript,mp->alice.secp,mp->bob.secp) != 0 )
         {
-            /*
-             IF msig redeemscript CHECKMULTISIG
-             ELSE <expiration> CLTV DROP alice.secp CHECKSIG
-             ENDIF */
             subatomic_redeemscript(redeemscript,mp->locktime,mp->alice.secp,mp->bob.secp);
             if ( (decodejson= get_decodescript(coin,acname,redeemscript)) != 0 )
             {
-                fprintf(stderr,"%s %s msigaddr.%s %s -> %s %s\n",mp->bobflag!=0?"bob":"alice",mp->base.coin,mp->msigaddr,mp->redeemscript,redeemscript,jprint(decodejson,0));
+                fprintf(stderr,"%s %s msigaddr.%s %s -> %s %s\n",mp->bobflag!=0?"bob":"alice",(mp->bobflag != 0) ? mp->base.coin : mp->rel.coin,mp->msigaddr,mp->redeemscript,redeemscript,jprint(decodejson,0));
                 free(decodejson);
             }
         }
@@ -713,10 +709,10 @@ int32_t subatomic_channelapproved(uint32_t inboxid,char *senderpub,cJSON *msgjso
                 strcpy(mp->bob.recvZaddr,addr);
             if ( (addr= jstr(msgjson,"bobsecp")) != 0 )
                 strcpy(mp->bob.secp,addr);
-            retval = subatomic_approved(mp,approval,msgjson,senderpub);
+            retval = subatomic_opened(mp,approval,msgjson,senderpub);
         }
         else if ( mp->bobflag != 0 && mp->status == SUBATOMIC_APPROVED )
-            retval = subatomic_opened(mp,approval,msgjson,senderpub);
+            retval = 1;
     }
     return(retval);
 }
@@ -730,8 +726,8 @@ int32_t subatomic_incomingopened(uint32_t inboxid,char *senderpub,cJSON *msgjson
         fprintf(stderr,"%u iambob.%d (%s/%s) incomingchannel status.%d\n",mp->origid,mp->bobflag,mp->base.coin,mp->rel.coin,mp->status);
         if ( mp->bobflag == 0 && mp->status == SUBATOMIC_APPROVED )
             retval = subatomic_payment(mp,payment,msgjson,senderpub);
-        else if ( mp->bobflag != 0 && mp->status == SUBATOMIC_OPENED )
-            retval = 1; // nothing to do
+        else if ( mp->bobflag != 0 && mp->status == SUBATOMIC_APPROVED )
+            retval = subatomic_opened(mp,approval,msgjson,senderpub);
     }
     return(retval);
 }
