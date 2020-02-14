ec260a379989c7a6bd3fe0dcd5cb25d2ac9e79ad
jl777 <jameslee777@yahoo.com>
2020-02-14 03:16:47 -1100

    ;

--- src/cc/dapps/subatomic.c
+++ src/cc/dapps/subatomic.c
@@ -423,7 +423,7 @@ uint64_t subatomic_orderbook_mpset(struct msginfo *mp,char *basecheck)
             mp->base.maxamount = volA*SATOSHIDEN + 0.0000000049999;
             mp->rel.maxamount = volB*SATOSHIDEN + 0.0000000049999;
             if ( mp->rel.istoken == 0 )
-                txfee = mp->rel.txfeel
+                txfee = mp->rel.txfee;
             if ( mp->base.maxamount != 0 && mp->rel.maxamount != 0 && volA > SMALLVAL && volB > SMALLVAL && mp->rel.satoshis <= mp->rel.maxamount )
             {
                 mp->price = volA / volB;
