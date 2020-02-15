051f31652d7bc35f0b5e71dc7a53393621d5549a
jl777 <jameslee777@yahoo.com>
2020-02-15 04:17:12 -1100

    Price

--- src/cc/dapps/dappinc.h
+++ src/cc/dapps/dappinc.h
@@ -1379,9 +1379,9 @@ int32_t dpow_tokenregister(char *existing,int32_t priority,char *token_name,char
     return(filehash);
 }*/
 
-int32_t dpow_fileregister(char *existing,int32_t priority,char *fname,char *coin,char *pricestr)
+int32_t dpow_fileregister(char *existing,int32_t priority,char *fname,char *coin,int64_t price)
 {
-    FILE *fp; cJSON *retjson,*array,*item; bits256 existinghash,filehash; char tagA[16],str[65],*retstr,*pstr=0; int32_t i,n=0,len;
+    FILE *fp; cJSON *retjson,*array,*item; bits256 existinghash,filehash; char tagA[16],pricestr[32],str[65],*retstr,*pstr=0; int32_t i,n=0,len;
     existing[0] = 0;
     fprintf(stderr,"file register %s %s %s\n",fname,coin,pricestr);
     memset(&filehash,0,sizeof(filehash));
@@ -1412,6 +1412,7 @@ int32_t dpow_fileregister(char *existing,int32_t priority,char *fname,char *coin
     }
     if ( pstr == 0 )
     {
+        sprintf(pricestr,"%.8f",price);
         fprintf(stderr,"broadcast %s %s (%s %s)\n",bits256_str(str,filehash),fname,coin,pricestr);
         dpow_broadcast(priority,bits256_str(str,filehash),tagA,coin,DPOW_pubkeystr,"1",pricestr);
         return(1);
