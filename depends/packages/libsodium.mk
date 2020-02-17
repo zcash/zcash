4fe59691572d2ff5f41482e4b8f2351715735f1b
Jack Grigg <jack@z.cash>
2020-02-17 23:52:10 +0000

    Patch libsodium 1.0.15 pubkey validation onto 1.0.18

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -4,9 +4,11 @@ $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
 $(package)_dependencies=
+$(package)_patches=1.0.15-pubkey-validation.diff
 $(package)_config_opts=
 
 define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/1.0.15-pubkey-validation.diff && \
   cd $($(package)_build_subdir); ./autogen.sh
 endef
 
