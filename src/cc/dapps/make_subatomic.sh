5b209c5f6e13e8ff9cc6e1e100e59b6457ef30a6
ca333 <ca333@users.noreply.github.com>
2020-03-05 18:21:19 +0100

    mv subatomic bin

--- src/cc/dapps/make_subatomic.sh
+++ src/cc/dapps/make_subatomic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 g++-8 -c -std=c++17 -Wfatal-errors -Wall -Wextra -Werror subatomic_utils.cpp
-gcc-8 -Wall -Wextra -Wfatal-errors -o subatomic subatomic.c subatomic_utils.o -lstdc++ -lstdc++fs -lcurl -lm
+gcc-8 -Wall -Wextra -Wfatal-errors -o ../../subatomic subatomic.c subatomic_utils.o -lstdc++ -lstdc++fs -lcurl -lm
