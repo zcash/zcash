d7733f8b588d5058f5953659d503735d5e73ff8e
ca333 <ca333@keemail.me>
2020-03-06 15:22:20 +0100

    update notarizer build

--- src/cc/dapps/make_notarizer.sh
+++ src/cc/dapps/make_notarizer.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+echo "Building notarizer"
+mkdir -p ~/.dpow/bin 
 g++-8 -c -std=c++17 -Wfatal-errors -Wall -Wextra -Werror subatomic_utils.cpp
-gcc-8 -Wall -Wextra -Wfatal-errors -o notarizer notarizer.c subatomic_utils.o -lstdc++ -lstdc++fs -lcurl -lm
-# sudo mv notarizer /usr/bin/.
+gcc-8 -Wall -Wextra -Wfatal-errors -o ~/.dpow/bin/notarizer notarizer.c subatomic_utils.o -lstdc++ -lstdc++fs -lcurl -lm
+echo "export PATH=$PATH:~/.dpow/bin/" >> ~/.bashrc # new session required to become effective
+# run "export PATH=$PATH:~/.dpow/bin/" for immediate effect
+echo "Build finished"
