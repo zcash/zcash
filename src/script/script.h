0fc66eb6ddc8b9e08ca9383b35a18e9b2097d390
ca333 <ca333@keemail.me>
2020-03-06 19:25:35 +0100

    rm template matching and pseudo opcodes

--- src/script/script.h
+++ src/script/script.h
@@ -195,10 +195,6 @@ enum opcodetype
 
     // template matching params
     OP_SMALLDATA = 0xf9,
-    OP_SMALLINTEGER = 0xfa,
-    OP_PUBKEYS = 0xfb,
-    OP_PUBKEYHASH = 0xfd,
-    OP_PUBKEY = 0xfe,
     OP_CRYPTOCONDITION = 0xfc,
 
     OP_INVALIDOPCODE = 0xff,
