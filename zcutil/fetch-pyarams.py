4bd8ff30e652cd7b20c0d02224baaac2d8e9690f
mdr0id <marshall@z.cash>
2020-04-08 16:09:32 -0700

    Remove old params

--- zcutil/fetch-pyarams.py
+++ zcutil/fetch-pyarams.py
@@ -36,8 +36,6 @@ from subprocess import Popen, PIPE
 logging.basicConfig(format='%(asctime)s - PID:%(process)d - %(levelname)s: %(message)s', level=logging.INFO)
 
 PARAM_FILES = {
-    "sprout-proving.key" : "8bc20a7f013b2b58970cddd2e7ea028975c88ae7ceb9259a5344a16bc2c0eef7",
-    "sprout-verifying.key" : "4bd498dae0aacfd8e98dc306338d017d9c08dd0918ead18172bd0aec2fc5df82",
     "sapling-spend.params" : "8e48ffd23abb3a5fd9c5589204f32d9c31285a04b78096ba40a79b75677efc13",
     "sapling-output.params" : "2f0ebbcbb9bb0bcffe95a397e7eba89c29eb4dde6191c339db88570e3f3fb0e4",
     "sprout-groth16.params" : "b685d700c60328498fbde589c8c7c484c722b788b265b72af448a5bf0ee55b50"
