9eadd525a541c15ce75d1ead6e03171ae9f9a4aa
Sean Bowe <ewillbefull@gmail.com>
2020-04-22 10:43:50 -0600

    make-release.py: Versioning changes for 2.1.2.

--- src/clientversion.h
+++ src/clientversion.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 2
 #define CLIENT_VERSION_MINOR 1
 #define CLIENT_VERSION_REVISION 2
-#define CLIENT_VERSION_BUILD 25
+#define CLIENT_VERSION_BUILD 50
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
