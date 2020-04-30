945efd03053685d9b757fbe0cb3b65e57d30b867
fanquake <fanquake@gmail.com>
2020-04-30 16:40:25 +0800

    build: remove fdelt_chk backwards compatibility code
    Now that we require glibc 2.17 or later, we no longer need to check for
    different return types in fdelt_chk. It was changed from unsigned long
    int to long int in glibc 2.16 . See this commit:
    https://sourceware.org/git/?p=glibc.git;a=commit;h=ceb9e56b3d1f8c1922e0526c2e841373843460e2
    and related issue:
    https://sourceware.org/bugzilla/show_bug.cgi?id=14210.
    
    (cherry picked from commit bitcoin/bitcoin@8bf1540cc235fb8fb5330a7ae8ab638247ceb177)

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -9,22 +9,9 @@
 
 #include <cstddef>
 
-#if defined(HAVE_SYS_SELECT_H)
-#include <sys/select.h>
-#endif
-
 // Prior to GLIBC_2.14, memcpy was aliased to memmove.
 extern "C" void* memmove(void* a, const void* b, size_t c);
 extern "C" void* memcpy(void* a, const void* b, size_t c)
 {
     return memmove(a, b, c);
 }
-
-extern "C" void __chk_fail(void) __attribute__((__noreturn__));
-extern "C" FDELT_TYPE __fdelt_warn(FDELT_TYPE a)
-{
-    if (a >= FD_SETSIZE)
-        __chk_fail();
-    return a / __NFDBITS;
-}
-extern "C" FDELT_TYPE __fdelt_chk(FDELT_TYPE) __attribute__((weak, alias("__fdelt_warn")));
