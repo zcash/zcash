f505d64c5042241778655887b7e06542e8a45486
Alfredo Garcia <oxarbitrage@gmail.com>
2020-04-30 21:49:52 -0300

    add documentation and command line parsing to afl scripts

--- zcutil/afl/afl-run.sh
+++ zcutil/afl/afl-run.sh
@@ -2,8 +2,55 @@
 
 set -eu -o pipefail
 
-AFL_INSTALL_DIR="$1"
-FUZZ_CASE="$2"
-shift 2
+FUZZ_OPTIONS_STRING="Options are: CheckBlock, DecodeHexTx, DeserializeAddrMan, DeserializeTx or ReadFeeEstimates"
+
+required_options_count=0
+
+function help {
+    cat <<EOF
+Start fuzzing a case in a previously zcashd built for AFL.
+Additional arguments are passed-through to AFL.
+
+Usage:
+    $0 --afl-install=AFL_INSTALL_DIR --fuzz-case=FUZZ_CASE [ ARGUMENTS... ]
+
+    OPTIONS:
+        -f, --fuzz-case         $FUZZ_OPTIONS_STRING
+        -h, --help              Print this help message
+        -i, --afl-install       Directory where AFL is installed
+    EXAMPLE:
+        ./zcutil/afl/afl-run.sh -i /tmp/afl -f DecodeHexTx
+
+EOF
+}
+
+while (( "$#" )); do
+    case "$1" in
+        -f|--fuzz-case)
+            FUZZ_CASE=$2
+            ((++required_options_count))
+            shift 2
+        ;;
+        -i|--afl-install)
+            AFL_INSTALL_DIR=$2
+            ((++required_options_count))
+            shift 2
+        ;;
+        -h|--help)
+            help
+            exit 0
+        ;;
+        -*|--*=)
+            echo "Error: Unsupported flag $1" >&2
+            help
+            exit 1
+        ;;
+    esac
+done
+
+if ((required_options_count < 2)); then
+    help
+    exit 1
+fi
 
 "$AFL_INSTALL_DIR/afl-fuzz" -i "./src/fuzzing/$FUZZ_CASE/input" -o "./src/fuzzing/$FUZZ_CASE/output" "$@" ./src/zcashd @@
