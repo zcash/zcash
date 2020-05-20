df8f19b9329c07c92e440d9f9bc5409f3146c7af
Pieter Wuille <pieter.wuille@gmail.com>
2020-05-20 11:13:06 -0700

    Convert LimitedString to formatter
    (cherry picked from commit 92beff15d3ae2646c00bd78146d7592a7097ce9c)

--- src/serialize.h
+++ src/serialize.h
@@ -611,31 +611,23 @@ struct CompactSizeFormatter
 };
 
 template<size_t Limit>
-class LimitedString
+struct LimitedStringFormatter
 {
-protected:
-    std::string& string;
-public:
-    LimitedString(std::string& _string) : string(_string) {}
-
     template<typename Stream>
-    void Unserialize(Stream& s)
+    void Unser(Stream& s, std::string& v)
     {
         size_t size = ReadCompactSize(s);
         if (size > Limit) {
             throw std::ios_base::failure("String length limit exceeded");
         }
-        string.resize(size);
-        if (size != 0)
-            s.read((char*)&string[0], size);
+        v.resize(size);
+        if (size != 0) s.read((char*)v.data(), size);
     }
 
     template<typename Stream>
-    void Serialize(Stream& s) const
+    void Ser(Stream& s, const std::string& v)
     {
-        WriteCompactSize(s, string.size());
-        if (!string.empty())
-            s.write((char*)&string[0], string.size());
+        s << v;
     }
 };
 
