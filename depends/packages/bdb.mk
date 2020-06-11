19f641560fd724f317513910734018d403e4c96c
Alfredo Garcia <oxarbitrage@gmail.com>
2020-06-11 11:48:14 -0300

    install bdb binaries
    Co-authored-by: Jack Grigg <jack@z.cash>

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -32,5 +32,11 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install_lib install_include
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  cd $(BASEDIR)/../zcutil && \
+  mkdir -p bin && \
+  mv -f $($(package)_staging_dir)$(host_prefix)/bin/db_* bin
 endef
