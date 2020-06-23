9f71fa1c09692ae985ac49f29770fee62d0b0b99
Sean Bowe <ewillbefull@gmail.com>
2020-06-23 12:24:45 -0600

    Change to version of ed25519-zebra crate which is compliant with ZIP 215.

--- depends/packages/crate_ed25519_zebra.mk
+++ depends/packages/crate_ed25519_zebra.mk
@@ -1,13 +1,14 @@
 package=crate_ed25519_zebra
 $(package)_crate_name=ed25519-zebra
-$(package)_version=0.4.1
-$(package)_download_path=https://static.crates.io/crates/$($(package)_crate_name)
-$(package)_file_name=$($(package)_crate_name)-$($(package)_version).crate
-$(package)_sha256_hash=a76f15c88332faad36abb368aca89deb5cc4f440e5181c8848f8bdd049848f7b
+$(package)_download_path=https://github.com/ebfull/$($(package)_crate_name)/archive/
+$(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
+$(package)_download_file=$($(package)_git_commit).tar.gz
+$(package)_sha256_hash=e0b432e0161abe9cb27cf96dd42c1486d32297e2fdbf725b70e7ae1f5b53d052
+$(package)_git_commit=8c97acde89b6446f7f2505069fc7b9ac3a541417
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
-  $(call generate_crate_checksum,$(package))
+  $(call generate_unpackaged_crate_checksum,$(package))
 endef
 
 define $(package)_stage_cmds
