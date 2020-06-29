7dd1889944e51dccdbc45c0d5a8fe90daa311608
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2020-06-29 16:24:25 -0600

    Trivial copyright fix.
    Co-authored-by: str4d <thestr4d@gmail.com>

--- qa/rpc-tests/remove_sprout_shielding.py
+++ qa/rpc-tests/remove_sprout_shielding.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2019 The Zcash developers
+# Copyright (c) 2020 The Zcash developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://www.opensource.org/licenses/mit-license.php .
 
@@ -126,4 +126,3 @@ class RemoveSproutShieldingTest (BitcoinTestFramework):
 
 if __name__ == '__main__':
     RemoveSproutShieldingTest().main()
-
