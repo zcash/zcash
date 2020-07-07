ed4550eb7762069513fa8ffea82c9132b9f8500e
Jack Grigg <jack@z.cash>
2020-07-08 11:39:57 +1200

    Move GrothProof and SproutProof definitions into zcash/Proof.hpp

--- src/zcash/JoinSplit.hpp
+++ src/zcash/JoinSplit.hpp
@@ -15,17 +15,6 @@
 
 namespace libzcash {
 
-static constexpr size_t GROTH_PROOF_SIZE = (
-    48 + // π_A
-    96 + // π_B
-    48); // π_C
-
-typedef std::array<unsigned char, GROTH_PROOF_SIZE> GrothProof;
-// TODO: Because PHGRProof is listed first, using the default
-// constructor for JSDescription() will create a JSDescription
-// with a PHGRProof. The default however should be GrothProof.
-typedef boost::variant<PHGRProof, GrothProof> SproutProof;
-
 class JSInput {
 public:
     SproutWitness witness;
