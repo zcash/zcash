84e8cce77dca9d04aeb1f2fbaaa7566d478ab3d3
Kris Nuttycombe <kris@electriccoin.co>
2020-07-08 12:57:04 -0600

    Use ed25519-zebra from crates.io.

--- depends/packages/crate_ed25519_zebra.mk
+++ depends/packages/crate_ed25519_zebra.mk
@@ -1,14 +1,13 @@
 package=crate_ed25519_zebra
 $(package)_crate_name=ed25519-zebra
-$(package)_download_path=https://github.com/ebfull/$($(package)_crate_name)/archive/
-$(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
-$(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=e0b432e0161abe9cb27cf96dd42c1486d32297e2fdbf725b70e7ae1f5b53d052
-$(package)_git_commit=8c97acde89b6446f7f2505069fc7b9ac3a541417
+$(package)_version=2.0.0
+$(package)_download_path=https://static.crates.io/crates/$($(package)_crate_name)
+$(package)_file_name=$($(package)_crate_name)-$($(package)_version).crate
+$(package)_sha256_hash=beeba2b02b91dc7cc2d1f42c96b9e82db4cd20ad1c326f1dbf64ac8943c7bf32
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
-  $(call generate_unpackaged_crate_checksum,$(package))
+  $(call generate_crate_checksum,$(package))
 endef
 
 define $(package)_stage_cmds
