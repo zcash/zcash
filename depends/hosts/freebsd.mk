d306b72be4bbb836377ae1831580d7c0e8d91b86
Jack Grigg <jack@z.cash>
2020-07-16 18:20:51 +1200

    depends: Use vendored Clang for native compilation
    Clang is used for compiling C/C++ dependencies, as well as the Zcash
    binaries themselves. GCC is still required for compiling native
    toolchain dependencies (such as ccache).

--- depends/hosts/freebsd.mk
+++ depends/hosts/freebsd.mk
@@ -9,23 +9,7 @@ freebsd_debug_CXXFLAGS=$(freebsd_debug_CFLAGS)
 
 freebsd_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 
-ifeq (86,$(findstring 86,$(build_arch)))
-i686_freebsd_CC=gcc -m32
-i686_freebsd_CXX=g++ -m32
-i686_freebsd_AR=ar
-i686_freebsd_RANLIB=ranlib
-i686_freebsd_NM=nm
-i686_freebsd_STRIP=strip
-
-x86_64_freebsd_CC=gcc -m64
-x86_64_freebsd_CXX=g++ -m64
-x86_64_freebsd_AR=ar
-x86_64_freebsd_RANLIB=ranlib
-x86_64_freebsd_NM=nm
-x86_64_freebsd_STRIP=strip
-else
 i686_freebsd_CC=$(default_host_CC) -m32
 i686_freebsd_CXX=$(default_host_CXX) -m32
 x86_64_freebsd_CC=$(default_host_CC) -m64
 x86_64_freebsd_CXX=$(default_host_CXX) -m64
-endif
