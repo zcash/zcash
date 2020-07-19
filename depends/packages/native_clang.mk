05abfe71b5d60a6f73a47f3151466581b1a13372
Daira Hopwood <daira@jacaranda.org>
2020-07-19 23:10:13 +0100

    Back down to clang 9.0.1.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -1,13 +1,13 @@
 package=native_clang
-$(package)_major_version=10
-$(package)_version=10.0.0
+$(package)_major_version=9
+$(package)_version=9.0.1
 $(package)_download_path=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
-$(package)_download_file_linux=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-$(package)_file_name_linux=clang-llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-$(package)_sha256_hash_linux=b25f592a0c00686f03e3b7db68ca6dc87418f681f4ead4df4745a01d9be63843
+$(package)_download_file_linux=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+$(package)_file_name_linux=clang-llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+$(package)_sha256_hash_linux=1af280e96fec62acf5f3bb525e36baafe09f95f940dc9806e22809a83dfff4f8
 $(package)_download_file_darwin=clang+llvm-$($(package)_version)-x86_64-apple-darwin.tar.xz
 $(package)_file_name_darwin=clang-llvm-$($(package)_version)-x86_64-apple-darwin.tar.xz
-$(package)_sha256_hash_darwin=633a833396bf2276094c126b072d52b59aca6249e7ce8eae14c728016edb5e61
+$(package)_sha256_hash_darwin=2fdb7c04fefa4cc651a8b96a5d72311efe6b4be845a08c921166dc9883b0c5b9
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/bin && \
@@ -17,7 +17,6 @@ define $(package)_stage_cmds
   cp bin/llvm-objcopy $($(package)_staging_prefix_dir)/bin && \
   cp -P bin/clang $($(package)_staging_prefix_dir)/bin && \
   cp -P bin/clang++ $($(package)_staging_prefix_dir)/bin && \
-  cp -P bin/llvm-install-name-tool $($(package)_staging_prefix_dir)/bin && \
   cp -P bin/llvm-ranlib $($(package)_staging_prefix_dir)/bin && \
   cp -P bin/llvm-strip $($(package)_staging_prefix_dir)/bin && \
   mv include/ $($(package)_staging_prefix_dir) && \
