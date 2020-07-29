499d9c222ca0c318dba41103d6462a9c24a0cb3f
Daira Hopwood <daira@jacaranda.org>
2020-07-29 13:25:43 +0100

    zcutil/distclean.sh: remove BDB utility programs.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- zcutil/distclean.sh
+++ zcutil/distclean.sh
@@ -10,3 +10,6 @@ rm -rf depends/sources
 rm -rf afl-temp
 rm -rf src/fuzzing/*/output
 
+# These are not in clean.sh because they are only generated when building dependencies.
+rm -f zcutil/bin/db_*
+rmdir zcutil/bin 2>/dev/null
