70c518bb8456e9cc201b0a1512cd61c36ee901be
Jack Grigg <jack@z.cash>
2020-08-06 13:05:04 +0100

    depends: tracing-core 0.1.13

--- depends/packages/crate_tracing_core.mk
+++ depends/packages/crate_tracing_core.mk
@@ -1,9 +1,9 @@
 package=crate_tracing_core
 $(package)_crate_name=tracing-core
-$(package)_version=0.1.12
+$(package)_version=0.1.13
 $(package)_download_path=https://static.crates.io/crates/$($(package)_crate_name)
 $(package)_file_name=$($(package)_crate_name)-$($(package)_version).crate
-$(package)_sha256_hash=b2734b5a028fa697686f16c6d18c2c6a3c7e41513f9a213abb6754c4acb3c8d7
+$(package)_sha256_hash=d593f98af59ebc017c0648f0117525db358745a8894a8d684e185ba3f45954f9
 $(package)_crate_versioned_name=$($(package)_crate_name)
 
 define $(package)_preprocess_cmds
