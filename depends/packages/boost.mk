44f95d404394dabe315b3a656a511bdc4622288f
Carl Dong <contact@carldong.me>
2020-08-08 17:30:59 -0400

    depends: boost: Disable all compression

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -13,7 +13,7 @@ define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
 $(package)_config_opts=--layout=system --user-config=user-config.jam
-$(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
+$(package)_config_opts+=threading=multi link=static -sNO_COMPRESSION=1
 $(package)_config_opts_linux=target-os=linux threadapi=pthread runtime-link=shared
 $(package)_config_opts_freebsd=cxxflags=-fPIC
 $(package)_config_opts_darwin=target-os=darwin runtime-link=shared
