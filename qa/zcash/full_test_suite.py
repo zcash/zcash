57a0418fbb000975231df9fa1fd91283c5ad7f9c
Daira Hopwood <daira@jacaranda.org>
2020-08-14 17:58:38 +0100

    Repair full_test_suite.py for new checksec.sh.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- qa/zcash/full_test_suite.py
+++ qa/zcash/full_test_suite.py
@@ -32,7 +32,7 @@ RE_FORTIFY_USED = re.compile('Binary compiled with FORTIFY_SOURCE support.*Yes')
 
 def test_rpath_runpath(filename):
     output = subprocess.check_output(
-        [repofile('qa/zcash/checksec.sh'), '--file', repofile(filename)]
+        [repofile('qa/zcash/checksec.sh'), '--file=' + repofile(filename)]
     )
     if RE_RPATH_RUNPATH.search(output.decode('utf-8')):
         print('PASS: %s has no RPATH or RUNPATH.' % filename)
@@ -44,7 +44,7 @@ def test_rpath_runpath(filename):
 
 def test_fortify_source(filename):
     proc = subprocess.Popen(
-        [repofile('qa/zcash/checksec.sh'), '--fortify-file', repofile(filename)],
+        [repofile('qa/zcash/checksec.sh'), '--fortify-file=' + repofile(filename)],
         stdout=subprocess.PIPE,
     )
     line1 = proc.stdout.readline()
