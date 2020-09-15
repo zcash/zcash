a8da36185deaaa69fbb5721148a17041002582b5
ca333 <ca333@users.noreply.github.com>
2020-09-15 13:54:28 +0200

    default deactivate nspv proc

--- src/main.h
+++ src/main.h
@@ -120,6 +120,9 @@ static const unsigned int DATABASE_FLUSH_INTERVAL = 24 * 60 * 60;
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
 static const int64_t DEFAULT_MAX_TIP_AGE = 24 * 60 * 60;
 
+/** Default NSPV support enabled */
+static const bool DEFAULT_NSPV_PROCESSING = false;
+
 //static const bool DEFAULT_ADDRESSINDEX = false;
 //static const bool DEFAULT_SPENTINDEX = false;
 #define DEFAULT_ADDRESSINDEX (GetArg("-ac_cc",0) != 0 || GetArg("-ac_ccactivate",0) != 0)
