eeb6c4e59bed99ef1d97548cfb53d063f2084a7a
Alfredo Garcia <oxarbitrage@gmail.com>
2020-09-18 11:56:13 -0300

    fix docs
    Co-authored-by: Daira Hopwood <daira@jacaranda.org>

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3411,19 +3411,19 @@ UniValue z_listreceivedbyaddress(const UniValue& params, bool fHelp)
     if (!EnsureWalletIsAvailable(fHelp))
         return NullUniValue;
 
-    if (fHelp || params.size()==0 || params.size() >5)
+    if (fHelp || params.size() == 0 || params.size() > 4)
         throw runtime_error(
-            "z_listreceivedbyaddress \"address\" ( minconf )\n"
+            "z_listreceivedbyaddress \"address\" ( minconf timestamp limit )\n"
             "\nReturn a list of amounts received by a zaddr belonging to the node's wallet.\n"
             "\nArguments:\n"
             "1. \"address\"       (string) The private address.\n"
             "2. minconf         (numeric, optional, default=1) Only include transactions confirmed at least this many times.\n"
             "3. timestamp       (numeric, optional, default=0) Start getting results after this date as a Unix timestamp in nanoseconds\n"
-            "4. limit           (numeric, optional, default=0=all available entries) Number of results to get in response.\n"
+            "4. limit           (numeric, optional, default=0) Number of results to get in response. 0 means all available entries.\n"
             "\nResult:\n"
             "{\n"
             "  \"txid\": \"txid\",           (string) the transaction id\n"
-            "  \"timestamp\": xxx,           (numeric) local time of when the transaction was first inserted to wallet in nanoseconds since epoch (midnight Jan 1 1970 GMT).\n"
+            "  \"timestamp\": xxx,           (numeric) time when the transaction was first added to the wallet in Unix UTC nanoseconds since epoch (midnight Jan 1 1970).\n"
             "  \"amount\": xxxxx,         (numeric) the amount of value in the note\n"
             "  \"amountZat\" : xxxx       (numeric) The amount in " + MINOR_CURRENCY_UNIT + "\n"
             "  \"memo\": xxxxx,           (string) hexadecimal string representation of memo field\n"
