b7ce6a0d032609b8551ef5907694c86f75cc5304
Nate Wilcox <nathan+dev@electriccoin.co>
2020-09-22 07:36:18 +0000

    Pin the pkgs tarball by hash to remove vulnerability to network modification.

--- zcutil/nix-builder/pkgs-pinned.nix
+++ zcutil/nix-builder/pkgs-pinned.nix
@@ -1,7 +1,8 @@
 let
   # channelrev taken from https://status.nixos.org/ for nixos-20.09
-  channelrev = "61525137fd1002f6f2a5eb0ea27d480713362cd5";
+  rev = "61525137fd1002f6f2a5eb0ea27d480713362cd5";
+  sha256 = "1gzwz9jvfcf0is6zma7qlgszkngfb2aa4kam0nhs0qnwb4nqn7mg";
 
-  channelurl = "https://github.com/NixOS/nixpkgs-channels/archive/${channelrev}.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz";
 in
-  import (fetchTarball channelurl) {}
+  import (fetchTarball { inherit url sha256; }) {}
