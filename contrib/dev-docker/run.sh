1909bb0537daacba0c298470cd3b584ec5ac5369
Nate Wilcox <nathan+dev@electriccoin.co>
2020-09-23 21:39:17 +0000

    Make sure the build has the same UID as the host.

--- contrib/dev-docker/run.sh
+++ contrib/dev-docker/run.sh
@@ -3,5 +3,12 @@ set -efuxo pipefail
 
 # cd to the repo root:
 cd "$(readlink -f "$0/../../../")"
-docker build -t zcash-dev-docker ./contrib/dev-docker/
-exec docker run -v "$(pwd)/:/workspace" zcash-dev-docker "$@"
+docker build \
+    -t zcash-dev-docker \
+    --build-arg "BUILDER_UID=$(id -u)" \
+    ./contrib/dev-docker/
+
+exec docker run \
+    -v "$(pwd)/:/workspace" \
+    zcash-dev-docker \
+    "$@"
