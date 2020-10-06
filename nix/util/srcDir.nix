5068ed279144a46db23753d1b9928babcf27dd63
Nate Wilcox <nathan+dev@electriccoin.co>
2020-10-06 15:13:57 +0900

    Start filtering the source so that zcash is not rebuilt when certain paths are modified.

--- nix/util/srcDir.nix
+++ nix/util/srcDir.nix
@@ -1,3 +1,17 @@
-# Parent of util is nix.
-# Parent of nix is srcDir:
-./../..
+let
+  inherit (builtins) any filterSource;
+
+  # Parent of util is nix.
+  # Parent of nix is srcDir:
+  rawSrcDir = ../..;
+
+  skipPaths = [
+    ".git"
+    "nix"
+  ];
+
+  selectPath = path: kind:
+    !(any (skipPath: path == rawSrcDir + "/${skipPath}") skipPaths);
+
+in
+  filterSource selectPath rawSrcDir
