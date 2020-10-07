81414e139ea0d7a539fb89c8a5cb8d898084ec32
Nate Wilcox <nathan+dev@electriccoin.co>
2020-10-07 13:22:29 +0900

    Fix the ./depends consistency parser for `native_rust.mk`.

--- nix/packages/parser/parseDependsPackages.nix
+++ nix/packages/parser/parseDependsPackages.nix
@@ -2,7 +2,7 @@ let
   dependsPackagesDir = ./../../../depends/packages;
 
   inherit (builtins) mapAttrs removeAttrs;
-  inherit (import ../../util) readDirBySuffix;
+  inherit (import ../../util) readDirBySuffix nixpkgs;
 
   makeFiles = readDirBySuffix ".mk" dependsPackagesDir;
 
@@ -11,12 +11,14 @@ let
 
   parseMakeFileHash = name: path:
     let
-      inherit (builtins) head length match readFile;
+      inherit (builtins) head length match readFile tail;
+      kernel = nixpkgs.buildPlatform.parsed.kernel.name;
+
       text = readFile path;
-      matches = match ".*\n[^\n]*_sha256_hash=([^\n]*)\n.*" text;
+      matches = match ".*\n[^\n]*_sha256_hash(_${kernel})?=([a-f0-9]*)\n.*" text;
     in
-      assert length matches == 1;
-      head matches;
+      assert length matches == 2;
+      head (tail matches);
 
 in
   mapAttrs parseMakeFileHash pkgFiles
