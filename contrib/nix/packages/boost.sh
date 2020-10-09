f00e924f9abf5c5d135643ac97f0c4656e9d1246
Nate Wilcox <nathan+dev@electriccoin.co>
2020-10-09 03:21:09 +0000

    Fix a few broken bits in the boost build script.

--- contrib/nix/packages/boost.sh
+++ contrib/nix/packages/boost.sh
@@ -2,13 +2,13 @@ source "$stdenv/setup"
 set -efuo pipefail
 
 tar -xf "$src"
-cd "$(echo "$archive" | sed 's,\..*$,,')"
+cd "$(ls | grep -v 'env-vars')"
 
 : ./depends Preprocessing
 for patch in $patches
 do
   echo "Applying patch: $patch"
-  patch -p1 < "$patch"
+  patch -p2 < "$patch"
 done
 
 : ./depends Configuring
@@ -18,7 +18,7 @@ done
 #sed -i -e "s|using gcc ;|using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;|" project-config.jam
 
 : ./depends Building
-./b2 -d2 -j2 -d1 --prefix="$out" $configureFlags CXXFLAGS="$cxxFlags" stage
+./b2 -d2 -j2 -d1 --prefix="$out" $configureFlags cxxflags="$cxxFlags" stage
 
 : ./depends Staging
 ./b2 -d0 -j4 --prefix="$out" $configureFlags install
