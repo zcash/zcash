4adbcbd26543be782b1c29663d5373da98639820
Nate Wilcox <nathan+dev@electriccoin.co>
2020-10-12 14:31:50 +0900

    Change everything from a multi-output derivation to a single symlinked output.

--- contrib/nix/everything.nix
+++ contrib/nix/everything.nix
@@ -1,9 +1,12 @@
 {
   allowInconsistency ? false
 }:
-{
-  zcash = import ./zcash.nix { inherit allowInconsistency; };
-  sources = import ./sources;
-  meta = import ./meta;
-  version = (import ./version.nix).derivation;
-}
+  let
+    inherit (import ./util) mkLinkDerivation;
+  in
+    mkLinkDerivation "everything" [
+      (import ./zcash.nix { inherit allowInconsistency; })
+      (import ./sources)
+      (import ./meta)
+      ((import ./version.nix).derivation)
+    ]
