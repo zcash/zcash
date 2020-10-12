0887f64c90885fac22c24548c82666c352292a46
Jack Grigg <jack@z.cash>
2020-10-12 20:25:24 +0100

    depends: Don't replace default CXXFLAGS in C++ dependencies

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -11,7 +11,7 @@ define $(package)_set_vars
   $(package)_config_opts += --disable-drafts --enable-option-checking
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_freebsd=--with-pic
-  $(package)_cxxflags=-std=c++11
+  $(package)_cxxflags+=-std=c++11
 endef
 
 define $(package)_config_cmds
