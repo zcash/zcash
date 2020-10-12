fa54a1661a70724772f47add88ece12432cafc39
Jack Grigg <jack@z.cash>
2020-10-12 20:47:47 +0100

    depends: Add multilib paths for Linux cross-compile

--- depends/hosts/linux.mk
+++ depends/hosts/linux.mk
@@ -15,3 +15,11 @@ i686_linux_CC=$(default_host_CC) -m32
 i686_linux_CXX=$(default_host_CXX) -m32
 x86_64_linux_CC=$(default_host_CC) -m64
 x86_64_linux_CXX=$(default_host_CXX) -m64
+
+# Clang doesn't appear to find these multilib paths by default,
+# so help it out if we are cross-compiling.
+ifneq ($(canonical_host),$(build))
+  # CFLAGS is copied to CXXFLAGS after it is fully-evaluated.
+  linux_CFLAGS += -idirafter /usr/$(host)/include
+  linux_LDFLAGS += -L/usr/$(host)/lib
+endif
