38bbf0b16306f56e22fe434b7c5294a984928f93
Nate Wilcox <nathan+dev@electriccoin.co>
2020-10-12 08:04:44 +0000

    Include the final paths required for successful build.

--- contrib/nix/sources/zcashFilteredSource.nix
+++ contrib/nix/sources/zcashFilteredSource.nix
@@ -1,12 +1,19 @@
 let
   inherit (import ../util) selectSource;
 in selectSource "zcash-filtered-source" "." [
+  ".cargo"
+  "Cargo.lock"
+  "Cargo.toml"
+  "Makefile.am"
   "autogen.sh"
   "build-aux"
   "configure.ac"
   "doc"
   "libzcashconsensus.pc.in"
-  "Makefile.am"
   "qa/pull-tester"
+  "share/genbuild.sh"
   "src"
+  "zcutil/build-debian-package.sh"
+  "zcutil/build.sh"
+  "zcutil/fetch-params.sh"
 ]
