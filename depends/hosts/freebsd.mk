eaed8d57974afcf51136fc1a99789e4768779e06
str4d <thestr4d@gmail.com>
2020-10-19 20:19:00 +0100

    depends: Add untested note to FreeBSD host
    Co-authored-by: Daira Hopwood <daira@jacaranda.org>

--- depends/hosts/freebsd.mk
+++ depends/hosts/freebsd.mk
@@ -9,6 +9,9 @@ freebsd_debug_CXXFLAGS=$(freebsd_debug_CFLAGS)
 
 freebsd_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 
+# Changes below have not been tested. If you try to build on FreeBSD,
+# please let us know how it goes.
+
 freebsd_LDFLAGS?=-fuse-ld=lld
 
 i686_freebsd_CC=$(default_host_CC) -m32
