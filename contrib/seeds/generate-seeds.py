hebasto/bitcoin-qtw

Explicitly specify encoding when opening text files in Python code

contrib/seeds/generate-seeds.py
@@ -127,10 +127,10 @@ def main():
     g.write(' * Each line contains a 16-byte IPv6 address and a port.\n')
     g.write(' * IPv4 as well as onion addresses are wrapped inside an IPv6 address accordingly.\n')
     g.write(' */\n')
-    with open(os.path.join(indir,'nodes_main.txt'),'r') as f:
+    with open(os.path.join(indir,'nodes_main.txt'), 'r', encoding="utf8") as f:
         process_nodes(g, f, 'pnSeed6_main', 8333)
     g.write('\n')
-    with open(os.path.join(indir,'nodes_test.txt'),'r') as f:
+    with open(os.path.join(indir,'nodes_test.txt'), 'r', encoding="utf8") as f:
         process_nodes(g, f, 'pnSeed6_test', 18333)
     g.write('#endif // BITCOIN_CHAINPARAMSSEEDS_H\n')
 

zcash/zcash

contrib/seeds/generate-seeds.py
@@ -126,10 +126,10 @@ def main():
     g.write(' * Each line contains a 16-byte IPv6 address and a port.\n')
     g.write(' * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.\n')
     g.write(' */\n')
-    with open(os.path.join(indir,'nodes_main.txt'),'r') as f:
+    with open(os.path.join(indir,'nodes_main.txt'),'r', encoding="utf8") as f:
         process_nodes(g, f, 'pnSeed6_main', 8233)
     g.write('\n')
-    with open(os.path.join(indir,'nodes_test.txt'),'r') as f:
+    with open(os.path.join(indir,'nodes_test.txt'),'r', encoding="utf8") as f:
         process_nodes(g, f, 'pnSeed6_test', 18233)
     g.write('#endif // BITCOIN_CHAINPARAMSSEEDS_H\n')
             