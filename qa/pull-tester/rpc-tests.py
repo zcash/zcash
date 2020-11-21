tmagik/catoshi

Reorder RPC tests for running time

qa/pull-tester/rpc-tests.py
@@ -100,78 +100,87 @@ if ENABLE_ZMQ:
 
 testScripts = [
     # longest test should go first, to favor running tests in parallel
-    'p2p-fullblocktest.py',
+    'wallet-hd.py',
     'walletbackup.py',
-    'bip68-112-113-p2p.py',
+    # vv Tests less than 5m vv
+    'p2p-fullblocktest.py',
+    'fundrawtransaction.py',
+    'p2p-compactblocks.py',
+    'segwit.py',
+    # vv Tests less than 2m vv
     'wallet.py',
     'wallet-accounts.py',
-    'wallet-hd.py',
+    'p2p-segwit.py',
     'wallet-dump.py',
     'listtransactions.py',
+    # vv Tests less than 60s vv
+    'sendheaders.py',
+    'zapwallettxes.py',
+    'importmulti.py',
+    'mempool_limit.py',
+    'merkle_blocks.py',
     'receivedby.py',
+    'abandonconflict.py',
+    'bip68-112-113-p2p.py',
+    'rawtransactions.py',
+    'reindex.py',
+    # vv Tests less than 30s vv
     'mempool_resurrect_test.py',
     'txn_doublespend.py --mineblock',
-    'p2p-segwit.py',
-    'segwit.py',
     'txn_clone.py',
     'getchaintips.py',
-    'rawtransactions.py',
     'rest.py',
     'mempool_spendcoinbase.py',
     'mempool_reorg.py',
-    'mempool_limit.py',
     'httpbasics.py',
     'multi_rpc.py',
-    'zapwallettxes.py',
     'proxy_test.py',
-    'merkle_blocks.py',
-    'fundrawtransaction.py',
     'signrawtransactions.py',
     'nodehandling.py',
-    'reindex.py',
     'decodescript.py',
     'blockchain.py',
     'disablewallet.py',
-    'sendheaders.py',
     'keypool.py',
     'p2p-mempool.py',
     'prioritise_transaction.py',
     'invalidblockrequest.py',
     'invalidtxrequest.py',
-    'abandonconflict.py',
     'p2p-versionbits-warning.py',
     'preciousblock.py',
     'importprunedfunds.py',
     'signmessages.py',
-    'p2p-compactblocks.py',
     'nulldummy.py',
-    'importmulti.py',
 ]
 if ENABLE_ZMQ:
     testScripts.append('zmq_test.py')
 
 testScriptsExt = [
+    'pruning.py',
+    # vv Tests less than 20m vv
+    'smartfees.py',
+    # vv Tests less than 5m vv
+    'maxuploadtarget.py',
+    'mempool_packages.py',
+    # vv Tests less than 2m vv
+    'bip68-sequence.py',
+    'getblocktemplate_longpoll.py',
+    # vv Tests less than 60s vv
     'bip9-softforks.py',
+    'p2p-feefilter.py',
+    'rpcbind_test.py',
+    # vv Tests less than 30s vv
     'bip65-cltv.py',
     'bip65-cltv-p2p.py',
-    'bip68-sequence.py',
     'bipdersig-p2p.py',
     'bipdersig.py',
-    'getblocktemplate_longpoll.py',
     'getblocktemplate_proposals.py',
     'txn_doublespend.py',
     'txn_clone.py --mineblock',
     'forknotify.py',
     'invalidateblock.py',
-    'rpcbind_test.py',
-    'smartfees.py',
     'maxblocksinflight.py',
     'p2p-acceptblock.py',
-    'mempool_packages.py',
-    'maxuploadtarget.py',
     'replace-by-fee.py',
-    'p2p-feefilter.py',
-    'pruning.py', # leave pruning last as it takes a REALLY long time
 ]
 
 

zcash/zcash

qa/pull-tester/rpc-tests.py
@@ -100,31 +100,48 @@ if ENABLE_ZMQ:
 
 testScripts = [
     # longest test should go first, to favor running tests in parallel
-    'p2p-fullblocktest.py',
-    'walletbackup.py',
-    'paymentdisclosure.py',
-    'prioritisetransaction.py',
-    'wallet_treestate.py',
-    'wallet_anchorfork.py',
-    'wallet_changeaddresses.py',
-    'wallet_changeindicator.py',
-    'wallet_import_export.py',
-    'wallet_sendmany_any_taddr.py',
-    'wallet_shieldingcoinbase.py',
-    'wallet_shieldcoinbase_sprout.py',
-    'wallet_shieldcoinbase_sapling.py',
-    'wallet_listreceived.py',
+    # vv Tests less than 5m vv
+    'mergetoaddress_sapling.py',
     'wallet.py',
+    'wallet_shieldcoinbase_sprout.py',
+    'sprout_sapling_migration.py',
+    'remove_sprout_shielding.py',
+    'zcjoinsplitdoublespend.py',
+    # vv Tests less than 2m vv
+    'zcjoinsplit.py',
+    'mergetoaddress_mixednotes.py',
+    'wallet_shieldcoinbase_sapling.py',
+    'turnstile.py',
+    'walletbackup.py',
+    'zkey_import_export.py',
+    'prioritisetransaction.py',
+    'wallet_changeaddresses.py',
+    'wallet_listreceived.py',
+    'mempool_tx_expiry.py',
+    'finalsaplingroot.py',
     'wallet_overwintertx.py',
     'wallet_persistence.py',
-    'wallet_nullifiers.py',
+    'wallet_listnotes.py',
+    # v-- This test is down here to spread the CPU load.
+    'mergetoaddress_sprout.py',
+    # vv Tests less than 60s vv
+    'fundrawtransaction.py',
+    'reorg_limit.py',
+    'mempool_limit.py',
+    'p2p-fullblocktest.py',
+    'paymentdisclosure.py',
+    # vv Tests less than 30s vv
     'wallet_1941.py',
     'wallet_addresses.py',
+    'wallet_anchorfork.py',
+    'wallet_changeindicator.py',
+    'wallet_import_export.py',
+    'wallet_nullifiers.py',
     'wallet_sapling.py',
-    'wallet_listnotes.py',
-    'mergetoaddress_sprout.py',
-    'mergetoaddress_sapling.py',
-    'mergetoaddress_mixednotes.py',
+    'wallet_sendmany_any_taddr.py',
+    # v-- This test is down here to spread the CPU load.
+    'wallet_shieldingcoinbase.py',
+    'wallet_treestate.py',
     'listtransactions.py',
     'mempool_resurrect_test.py',
     'txn_doublespend.py',
@@ -133,17 +150,14 @@ testScripts = [
     'rawtransactions.py',
     'getrawtransaction_insight.py',
     'rest.py',
-    'mempool_limit.py',
     'mempool_spendcoinbase.py',
     'mempool_reorg.py',
     'mempool_nu_activation.py',
-    'mempool_tx_expiry.py',
     'httpbasics.py',
     'multi_rpc.py',
     'zapwallettxes.py',
     'proxy_test.py',
     'merkle_blocks.py',
-    'fundrawtransaction.py',
     'signrawtransactions.py',
     'signrawtransaction_offline.py',
     'key_import_export.py',
@@ -156,10 +170,6 @@ testScripts = [
     'blockchain.py',
     'disablewallet.py',
     'keypool.py',
-    'zcjoinsplit.py',
-    'zcjoinsplitdoublespend.py',
-    'zkey_import_export.py',
-    'reorg_limit.py',
     'getblocktemplate.py',
     'bip65-cltv-p2p.py',
     'bipdersig-p2p.py',
@@ -169,10 +179,7 @@ testScripts = [
     'p2p_txexpiringsoon.py',
     'p2p_node_bloom.py',
     'regtest_signrawtransaction.py',
-    'finalsaplingroot.py',
     'shorter_block_times.py',
-    'sprout_sapling_migration.py',
-    'turnstile.py',
     'mining_shielded_coinbase.py',
     'coinbase_funding_streams.py',
     'framework.py',
@@ -181,22 +188,26 @@ testScripts = [
     'upgrade_golden.py',
     'post_heartwood_rollback.py',
     'feature_logging.py',
-    'remove_sprout_shielding.py',
     'feature_walletfile.py',
 ]
 if ENABLE_ZMQ:
     testScripts.append('zmq_test.py')
 
 testScriptsExt = [
-    'getblocktemplate_longpoll.py',
-    'getblocktemplate_proposals.py',
     'pruning.py',
+    # vv Tests less than 20m vv
+    'smartfees.py',
+    # vv Tests less than 5m vv
+    # vv Tests less than 2m vv
+    'getblocktemplate_longpoll.py',
+    # vv Tests less than 60s vv
+    'rpcbind_test.py',
+    # vv Tests less than 30s vv
+    'getblocktemplate_proposals.py',
     'forknotify.py',
     'hardforkdetection.py',
     'invalidateblock.py',
     'receivedby.py',
-    'rpcbind_test.py',
-    'smartfees.py',
     'maxblocksinflight.py',
     'invalidblockrequest.py',
 #    'forknotify.py',