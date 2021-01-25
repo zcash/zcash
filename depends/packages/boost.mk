c996fc1ca4308d7385d71a275b2abe01859c10e8
Jack Grigg <jack@z.cash>
2021-01-25 11:35:18 +0000

    Revert "Update boost to 1.75, postpone other updates."
    This reverts commit 8f4dd4460bcb6e43a21dd9deafe9ccebbf24c577.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,10 +1,10 @@
 package=boost
-$(package)_version=1_75_0
+$(package)_version=1_74_0
 $(package)_download_path=https://dl.bintray.com/boostorg/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
-$(package)_sha256_hash=953db31e016db7bb207f11432bef7df100516eeb746843fa0486a222e3fd49cb
+$(package)_sha256_hash=83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1
 $(package)_dependencies=native_b2
-$(package)_patches=deprecated-two-arg-allocate.diff
+$(package)_patches=iostreams-106.patch signals2-noise.patch deprecated-two-arg-allocate.diff
 
 ifneq ($(host_os),darwin)
 $(package)_dependencies+=libcxx
@@ -42,6 +42,8 @@ endif
 endef
 
 define $(package)_preprocess_cmds
+  patch -p2 < $($(package)_patch_dir)/iostreams-106.patch && \
+  patch -p2 < $($(package)_patch_dir)/signals2-noise.patch && \
   patch -p2 < $($(package)_patch_dir)/deprecated-two-arg-allocate.diff && \
   echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cflags>\"$($(package)_cflags)\" <cxxflags>\"$($(package)_cxxflags)\" <compileflags>\"$($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_ar)\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
