zcash/zcash

commit e286250ce49309bfa931b622fabfc37100246266

Make fDisconnect an std::atomic

src/net.h
@@ -287,7 +287,7 @@ public:
     bool fInbound;
     bool fNetworkNode;
     bool fSuccessfullyConnected;
-    bool fDisconnect;
+    std::atomic_bool fDisconnect;
     // We use fRelayTxes for two purposes -
     // a) it allows us to not relay tx invs before receiving the peer's version message
     // b) the peer may tell us in its version message that we should not relay tx invs

commit a0ac27ec6ed434a233c7ad2468258f6e6e7e9688

Fix regression introduced in e286250ce49309bfa931b622fabfc37100246266 by adding #include <atomic>. fixes #5014
Signed-off-by: Daira Hopwood <daira@jacaranda.org>

src/net.h
@@ -23,6 +23,7 @@
 
 #include <deque>
 #include <stdint.h>
+#include <atomic>
 
 #ifndef WIN32
 #include <arpa/inet.h>