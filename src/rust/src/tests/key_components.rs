2e12078cfcf1d4c2bebf56ba3e4f5ede4a9cbe44
Jack Grigg <jack@z.cash>
2021-03-27 10:55:56 +1300

    depends: Update Rust to 1.51.0

--- src/rust/src/tests/key_components.rs
+++ src/rust/src/tests/key_components.rs
@@ -27,7 +27,7 @@ fn key_components() {
         note_cm: [u8; 32],
         note_pos: u64,
         note_nf: [u8; 32],
-    };
+    }
 
     // From https://github.com/zcash-hackworks/zcash-test-vectors/blob/master/sapling_key_components.py
     let test_vectors = vec![
