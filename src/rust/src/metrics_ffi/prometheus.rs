83eef40f4c021fcb53bc969af534b49683d29ed6
Jack Grigg <jack@z.cash>
2021-03-31 09:15:23 +1300

    rust: Add license header to metrics_ffi::prometheus

--- src/rust/src/metrics_ffi/prometheus.rs
+++ src/rust/src/metrics_ffi/prometheus.rs
@@ -1,5 +1,16 @@
-// This is mostly code copied from metrics_exporter_prometheus. It is licensed under the
-// same license as the zcash codebase.
+// This is mostly code copied from metrics_exporter_prometheus. The copied portions are
+// licensed under the same terms as the zcash codebase (reproduced below from
+// https://github.com/metrics-rs/metrics/blob/main/metrics-exporter-prometheus/LICENSE):
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
 use hyper::{
     server::{conn::AddrStream, Server},
