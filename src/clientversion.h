f4d88aec2061b3f092d146ebd6d64259a5ccd04c
Jack Grigg <jack@z.cash>
2021-04-09 17:32:49 +1200

    make-release.py: Versioning changes for 4.4.0-rc1.

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,9 +16,9 @@
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 4
-#define CLIENT_VERSION_MINOR 3
+#define CLIENT_VERSION_MINOR 4
 #define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 50
+#define CLIENT_VERSION_BUILD 25
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
