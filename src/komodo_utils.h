ea738d097d5974fa2d1ddd4ae93b121dafc58c48
dimxy <dimxy@komodoplatform.com>
2021-05-12 17:46:42 +0500

    fixing S5 timestamp var

--- src/komodo_utils.h
+++ src/komodo_utils.h
@@ -2500,5 +2500,5 @@ void komodo_prefetch(FILE *fp)
 
 bool komodo_Is2021JuneHFActive()
 {
-    return GetLatestTimestamp(komodo_currentheight()) < JUNE2021_NNELECTION_HARDFORK;
+    return GetLatestTimestamp(komodo_currentheight()) < nS5Timestamp;
 }
