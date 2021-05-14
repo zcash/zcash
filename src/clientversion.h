22035cca956a35d9fe2be8df07713f2fc5a272dc
ca333 <ca333@keemail.me>
2021-05-14 10:49:38 +0200

    bump version 0.7.0

--- src/clientversion.h
+++ src/clientversion.h
@@ -30,9 +30,9 @@
  */
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 3
-#define CLIENT_VERSION_MINOR 0
-#define CLIENT_VERSION_REVISION 1
+#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MINOR 7
+#define CLIENT_VERSION_REVISION 0
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
@@ -42,7 +42,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2019
+#define COPYRIGHT_YEAR 2021
 
 #endif //HAVE_CONFIG_H
 
