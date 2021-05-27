b3chain/b3chain

refactor: Avoid extra lookups of mapAddressBook in listunspent RPC

src/wallet/rpcwallet.cpp
@@ -3171,10 +3171,11 @@ UniValue listunspent(const JSONRPCRequest& request)
         if (fValidAddress) {
             entry.pushKV("address", EncodeDestination(address));
 
-            if (pwallet->mapAddressBook.count(address)) {
-                entry.pushKV("label", pwallet->mapAddressBook[address].name);
+            auto i = pwallet->mapAddressBook.find(address);
+            if (i != pwallet->mapAddressBook.end()) {
+                entry.pushKV("label", i->second.name);
                 if (IsDeprecatedRPCEnabled("accounts")) {
-                    entry.pushKV("account", pwallet->mapAddressBook[address].name);
+                    entry.pushKV("account", i->second.name);
                 }
             }
 

zcash/zcash

zcash: cherry picked from commit a59dac35abf64d5af4d499bc3397b3369eb76eda
zcash: https://github.com/bitcoin/bitcoin/pull/12639

src/wallet/rpcwallet.cpp
@@ -2518,8 +2518,10 @@ UniValue listunspent(const UniValue& params, bool fHelp)
         if (fValidAddress) {
             entry.pushKV("address", keyIO.EncodeDestination(address));
 
-            if (pwalletMain->mapAddressBook.count(address))
-                entry.pushKV("account", pwalletMain->mapAddressBook[address].name);
+            auto i = pwalletMain->mapAddressBook.find(address);
+            if (i != pwalletMain->mapAddressBook.end()) {
+                entry.pushKV("account", i->second.name);
+            }
 
             if (scriptPubKey.IsPayToScriptHash()) {
                 const CScriptID& hash = std::get<CScriptID>(address);