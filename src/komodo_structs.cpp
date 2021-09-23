e74e006821143db16e5d60495deff0d0412aa1ca
DeckerSU <deckersu@protonmail.com>
2021-09-23 18:02:45 +0300

    fix init of event_notarized dest field

--- src/komodo_structs.cpp
+++ src/komodo_structs.cpp
@@ -171,9 +171,10 @@ std::ostream& operator<<(std::ostream& os, const event_rewind& in)
     return os;
 }
 
-event_notarized::event_notarized(uint8_t *data, long &pos, long data_len, int32_t height, bool includeMoM) 
+event_notarized::event_notarized(uint8_t *data, long &pos, long data_len, int32_t height, const char* _dest, bool includeMoM)
         : event(EVENT_NOTARIZED, height), MoMdepth(0)
 {
+    memcpy(this->dest, _dest, sizeof(this->dest)-1);
     MoM.SetNull();
     mem_read(this->notarizedheight, data, pos, data_len);
     mem_read(this->blockhash, data, pos, data_len);
@@ -185,9 +186,10 @@ event_notarized::event_notarized(uint8_t *data, long &pos, long data_len, int32_
     }
 }
 
-event_notarized::event_notarized(FILE* fp, int32_t height, bool includeMoM) 
+event_notarized::event_notarized(FILE* fp, int32_t height, const char* _dest, bool includeMoM)
         : event(EVENT_NOTARIZED, height), MoMdepth(0)
 {
+    memcpy(this->dest, _dest, sizeof(this->dest)-1);
     MoM.SetNull();
     if ( fread(&notarizedheight,1,sizeof(notarizedheight),fp) != sizeof(notarizedheight) )
         throw parse_error("Invalid notarization height");
