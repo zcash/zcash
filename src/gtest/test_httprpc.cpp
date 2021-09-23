7cdd185500327f19e640b8e96421cff92dd5a877
zancas <zancas@zingolabs.org>
2021-09-22 20:47:02 -0600

    copy static helper from addrman_tests.cpp

--- src/gtest/test_httprpc.cpp
+++ src/gtest/test_httprpc.cpp
@@ -6,6 +6,13 @@
 
 using ::testing::Return;
 
+static CService ResolveService(const char* ip, int port = 0)
+{
+    CService serv;
+    Lookup(ip, serv, port, false), strprintf("failed to resolve: %s:%i", ip, port);
+    return serv;
+}
+
 class MockHTTPRequest : public HTTPRequest {
 public:
     MOCK_METHOD0(GetPeer, CService());
@@ -52,7 +59,7 @@ TEST(HTTPRPC, FailsWithBadAuth) {
     EXPECT_CALL(req, GetHeader("authorization"))
         .WillRepeatedly(Return(std::make_pair(true, "Basic spam:eggs")));
     EXPECT_CALL(req, GetPeer())
-        .WillRepeatedly(Return(CService("127.0.0.1:1337")));
+        .WillRepeatedly(Return(ResolveService("127.0.0.1:1337")));
     EXPECT_CALL(req, WriteHeader("WWW-Authenticate", "Basic realm=\"jsonrpc\""))
         .Times(1);
     EXPECT_CALL(req, WriteReply(HTTP_UNAUTHORIZED, ""))
