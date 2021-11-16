3bae89c73202d6b61fd4f397355f7877167a2416
teor <teor@riseup.net>
2021-11-16 10:46:08 +1000

    Use correct copyright header
    Co-authored-by: str4d <thestr4d@gmail.com>

--- src/primitives/tx_version_info.cpp
+++ src/primitives/tx_version_info.cpp
@@ -1,5 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2021 The Zcash developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php .
 
