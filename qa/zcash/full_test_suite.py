48d657497adc92c0fab2f4861a999940d2f6a613
Marshall Gaucher <36639405+mdr0id@users.noreply.github.com>
2022-01-21 16:22:03 -0800

    Use -j option in underlying python script for RPCs
    Make use of the jobs option on machines that have more than the default 4 threads

--- qa/zcash/full_test_suite.py
+++ qa/zcash/full_test_suite.py
@@ -182,7 +182,7 @@ STAGE_COMMANDS = {
     'util-test': util_test,
     'secp256k1': ['make', '-C', repofile('src/secp256k1'), 'check'],
     'univalue': ['make', '-C', repofile('src/univalue'), 'check'],
-    'rpc': [repofile('qa/pull-tester/rpc-tests.py')],
+    'rpc': [repofile('qa/pull-tester/rpc-tests.py'), '-j8'],
 }
 
 
