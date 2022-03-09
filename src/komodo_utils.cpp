0ada63acd75ee48a5a0df67779f2939c4a0a2220
Alrighttt <36680730+Alrighttt@users.noreply.github.com>
2022-03-09 11:34:56 -0500

    Update src/komodo_utils.cpp
    Co-authored-by: DeckerSU <support@decker.su>

--- src/komodo_utils.cpp
+++ src/komodo_utils.cpp
@@ -1293,7 +1293,7 @@ void komodo_args(char *argv0)
     {
         KOMODO_MININGTHREADS = GetArg("-genproclimit",-1);
     }
-    IS_MODE_EXCHANGEWALLET = GetArg("-exchange", 0);
+    IS_MODE_EXCHANGEWALLET = GetBoolArg("-exchange", false);
     DONATION_PUBKEY = GetArg("-donation", "");
     NOTARY_PUBKEY = GetArg("-pubkey", "");
     IS_KOMODO_DEALERNODE = GetArg("-dealer",0);
