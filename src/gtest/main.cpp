753412c5b3e34b76628420cf937889564331fef2
Jack Grigg <jack@z.cash>
2022-04-01 14:34:38 +0000

    test: Capture gtest log output and only print on error
    In zcash/zcash#5762 we altered the gtest runner to turn on logging to
    stdout instead of dropping the logs, to make figuring out test failures
    easier. However, this also meant the logs would be displayed for tests
    that succeeded, and it was confusing to see `ERROR` log lines present.
    
    We now have a test-specific initialization method for tracing that uses
    synchronous logging instead of a background thread, and only logs to a
    file. In the gtests, we initialize this with a new temporary file, and
    add a gtest event listener that clears the file at the start of each
    test, and then prints its contents if the test fails.
    
    Example test output:
    
    ```
    [ RUN      ] ChecktransactionTests.CheckVpubNotBothNonzero
    gtest/test_checktransaction.cpp:39: Failure
    Expected equality of these values:
      state.GetRejectReason()
        Which is: "bad-txns-vpubs-both-nonzero"
      "bad-txns-vpubs-both-nonzer"
    
    --- Logs:
      ERROR main: CheckTransaction(): joinsplit.vpub_new and joinsplit.vpub_old both nonzero
    ---
    [  FAILED  ] ChecktransactionTests.CheckVpubNotBothNonzero (0 ms)
    ```
    
    Closes zcash/zcash#5766.

--- src/gtest/main.cpp
+++ src/gtest/main.cpp
@@ -9,6 +9,8 @@
 #include <sodium.h>
 #include <tracing.h>
 
+#include <boost/filesystem.hpp>
+
 const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 
 struct ECCryptoClosure
@@ -18,22 +20,72 @@ struct ECCryptoClosure
 
 ECCryptoClosure instance_of_eccryptoclosure;
 
+class LogGrabber : public ::testing::EmptyTestEventListener {
+    fs::path logPath;
+
+public:
+    LogGrabber(fs::path logPathIn) : logPath(logPathIn) {}
+
+    virtual void OnTestStart(const ::testing::TestInfo& test_info) {
+        // Test logs are written synchronously, so we can clear the log file to
+        // ensure that at the end of the test, the log lines are all related to
+        // the test itself.
+        std::ofstream logFile;
+        logFile.open(logPath.string(), std::ofstream::out | std::ofstream::trunc);
+        logFile.close();
+    }
+
+    virtual void OnTestEnd(const ::testing::TestInfo& test_info) {
+        // If the test failed, print the test logs.
+        auto result = test_info.result();
+        if (result && result->Failed()) {
+            std::cout << "\n--- Logs:";
+
+            std::ifstream logFile;
+            logFile.open(logPath.string(), std::ios::in | std::ios::ate);
+            ASSERT_TRUE(logFile.is_open());
+            logFile.seekg(0, logFile.beg);
+            std::string line;
+            while (logFile.good()) {
+                std::getline(logFile, line);
+                if (!line.empty()) {
+                    std::cout << "\n  " << line;
+                }
+            }
+
+            std::cout << "\n---" << std::endl;
+        }
+    }
+};
+
 int main(int argc, char **argv) {
   assert(sodium_init() != -1);
   ECC_Start();
 
-  // Log all errors to stdout so we see them in test output.
-  std::string initialFilter = "error";
-  pTracingHandle = tracing_init(
-      nullptr, 0,
-      initialFilter.c_str(),
-      false);
+    // Log all errors to a common test file.
+    fs::path tmpPath = fs::temp_directory_path();
+    fs::path tmpFilename = fs::unique_path("%%%%%%%%");
+    fs::path logPath = tmpPath / tmpFilename;
+    const fs::path::string_type& logPathStr = logPath.native();
+    static_assert(sizeof(fs::path::value_type) == sizeof(codeunit),
+                    "native path has unexpected code unit size");
+    const codeunit* logPathCStr = reinterpret_cast<const codeunit*>(logPathStr.c_str());
+    size_t logPathLen = logPathStr.length();
+
+    std::string initialFilter = "error";
+    pTracingHandle = tracing_init_test(
+        logPathCStr, logPathLen,
+        initialFilter.c_str());
 
   testing::InitGoogleMock(&argc, argv);
 
   // The "threadsafe" style is necessary for correct operation of death/exit
   // tests on macOS (https://github.com/zcash/zcash/issues/4802).
   testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Append(new LogGrabber(logPath));
 
   auto ret = RUN_ALL_TESTS();
 
