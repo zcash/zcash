57d9d9ee054280c24eb61d654c58ab3a7213fd0b
Kris Nuttycombe <kris@nutty.land>
2022-04-13 21:04:22 -0600

    Update boost dependencies to version 1.79.0

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_78_0
+$(package)_version=1_79_0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
-$(package)_sha256_hash=8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc
+$(package)_sha256_hash=475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39
 $(package)_dependencies=native_b2
 
 ifneq ($(host_os),darwin)
