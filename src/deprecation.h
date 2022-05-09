9fe516576fdf6cc4b8d6cf9edaf155f7f033a9db
Kris Nuttycombe <kris@nutty.land>
2022-05-09 09:11:05 -0600

    Set RELEASE_TO_DEPRECATION_WEEKS to 2 weeks to provide RC EOS halt.

--- src/deprecation.h
+++ src/deprecation.h
@@ -11,7 +11,7 @@
 // Shut down nodes running this version of code, 16 weeks' worth of blocks after the estimated
 // release block height. A warning is shown during the 14 days' worth of blocks prior to shut down.
 static const int APPROX_RELEASE_HEIGHT = 1540976;
-static const int RELEASE_TO_DEPRECATION_WEEKS = 16;
+static const int RELEASE_TO_DEPRECATION_WEEKS = 2;
 static const int EXPECTED_BLOCKS_PER_HOUR = 3600 / Consensus::POST_BLOSSOM_POW_TARGET_SPACING;
 static_assert(EXPECTED_BLOCKS_PER_HOUR == 48, "The value of Consensus::POST_BLOSSOM_POW_TARGET_SPACING was chosen such that this assertion holds.");
 static const int ACTIVATION_TO_DEPRECATION_BLOCKS = (RELEASE_TO_DEPRECATION_WEEKS * 7 * 24 * EXPECTED_BLOCKS_PER_HOUR);
