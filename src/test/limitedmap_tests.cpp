c545f64356b3e19438fb8603e9ef1917dbd25028
Kris Nuttycombe <kris@nutty.land>
2022-05-11 17:34:50 -0600

    scripted-diff: Add 2021-2022 copyright headers for files added/modified in 2021
    -BEGIN VERIFY SCRIPT-
    grep -l "Copyright" $(grep -L "The Zcash developers" $(git diff --name-only --diff-filter=ACM a960e896dd4c0b6d96db3d5ebbb1caa6c92a9f39..dfefab2f5587b873b02c4ed22ee3441d63f8a111 -- src/ test/ zcutil/ qa/)) | xargs -I {} sed -i"" -e "s#\(\(.*\)Copyright (c) .* The Bitcoin Core developers\)#\1\n\2Copyright (c) 2021-2022 The Zcash developers#" {}
    -END VERIFY SCRIPT-

--- src/test/limitedmap_tests.cpp
+++ src/test/limitedmap_tests.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2015 The Bitcoin Core developers
+// Copyright (c) 2021-2022 The Zcash developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
