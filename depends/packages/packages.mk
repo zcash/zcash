21f1bbf4aa509be63995583fb28c043861af5902
Jack Grigg <jack@z.cash>
2022-05-26 01:07:30 +0000

    depends: Add `rust/cxx.h` header as a dependency
    We could also generate this in the main build with `cxxbridge --header`,
    but we have the source from vendoring `cxxbridge`, and this simplifies
    the automake integration slightly.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-zcash_packages := libsodium utfcpp
+zcash_packages := libsodium rustcxx utfcpp
 packages := boost libevent zeromq $(zcash_packages) googletest
 native_packages := native_clang native_ccache native_rust native_cxxbridge
 
