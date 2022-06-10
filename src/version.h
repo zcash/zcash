98f7b874dd0483181f170c7b5f72a1d8c33be199
DeckerSU <deckersu@protonmail.com>
2022-06-10 23:36:29 +0300

    allow nodes with 170010 proto version to connect

--- src/version.h
+++ src/version.h
@@ -33,8 +33,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 170011;
-static const int STAKEDMIN_PEER_PROTO_VERSION = 170011;
+static const int MIN_PEER_PROTO_VERSION = 170010;
+static const int STAKEDMIN_PEER_PROTO_VERSION = 170010;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
