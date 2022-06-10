5874f7d35514111e316fb01c3d7f07a08d5de9dc
DeckerSU <deckersu@protonmail.com>
2022-06-10 12:44:27 +0300

    for staked chains also better to use latest proto bcz of s6

--- src/version.h
+++ src/version.h
@@ -34,7 +34,7 @@ static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 170011;
-static const int STAKEDMIN_PEER_PROTO_VERSION = 170008;
+static const int STAKEDMIN_PEER_PROTO_VERSION = 170011;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
