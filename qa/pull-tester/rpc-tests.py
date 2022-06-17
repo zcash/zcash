2f26a0cd6ba69c469d23a5339d7830c949090f9b
Marshall Gaucher <36639405+mdr0id@users.noreply.github.com>
2022-06-17 11:44:17 -0700

    Update rpc-tests.py
    Add command line args to allow test groups to sort for N machines.
    
    Update rpc-tests.py
    
    Resolve diff issue for missing lines

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -190,6 +190,8 @@ def main():
     parser.add_argument('--force', '-f', action='store_true', help='run tests even on platforms where they are disabled by default (e.g. windows).')
     parser.add_argument('--help', '-h', '-?', action='store_true', help='print help text and exit')
     parser.add_argument('--jobs', '-j', type=int, default=4, help='how many test scripts to run in parallel. Default=4.')
+    parser.add_argument('--machines', '-m', type=int, default=-1, help='how many machines run in parallel. Default=4.')
+    parser.add_argument('--rpcgroup', '-r', type=int, default=4, help='how many test on each machine to run in parallel. Default=4.')
     parser.add_argument('--nozmq', action='store_true', help='do not run the zmq tests')
     args, unknown_args = parser.parse_known_args()
 
@@ -266,7 +268,10 @@ def main():
         subprocess.check_call((config["environment"]["SRCDIR"] + '/qa/rpc-tests/' + test_list[0]).split() + ['-h'])
         sys.exit(0)
 
-    run_tests(test_list, config["environment"]["SRCDIR"], config["environment"]["BUILDDIR"], config["environment"]["EXEEXT"], args.jobs, args.coverage, passon_args)
+    #sort the tests into number of machines available; ensuring all tests are run
+    k, m = divmod(len(test_list), args.machines)
+    split_list = list(test_list[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(args.machines))
+    run_tests(split_list[args.rpcgroup], config["environment"]["SRCDIR"], config["environment"]["BUILDDIR"], config["environment"]["EXEEXT"], args.jobs, args.coverage, passon_args)
 
 def run_tests(test_list, src_dir, build_dir, exeext, jobs=1, enable_coverage=False, args=[]):
     BOLD = ("","")
