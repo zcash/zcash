bbe10e552a5328628b68f9b1ca5b2ae4a72948e2
Yasser Isa <yasser.isam@gmail.com>
2022-06-16 20:56:12 -0400

    Adding arguments to curl under depends/builders

--- depends/builders/freebsd.mk
+++ depends/builders/freebsd.mk
@@ -1,4 +1,4 @@
 build_freebsd_CC = clang
 build_freebsd_CXX = clang++
 build_freebsd_SHA256SUM = shasum -a 256
-build_freebsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
+build_freebsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) --retry-delay 0 --retry-max-time 40 --fail -o
--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,2 +1,2 @@
 build_linux_SHA256SUM = sha256sum
-build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) --retry-delay 0 --retry-max-time 40 --fail -o
