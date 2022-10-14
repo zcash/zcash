f7044137bcbf9b200c0a6cf5428b6cb9c41ee11a
Greg Pfeil <greg@technomadic.org>
2022-10-14 08:11:42 -1000

    Add a Nix build
    This builds everything, but doesn’t yet pass full_test_suite.py.
    
    If you have Nix installed on your system, running `nix-build --pure` should give
    you a functioning (but apparently not bug-free) version of zcashd, etc.
    
    Co-authored-by: Kris Nuttycombe <kris.nuttycombe@gmail.com>

---
+++ default.nix
@@ -0,0 +1,57 @@
+# In development, load this with `nix-shell`
+# In CI, build this with `nix-build --pure`
+#
+# NB: Do not remove anything from this file without testing it against
+#    `nix-build --pure`. This builds it in a sandboxed environment, but makes
+#     development much harder as it means most of your not-required-for-build
+#     tooling is unavailable.
+#
+# Currently need to run
+# > ./configure $configureFlags
+# which I don’t understand, because the boost-libdir should be in LD_FLAGS, etc. already.
+
+{ pkgs ? import ./contrib/nix/pkgs.nix {} }:
+
+pkgs.clangStdenv.mkDerivation {
+  name = "zcash";
+
+  src = ./.;
+  
+  # These are roughly copied from depends/packages/packages.mk, however they
+  # should be more complete, as `nix-build --pure` prevents us from relying on
+  # any system libraries or tools.
+  
+  buildInputs = [
+    pkgs.boost
+    pkgs.db62
+    pkgs.gtest
+    pkgs.libevent
+    pkgs.openssl
+    pkgs.libsodium
+    pkgs.utf8cpp
+    pkgs.zeromq
+  ];
+
+  nativeBuildInputs = [
+    pkgs.autoreconfHook
+    pkgs.cargo
+    pkgs.cxx-rs
+    pkgs.git
+    pkgs.hexdump
+    pkgs.makeWrapper
+    pkgs.pkg-config
+    pkgs.python3
+    pkgs.rustc
+  ];
+
+  # I think this is needed because the “utf8cpp” dir component is non-standard,
+  # but I don’t know why the package doesn’t set that up correctly.
+  CXXFLAGS = "-I${pkgs.utf8cpp}/include/utf8cpp";
+
+  RUST_TARGET = pkgs.buildPlatform.config;
+
+  configureFlags = [
+    "--enable-online-rust"
+    "--with-boost-libdir=${pkgs.boost}/lib"
+  ];
+}
