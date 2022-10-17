125ea043a46166b4be06e341e2bc33ff2ac42d3e
Greg Pfeil <greg@technomadic.org>
2022-10-16 15:40:45 -1000

    Move Nix build to a flake
    This also handles vendored Rust crates via Nix, and maintains the pre-flake
    build (via flake-compat).

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "Internet money";
+
+  outputs = { self, crane, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          packages = rec {
+            default = zcash;
+
+            librustzcash = crane.lib.${system}.buildPackage {
+              doCheck = true;
+              src = ./.;
+            };
+
+            zcash = pkgs.llvmPackages_14.stdenv.mkDerivation {
+              pname = "zcash";
+              version = "5.3.0";
+
+              src = ./.;
+
+              buildInputs = [
+                librustzcash
+                pkgs.boost
+                pkgs.db62
+                pkgs.gtest
+                pkgs.libevent
+                (pkgs.libsodium.overrideAttrs (old: {
+                  patches = old.patches ++ [
+                    ./depends/patches/libsodium/1.0.15-pubkey-validation.diff
+                    ./depends/patches/libsodium/1.0.15-signature-validation.diff
+                  ];
+                }))
+                pkgs.openssl
+                pkgs.utf8cpp
+                (pkgs.zeromq.overrideAttrs (old: {
+                  patches = [
+                    ./depends/patches/zeromq/windows-unused-variables.diff
+                  ];
+                }))
+              ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+                pkgs.darwin.apple_sdk.frameworks.Security
+              ];
+
+              nativeBuildInputs = [
+                pkgs.autoreconfHook
+                pkgs.cxx-rs
+                pkgs.hexdump
+                pkgs.makeWrapper
+                pkgs.pkg-config
+                pkgs.python3
+              ];
+
+              # I think this is needed because the “utf8cpp” dir component is
+              # non-standard, but I don’t know why the package doesn’t set that
+              # up correctly.
+              CXXFLAGS = "-I${pkgs.utf8cpp}/include/utf8cpp";
+
+              configureFlags = [
+                "--with-boost-libdir=${pkgs.boost}/lib"
+                "--with-rustzcash-dir=${librustzcash}"
+              ];
+            };
+          };
+        });
+
+  inputs = {
+    crane = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:ipetkov/crane";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # We use unstable because cxx-rs isn’t in a release yet … but we should
+    # probably be able to use 22.11 or whatever once it’s out.
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+}
