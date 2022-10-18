6e8b958383a3f86cfd71a224626afa2677ed8001
Greg Pfeil <greg@technomadic.org>
2022-10-17 17:01:02 -1000

    Modularize the Nix build
    Each package now has its own file. This makes the set of packages and the
    dependencies between them clearer.

---
+++ contrib/nix/zcash.nix
@@ -0,0 +1,70 @@
+{ autoreconfHook
+, boost
+, cxx-rs
+, db62
+, gtest
+, hexdump
+, libevent
+, librustzcash
+, libsodium
+, llvmPackages_14
+, makeWrapper
+, openssl
+, pkg-config
+, python3
+, utf8cpp
+, zeromq
+, zk-parameters
+}:
+
+llvmPackages_14.stdenv.mkDerivation {
+  pname = "zcash";
+  version = "5.3.0";
+
+  src = ../..;
+
+  buildInputs = [
+    boost
+    db62
+    gtest
+    libevent
+    librustzcash
+    (libsodium.overrideAttrs (old: {
+      patches = old.patches ++ [
+        ../../depends/patches/libsodium/1.0.15-pubkey-validation.diff
+        ../../depends/patches/libsodium/1.0.15-signature-validation.diff
+      ];
+    }))
+    openssl
+    utf8cpp
+    (zeromq.overrideAttrs (old: {
+      patches = [
+        ../../depends/patches/zeromq/windows-unused-variables.diff
+      ];
+    }))
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    cxx-rs
+    hexdump
+    makeWrapper
+    pkg-config
+    python3
+    zk-parameters
+  ];
+
+  # I think this is needed because the “utf8cpp” dir component is non-standard,
+  # but I don’t know why the package doesn’t set that up correctly.
+  CXXFLAGS = "-I${utf8cpp}/include/utf8cpp";
+
+  # Because of fetch-params, everything expects the parameters to be in `HOME`.
+  HOME = "${zk-parameters}/var/cache";
+
+  configureFlags = [
+    "--with-boost-libdir=${boost}/lib"
+    "--with-rustzcash-dir=${librustzcash}"
+  ];
+
+  doCheck = true;
+}
--- flake.nix
+++ flake.nix
@@ -10,102 +10,15 @@
           packages = rec {
             default = zcash;
 
-            librustzcash = crane.lib.${system}.buildPackage {
-              src = ./.;
-
-              buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
-                pkgs.darwin.apple_sdk.frameworks.Security
-                pkgs.libiconv
-              ];
-
-              doCheck = true;
+            librustzcash = pkgs.callPackage ./contrib/nix/librustzcash.nix {
+              crane = crane.lib.${system};
             };
 
-            zk-parameters = pkgs.stdenv.mkDerivation {
-              name = "zk-parameters";
-
-              src = ./.;
-
-              nativeBuildInputs = [
-                pkgs.cacert
-                pkgs.flock
-                pkgs.wget
-              ];
-
-              phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-
-              # This is currently needed because it’s the easiest way to break a
-              # catch-22. If the build is sandboxed (the ideal), then we can’t
-              # download arbitrary files. However, if it’s off, then `/tmp`
-              # doesn’t get relocated, and so the nix build user can’t access
-              # it. Ideally, we’d be able to figure out how to disable the
-              # sandbox _just enough_ to download data at this one point. then
-              # this change could go away.
-              patches = [
-                ./contrib/nix/patches/fetch-params.patch
-              ];
-
-              # We override `HOME` here because Nix sets it to somewhere
-              # unwritable when we’re sandboxed (as we should be). But
-              # fetch-params relies on `HOME`. So make a temporary `HOME` until
-              # we fix that dependency.
-              installPhase = ''
-                HOME="$out/var/cache" ./zcutil/fetch-params.sh
-              '';
+            zk-parameters = pkgs.callPackage ./contrib/nix/zk-parameters.nix {
             };
 
-            zcash = pkgs.llvmPackages_14.stdenv.mkDerivation {
-              pname = "zcash";
-              version = "5.3.0";
-
-              src = ./.;
-
-              buildInputs = [
-                librustzcash
-                pkgs.boost
-                pkgs.db62
-                pkgs.gtest
-                pkgs.libevent
-                (pkgs.libsodium.overrideAttrs (old: {
-                  patches = old.patches ++ [
-                    ./depends/patches/libsodium/1.0.15-pubkey-validation.diff
-                    ./depends/patches/libsodium/1.0.15-signature-validation.diff
-                  ];
-                }))
-                pkgs.openssl
-                pkgs.utf8cpp
-                (pkgs.zeromq.overrideAttrs (old: {
-                  patches = [
-                    ./depends/patches/zeromq/windows-unused-variables.diff
-                  ];
-                }))
-              ];
-
-              nativeBuildInputs = [
-                pkgs.autoreconfHook
-                pkgs.cxx-rs
-                pkgs.hexdump
-                pkgs.makeWrapper
-                pkgs.pkg-config
-                pkgs.python3
-                zk-parameters
-              ];
-
-              # I think this is needed because the “utf8cpp” dir component is
-              # non-standard, but I don’t know why the package doesn’t set that
-              # up correctly.
-              CXXFLAGS = "-I${pkgs.utf8cpp}/include/utf8cpp";
-
-              # Because of fetch-params, everything expects the parameters to be
-              # in `HOME`.
-              HOME = "${zk-parameters}/var/cache";
-
-              configureFlags = [
-                "--with-boost-libdir=${pkgs.boost}/lib"
-                "--with-rustzcash-dir=${librustzcash}"
-              ];
-
-              doCheck = true;
+            zcash = pkgs.callPackage ./contrib/nix/zcash.nix {
+              inherit librustzcash zk-parameters;
             };
           };
         });
