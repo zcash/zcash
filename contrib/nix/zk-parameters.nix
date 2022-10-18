289ed92136fbae63fc7e98ce4710b04c14a232aa
Greg Pfeil <greg@technomadic.org>
2022-10-17 20:13:22 -1000

    Restore sandboxability
    Make zk-parameters fixed-output so it can access the Internet. This still
    requires `--no-sandbox` for `checkPhase` to pass.

--- contrib/nix/zk-parameters.nix
+++ contrib/nix/zk-parameters.nix
@@ -15,22 +15,16 @@ stdenv.mkDerivation {
     wget
   ];
 
-  phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-
-  # This is currently needed because it’s the easiest way to break a
-  # catch-22. If the build is sandboxed (the ideal), then we can’t download
-  # arbitrary files. However, if it’s off, then `/tmp` doesn’t get relocated,
-  # and so the nix build user can’t access it. Ideally, we’d be able to figure
-  # out how to disable the sandbox _just enough_ to download data at this one
-  # point. then this change could go away.
-  patches = [
-    ./patches/fetch-params.patch
-  ];
+  phases = [ "unpackPhase" "installPhase" ];
 
   # We override `HOME` here because Nix sets it to somewhere unwritable when
   # we’re sandboxed (as we should be). But fetch-params relies on `HOME`. So
   # make a temporary `HOME` until we fix that dependency.
   installPhase = ''
-    HOME="$out/var/cache" ./zcutil/fetch-params.sh
+    HOME="$out/var/cache" $src/zcutil/fetch-params.sh
   '';
+
+  outputHash = "sha256-tRiqwvDDYFS1K41p0BSlrO+2wIdZWI7LWLDVzqDp3vs=";
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
 }
