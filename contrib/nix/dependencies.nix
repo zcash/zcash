0005c506c01f384ef003303109c3ae6786117c55
Greg Pfeil <greg@technomadic.org>
2022-10-21 08:16:13 -1000

    Make leveldb a Nix dependency

--- contrib/nix/dependencies.nix
+++ contrib/nix/dependencies.nix
@@ -2,6 +2,28 @@
 # for us.
 
 final: previous: {
+  leveldb = (previous.leveldb.override {
+    static = true;
+  }).overrideAttrs (old: {
+    version = "1.22.0";
+
+    # Replace upstream with Bitcoin’s subtree fork, pinned to the same revision
+    # as the “depends/” build.
+    src = final.fetchFromGitHub {
+      owner = "bitcoin-core";
+      repo = "leveldb-subtree";
+      rev = "f8ae182c1e5176d12e816fb2217ae33a5472fdd7";
+      sha256 = "sha256-CYy9+8YrWO4ZsdKFOyrK5TnwyWqwLjoJLgAyEndl3jc=";
+    };
+
+    # We rely on the internal memenv library. It seems like the original
+    # upstream _may_ have fixed this issue? See
+    # https://github.com/google/leveldb/issues/236
+    postInstall = old.postInstall + ''
+      cp $src/helpers/memenv/memenv.h $dev/include
+    '';
+  });
+
   libsodium = previous.libsodium.overrideAttrs (old: {
     patches = old.patches ++ [
       ../../depends/patches/libsodium/1.0.15-pubkey-validation.diff
@@ -11,12 +33,17 @@ final: previous: {
 
   llvmPackages = final.llvmPackages_14;
 
+  snappy = previous.snappy.override {
+    static = true;
+  };
+
   univalue = final.stdenv.mkDerivation {
     pname = "univalue";
     version = "1.0.3";
     src = final.fetchFromGitHub {
       owner = "bitcoin-core";
       repo = "univalue-subtree";
+      # Pinned to the same revision as the “depends/” build.
       rev = "98fadc090984fa7e070b6c41ccb514f69a371c85";
       sha256 = "sha256-MyV+KBuGLRtgJB9rB62x+D1b1cpy4RxJmXI2SBocxZI=";
     };
