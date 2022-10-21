ad1f88502e167d5a7c495a740b8906e9cc3e7f1d
Greg Pfeil <greg@technomadic.org>
2022-10-20 20:08:41 -1000

    Make univalue a Nix dependency
    The dependency has been removed from Bitcoin, so there’s no need to track it any
    more.

--- contrib/nix/dependencies.nix
+++ contrib/nix/dependencies.nix
@@ -11,6 +11,19 @@ final: previous: {
 
   llvmPackages = final.llvmPackages_14;
 
+  univalue = final.stdenv.mkDerivation {
+    pname = "univalue";
+    version = "1.0.3";
+    src = final.fetchFromGitHub {
+      owner = "bitcoin-core";
+      repo = "univalue-subtree";
+      rev = "98fadc090984fa7e070b6c41ccb514f69a371c85";
+      sha256 = "sha256-MyV+KBuGLRtgJB9rB62x+D1b1cpy4RxJmXI2SBocxZI=";
+    };
+
+    nativeBuildInputs = [ final.autoreconfHook ];
+  };
+
   zeromq = previous.zeromq.overrideAttrs (old: {
     patches = [ ../../depends/patches/zeromq/windows-unused-variables.diff ];
   });
