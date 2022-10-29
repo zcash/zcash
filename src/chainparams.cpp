37799ab56ba3aa17c171f1b31272411e129e5757
Marius Kjærstad <sandakersmann@users.noreply.github.com>
2022-10-29 16:25:12 +0200

    Hardened checkpoint update at block 1860000 for mainnet
    Hardened checkpoint update at block 1860000 for mainnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -242,7 +242,7 @@ public:
         }
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("000000000000000000000000000000000000000000000000098e5c63248dcb28");
+        consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000afb16bd5d58e5be");
 
         /**
          * The message start string should be awesome! ⓩ❤
@@ -293,10 +293,11 @@ public:
             (304600, uint256S("0x00000000028324e022a45014c4a4dc51e95d41e6bceb6ad554c5b65d5cea3ea5"))
             (410100, uint256S("0x0000000002c565958f783a24a4ac17cde898ff525e75ed9baf66861b0b9fcada"))
             (497000, uint256S("0x0000000000abd333f0acca6ffdf78a167699686d6a7d25c33fca5f295061ffff"))
-            (525000, uint256S("0x0000000001a36c500378be8862d9bf1bea8f1616da6e155971b608139cc7e39b")),
-            1556722044,     // * UNIX timestamp of last checkpoint block
-            4653556,        // * total number of transactions between genesis and last checkpoint
-            5106            // * estimated number of transactions per day after checkpoint
+            (525000, uint256S("0x0000000001a36c500378be8862d9bf1bea8f1616da6e155971b608139cc7e39b"))
+            (1860000, uint256S("0x000000000043a968c78af5fb8133e00e6fe340051c19dd969e53ab62bf3dc22a")),
+            1667052073,     // * UNIX timestamp of last checkpoint block
+            11768038,        // * total number of transactions between genesis and last checkpoint
+            3644            // * estimated number of transactions per day after checkpoint
                             //   total number of tx / (checkpoint block height / (24 * 24))
         };
 
