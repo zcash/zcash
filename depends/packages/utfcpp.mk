4e76a216d67a3dd24cf8ad9faf20d43fb290a8a9
Greg Pfeil <greg@z.cash>
2022-12-01 20:51:45 -0700

    Put utf8.h in the correct place
    Both CMake and Nix want it to be here, so I obliged.

--- depends/packages/utfcpp.mk
+++ depends/packages/utfcpp.mk
@@ -6,5 +6,6 @@ $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_sha256_hash=8d6aa7d77ad0abb35bb6139cb9a33597ac4c5b33da6a004ae42429b8598c9605
 
 define $(package)_stage_cmds
-  cp -a ./source $($(package)_staging_dir)$(host_prefix)/include
+  mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
+  cp -a ./source $($(package)_staging_dir)$(host_prefix)/include/utf8cpp
 endef
