3914b7f9c295dd12c03a1523aa8c61ddc6e68bdd
Greg Pfeil <greg@technomadic.org>
2022-12-12 21:26:51 -1000

    Don’t hardcode URLs for Nix depends

--- contrib/nix/dependencies/depends/default.nix
+++ contrib/nix/dependencies/depends/default.nix
@@ -5,18 +5,17 @@ in {
   db = mkDependsDerivation {
     pkg = final.db62;
     dependsPkgName = "bdb";
-    url = pname: version: "https://download.oracle.com/berkeley-db/${pname}-${version}.tar.gz";
   };
 
   boost = mkDependsDerivation {
     pkg = final.boost180;
+    # The URL substitution here is a bit too complex for `mkDependsDerivation`.
     url = pname: version: "https://boostorg.jfrog.io/artifactory/main/release/${builtins.replaceStrings ["_"] ["."] version}/source/${pname}_${version}.tar.bz2";
   };
 
   gtest = mkDependsDerivation {
     pkg = previous.gtest;
     dependsPkgName = "googletest";
-    url = pname: version: "https://github.com/google/${pname}/archive/release-${version}.tar.gz";
   };
 
   # FIXME: This is not pinned precisely enough, but we need this level rather
@@ -26,41 +25,29 @@ in {
   # This subsumes libcxx, clang
   llvmPackages = final.llvmPackages_14;
 
-  # libevent = mkDependsDerivation {
-  #   pkg = previous.libevent;
-  #   url = pname: version:
-  #     "https://github.com/libevent/libevent/archive/release-${version}-stable.tar.gz";
-  # };
+  # libevent = mkDependsDerivation {pkg = previous.libevent;};
 
-  libsodium = mkDependsDerivation {
-    pkg = previous.libsodium;
-    url = pname: version: "https://download.libsodium.org/libsodium/releases/${pname}-${version}.tar.gz";
-  };
+  libsodium = mkDependsDerivation {pkg = previous.libsodium;};
 
   ccache = mkDependsDerivation {
     pkg = previous.ccache;
     dependsPkgName = "native_ccache";
-    url = pname: version: "https://github.com/ccache/ccache/releases/download/v${version}/${pname}-${version}.tar.gz";
   };
 
   cctools = mkDependsDerivation {
     pkg = previous.cctools;
     dependsPkgName = "native_cctools";
-    url = pname: version: "https://github.com/tpoechtrager/cctools-port/archive/${version}.tar.gz";
   };
 
   libtapi = mkDependsDerivation {
     pkg = previous.libtapi;
     dependsPkgName = "native_cctools";
-    url = pname: version: "https://github.com/tpoechtrager/apple-libtapi/archive/${version}.tar.gz";
     infix = "_libtapi";
   };
 
   # cxx-rs = mkDependsDerivation {
   #   pkg = previous.cxx-rs;
   #   dependsPkgName = "native_cxxbridge";
-  #   url = pname: version:
-  #     "https://github.com/dtolnay/cxx/archive/refs/tags/${version}.tar.gz";
   # };
 
   # Depends only uses libtinfo, but outside of a Deb file, it’s only packaged as
@@ -84,12 +71,7 @@ in {
   # utf8cpp = mkDependsDerivation {
   #   pkg = previous.utf8cpp;
   #   dependsPkgName = "utfcpp";
-  #   url = pname: version:
-  #     "https://github.com/nemtrif/utfcpp/archive/v${version}.tar.gz";
   # };
 
-  zeromq = mkDependsDerivation {
-    pkg = previous.zeromq;
-    url = pname: version: "https://github.com/zeromq/libzmq/releases/download/v${version}/${pname}-${version}.tar.gz";
-  };
+  zeromq = mkDependsDerivation {pkg = previous.zeromq;};
 }
--- contrib/nix/dependencies/depends/mk-depends-derivation.nix
+++ contrib/nix/dependencies/depends/mk-depends-derivation.nix
@@ -1,31 +1,47 @@
 ## This requires minimal configuration and mostly pulls the information from the
 ## relevant .mk file.
 pkgs: let
-  readSHA = infix: contents:
-    builtins.elemAt
-    (builtins.match ".*\\$\\(package\\)${infix}_sha256_hash=([^\n]*).*" contents)
-    0;
-  readVersion = infix: contents:
-    builtins.elemAt
-    (builtins.match ".*\\$\\(package\\)${infix}_version=([^\n]*).*" contents)
-    0;
+  readField = regex: infix: contents: let
+    match =
+      builtins.match ".*\\$\\(package\\)${infix}_${regex}=([^\n]*).*" contents;
+  in
+    if match == null
+    then null
+    else builtins.elemAt match 0;
+  readDownloadFile = readField "download_file";
+  readDownloadPath = readField "download_path";
+  readFileName = readField "file_name";
+  readSHA = readField "sha256_hash";
+  readURL = infix: contents: dependsPkgName: version: let
+    downloadFile = readDownloadFile infix contents;
+  in
+    builtins.replaceStrings
+    ["$(package)" "$($(package)_version)"]
+    [dependsPkgName version]
+    (readDownloadPath infix contents
+      + "/"
+      + (
+        if downloadFile == null
+        then readFileName infix contents
+        else downloadFile
+      ));
+  readVersion = readField "version";
   readPatches = dependsPkgName: infix: contents: let
-    patchList =
-      builtins.match ".*\\$\\(package\\)${infix}_patches=([^\n]*).*" contents;
+    patchList = readField "patches" infix contents;
   in
     if patchList == null
     then []
     else
       map
       (patch: ../../../../depends/patches/${dependsPkgName}/${patch})
-      (pkgs.lib.splitString " " (builtins.elemAt patchList 0));
+      (pkgs.lib.splitString " " patchList);
   fileContents = dependsPkgName:
     builtins.readFile ../../../../depends/packages/${dependsPkgName}.mk;
 in
   {
     pkg,
     dependsPkgName ? pkg.pname,
-    url,
+    url ? null,
     infix ? "",
   }: let
     contents = fileContents dependsPkgName;
@@ -33,7 +49,10 @@ in
     pkg.overrideAttrs (old: rec {
       version = readVersion infix contents;
       src = pkgs.fetchurl {
-        url = url old.pname version;
+        url =
+          if url == null
+          then readURL infix contents dependsPkgName version
+          else url dependsPkgName version;
         sha256 = readSHA infix contents;
       };
       patches = readPatches dependsPkgName infix contents;
