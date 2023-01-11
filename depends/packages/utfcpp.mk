0e33a7b313beb3b69f4a4fbcc974f2f656c0f28e
Jack Grigg <jack@z.cash>
2023-01-11 15:53:20 +0000

    depends: utfcpp 3.2.3

--- depends/packages/utfcpp.mk
+++ depends/packages/utfcpp.mk
@@ -1,9 +1,9 @@
 package=utfcpp
-$(package)_version=3.2.1
+$(package)_version=3.2.3
 $(package)_download_path=https://github.com/nemtrif/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=v$($(package)_version).tar.gz
-$(package)_sha256_hash=8d6aa7d77ad0abb35bb6139cb9a33597ac4c5b33da6a004ae42429b8598c9605
+$(package)_sha256_hash=3ba9b0dbbff08767bdffe8f03b10e596ca351228862722e4c9d4d126d2865250
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
