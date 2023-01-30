145f2c31edc8f0336b08c7ee25dfe0b42611e733
Daira Hopwood <daira@jacaranda.org>
2023-01-30 20:22:25 +0000

    Use a more recent URL format for GitHub release archives.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- depends/packages/utfcpp.mk
+++ depends/packages/utfcpp.mk
@@ -1,6 +1,6 @@
 package=utfcpp
 $(package)_version=3.2.3
-$(package)_download_path=https://github.com/nemtrif/$(package)/archive/
+$(package)_download_path=https://github.com/nemtrif/$(package)/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_sha256_hash=3ba9b0dbbff08767bdffe8f03b10e596ca351228862722e4c9d4d126d2865250
