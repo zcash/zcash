965a908ebabc49ca13225db6dea49e8bc70bc860
Daira Hopwood <daira@jacaranda.org>
2023-02-09 11:25:46 +0000

    Use the same convention for the tl_expected download files as for native_cctools
    No 1.0.1 version has been released, and it would be at a different commit if it were.

--- depends/packages/tl_expected.mk
+++ depends/packages/tl_expected.mk
@@ -1,8 +1,8 @@
 package=tl_expected
-$(package)_version=1.0.1
+$(package)_version=96d547c03d2feab8db64c53c3744a9b4a7c8f2c5
 $(package)_download_path=https://github.com/TartanLlama/expected/archive
-$(package)_download_file=96d547c03d2feab8db64c53c3744a9b4a7c8f2c5.tar.gz
-$(package)_file_name=tl-expected-1.0.1.tar.gz
+$(package)_download_file=$($(package)_version).tar.gz
+$(package)_file_name=$(package)_$($(package)_version).tar.gz
 $(package)_sha256_hash=64901df1de9a5a3737b331d3e1de146fa6ffb997017368b322c08f45c51b90a7
 $(package)_patches=remove-undefined-behaviour.diff
 
