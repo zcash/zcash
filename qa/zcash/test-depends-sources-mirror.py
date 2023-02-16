7c613dd5ff7918c32a45f8f494cd568e6db1ba9a
Daira Hopwood <daira@jacaranda.org>
2023-02-16 12:53:58 +0000

    Improvements to `qa/zcash/test-depends-sources-mirror.py`:
    * report which file is being checked before making the request;
    * always check all files rather than exiting on the first failure;
    * download and check the full contents.
    
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- qa/zcash/test-depends-sources-mirror.py
+++ qa/zcash/test-depends-sources-mirror.py
@@ -12,32 +12,40 @@ import sys
 import os
 import requests
 
-MIRROR_URL_DIR="https://download.z.cash/depends-sources/"
-DEPENDS_SOURCES_DIR=os.path.realpath(os.path.join(
+MIRROR_URL_DIR = "https://download.z.cash/depends-sources/"
+DEPENDS_SOURCES_DIR = os.path.realpath(os.path.join(
     os.path.dirname(__file__),
     "..", "..", "depends", "sources"
 ))
 
 def get_depends_sources_list():
-    return filter(
-	lambda f: os.path.isfile(os.path.join(DEPENDS_SOURCES_DIR, f)),
-	os.listdir(DEPENDS_SOURCES_DIR)
-    )
+    return filter(os.path.isfile, [os.path.join(DEPENDS_SOURCES_DIR, f) for f in os.listdir(DEPENDS_SOURCES_DIR)])
 
-for filename in get_depends_sources_list():
-    resp = requests.head(MIRROR_URL_DIR + filename)
-
+passing = True
+for path in get_depends_sources_list():
+    filename = os.path.basename(path)
     print("Checking [" + filename + "] ...")
 
+    resp = requests.head(MIRROR_URL_DIR + filename)
     if resp.status_code != 200:
-	    print("FAIL. File not found on server: ", filename)
-	    sys.exit(1)
+        print("FAIL. File %s not found on server (status code %d)" % (filename, resp.status_code))
+        passing = False
+        continue
 
-    expected_size = os.path.getsize(os.path.join(DEPENDS_SOURCES_DIR, filename))
+    expected_size = os.path.getsize(path)
     server_size = int(resp.headers['Content-Length'])
     if expected_size != server_size:
         print("FAIL. On the server, %s is %d bytes, but locally it is %d bytes." % (filename, server_size, expected_size))
-        sys.exit(1)
+        passing = False
+        continue
 
-print("PASS.")
-sys.exit(0)
+    with open(path, 'rb') as f:
+        expected_content = f.read()
+
+    if resp.content != expected_content:
+        print("FAIL. The server and local files for %s have the same length but different contents" % (filename,))
+        passing = False
+        continue
+
+if passing: print("PASS.")
+sys.exit(0 if passing else 1)
