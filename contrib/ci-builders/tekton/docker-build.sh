6154fd4e6fc276fb5ef7419a01458785e2ac0045
Marshall Gaucher <36639405+mdr0id@users.noreply.github.com>
2023-03-21 11:22:36 -0700

    Update docker-build.sh

--- contrib/ci-builders/tekton/docker-build.sh
+++ contrib/ci-builders/tekton/docker-build.sh
@@ -28,16 +28,6 @@
 export LC_ALL=C
 set -exo pipefail
 
-# Debian 9
-docker build . -f Dockerfile-build-python.apt --build-arg FROMBASEOS=debian --build-arg FROMBASEOS_BUILD_TAG=9 -t electriccoinco/zcashd-build-debian9
-docker push electriccoinco/zcashd-build-debian9
-
-docker build . -f Dockerfile-tekton-worker --build-arg BASEOS=debian --build-arg FROMBASEOS=debian --build-arg FROMBASEOS_BUILD_TAG=9 -t electriccoinco/zcashd-worker-debian9
-docker push electriccoinco/zcashd-worker-debian9
-
-docker build . -f Dockerfile-tekton-bench --build-arg BASEOS=debian --build-arg FROMBASEOS=debian --build-arg FROMBASEOS_BUILD_TAG=9 -t electriccoinco/zcashd-bench-debian9
-docker push electriccoinco/zcashd-bench-debian9
-
 # Debian 10
 docker build . -f Dockerfile-build.apt --build-arg FROMBASEOS=debian --build-arg FROMBASEOS_BUILD_TAG=10 -t electriccoinco/zcashd-build-debian10
 docker push electriccoinco/zcashd-build-debian10
@@ -57,16 +47,6 @@ docker push electriccoinco/zcashd-worker-debian11
 
 docker build . -f Dockerfile-tekton-bench --build-arg FROMBASEOS=debian --build-arg FROMBASEOS_BUILD_TAG=11 -t electriccoinco/zcashd-bench-debian11
 docker push electriccoinco/zcashd-bench-debian11
-
-# Ubuntu 16.04
-docker build . -f Dockerfile-build.apt --build-arg FROMBASEOS=ubuntu --build-arg FROMBASEOS_BUILD_TAG=16.04 -t electriccoinco/zcashd-build-ubuntu1604
-docker push electriccoinco/zcashd-build-ubuntu1604
-
-docker build . -f Dockerfile-tekton-worker --build-arg BASEOS=ubuntu --build-arg FROMBASEOS=ubuntu --build-arg FROMBASEOS_BUILD_TAG=1604 -t electriccoinco/zcashd-worker-ubuntu1604
-docker push electriccoinco/zcashd-worker-ubuntu1604
-
-docker build . -f Dockerfile-tekton-bench --build-arg BASEOS=ubuntu --build-arg FROMBASEOS=ubuntu --build-arg FROMBASEOS_BUILD_TAG=1604 -t electriccoinco/zcashd-bench-ubuntu1604
-docker push electriccoinco/zcashd-bench-ubuntu1604
 
 # Ubuntu 18.04
 docker build . -f Dockerfile-build.apt --build-arg FROMBASEOS=ubuntu --build-arg FROMBASEOS_BUILD_TAG=18.04 -t electriccoinco/zcashd-build-ubuntu1804
@@ -97,16 +77,6 @@ docker push electriccoinco/zcashd-worker-ubuntu2204
 
 docker build . -f Dockerfile-tekton-bench --build-arg BASEOS=ubuntu --build-arg FROMBASEOS=ubuntu --build-arg FROMBASEOS_BUILD_TAG=2204 -t electriccoinco/zcashd-bench-ubuntu2204
 docker push electriccoinco/zcashd-bench-ubuntu2204
-
-# Centos8
-docker build . -f Dockerfile-build.centos8 -t electriccoinco/zcashd-build-centos8
-docker push electriccoinco/zcashd-build-centos8
-
-docker build . -f Dockerfile-tekton-worker --build-arg FROMBASEOS=centos --build-arg FROMBASEOS_BUILD_TAG=8  -t electriccoinco/zcashd-worker-centos8
-docker push electriccoinco/zcashd-worker-centos8
-
-docker build . -f Dockerfile-tekton-bench --build-arg FROMBASEOS=centos --build-arg FROMBASEOS_BUILD_TAG=8  -t electriccoinco/zcashd-bench-centos8
-docker push electriccoinco/zcashd-bench-centos8
 
 # Arch 20210418.0.20194
 docker build . -f Dockerfile-build.arch --build-arg FROMBASEOS=archlinux --build-arg FROMBASEOS_BUILD_TAG=base-20210418.0.20194 -t electriccoinco/zcashd-build-archlinux
