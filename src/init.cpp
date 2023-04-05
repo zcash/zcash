36b60090f854b02cd92880a3f3956d57201e102e
Greg Pfeil <greg@electriccoin.co>
2023-04-05 15:52:33 -0600

    Enable -Wundef

--- src/init.cpp
+++ src/init.cpp
@@ -1041,11 +1041,11 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
     // Turn off Microsoft heap dump noise
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_WARN, CreateFileA("NUL", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, 0));
-#endif
 #if _MSC_VER >= 1400
     // Disable confusing "helpful" text message on abort, Ctrl-C
     _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
-#endif
+#endif // _MSC_VER >= 1400
+#endif // defined(_MSC_VER)
 #ifdef WIN32
     // Enable Data Execution Prevention (DEP)
     SetProcessDEPPolicy(PROCESS_DEP_ENABLE);
