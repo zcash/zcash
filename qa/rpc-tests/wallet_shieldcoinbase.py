e3b18026372d9a87b7240fb8ae022dbd586cd3c2
Greg Pfeil <greg@electriccoin.co>
2023-04-18 19:39:47 -0600

    Don’t test “Insufficient funds” for `z_shieldcoinbase`
    I don’t think it’s possible for this error condition to be tested for `z_shieldcoinbase` any more
    
    1. to hit this failure, we need a fee higher than the amount available in coinbase UTXOs;
    2. if there are no UTXOs to shield, we fail before reaching an “insufficient funds” check;
    3. coinbase UTXOs are >1 ZEC, which is the highest we can set `-maxtxfee` to without emitting a
       warning;
    4. emitting a warning causes the test to fail.

--- qa/rpc-tests/wallet_shieldcoinbase.py
+++ qa/rpc-tests/wallet_shieldcoinbase.py
@@ -87,13 +87,6 @@ class WalletShieldCoinbaseTest (BitcoinTestFramework):
             errorString = e.error['message']
         assert_equal(errorString, "Amount out of range")
 
-        # Shielding will fail because fee is larger than sum of utxos
-        try:
-            self.nodes[0].z_shieldcoinbase("*", myzaddr, 999)
-        except JSONRPCException as e:
-            errorString = e.error['message']
-        assert_equal(errorString, "Insufficient funds: have 50.00, need 999.00.")
-
         # Shielding will fail because limit parameter must be at least 0
         try:
             self.nodes[0].z_shieldcoinbase("*", myzaddr, Decimal('0.001'), -1)
