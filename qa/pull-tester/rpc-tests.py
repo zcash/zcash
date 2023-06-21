c7232730b70e9ad647deab1a1d020ba1eebf1924
Kris Nuttycombe <kris@nutty.land>
2023-06-20 18:44:44 -0600

    Add a test demonstrating inability to spend from the tarnished state.

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -63,6 +63,7 @@ BASE_SCRIPTS= [
     'wallet_listnotes.py',
     'wallet_listunspent.py',
     'wallet_golden_5_6_0.py',
+    'wallet_tarnished_5_6_0.py',
     # vv Tests less than 60s vv
     'orchard_reorg.py',
     'fundrawtransaction.py',
