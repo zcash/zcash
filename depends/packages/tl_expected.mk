2f24f2c0cafbd87e54293931a894a74c09439964
Jack Grigg <jack@electriccoin.co>
2023-06-22 22:17:16 +0000

    depends: tl_expected 1.1.0
    Closes zcash/zcash#6698.

--- depends/packages/tl_expected.mk
+++ depends/packages/tl_expected.mk
@@ -1,14 +1,9 @@
 package=tl_expected
-$(package)_version=96d547c03d2feab8db64c53c3744a9b4a7c8f2c5
-$(package)_download_path=https://github.com/TartanLlama/expected/archive
-$(package)_download_file=$($(package)_version).tar.gz
+$(package)_version=1.1.0
+$(package)_download_path=https://github.com/TartanLlama/expected/archive/refs/tags
+$(package)_download_file=v$($(package)_version).tar.gz
 $(package)_file_name=$(package)_$($(package)_version).tar.gz
-$(package)_sha256_hash=64901df1de9a5a3737b331d3e1de146fa6ffb997017368b322c08f45c51b90a7
-$(package)_patches=remove-undefined-behaviour.diff
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/remove-undefined-behaviour.diff
-endef
+$(package)_sha256_hash=1db357f46dd2b24447156aaf970c4c40a793ef12a8a9c2ad9e096d9801368df6
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
