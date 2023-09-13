0331591bda8bfd9321828cdd40f822218fd82a26
Jack Grigg <jack@electriccoin.co>
2023-09-13 20:53:59 +0000

    depends: utfcpp 3.2.4
    No actual changes in this release, but easier to bump it so the update
    checker is happy.

--- depends/packages/utfcpp.mk
+++ depends/packages/utfcpp.mk
@@ -1,9 +1,9 @@
 package=utfcpp
-$(package)_version=3.2.3
+$(package)_version=3.2.4
 $(package)_download_path=https://github.com/nemtrif/$(package)/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=v$($(package)_version).tar.gz
-$(package)_sha256_hash=3ba9b0dbbff08767bdffe8f03b10e596ca351228862722e4c9d4d126d2865250
+$(package)_sha256_hash=fde21a4c519eed25f095a1cd8490167409cc70d7b5e9c38756142e588ccb7c7e
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
