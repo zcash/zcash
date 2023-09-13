0cf8950b3dce20517403b1a4d2ced19a4ece8b4f
Jack Grigg <jack@electriccoin.co>
2023-09-13 21:40:57 +0000

    depends: native_cmake 3.27.4

--- depends/packages/native_cmake.mk
+++ depends/packages/native_cmake.mk
@@ -1,8 +1,8 @@
 package=native_cmake
-$(package)_version=3.26.4
+$(package)_version=3.27.4
 $(package)_download_path=https://github.com/Kitware/CMake/releases/download/v$($(package)_version)
 $(package)_file_name=cmake-$($(package)_version).tar.gz
-$(package)_sha256_hash=313b6880c291bd4fe31c0aa51d6e62659282a521e695f30d5cc0d25abbd5c208
+$(package)_sha256_hash=0a905ca8635ca81aa152e123bdde7e54cbe764fdd9a70d62af44cad8b92967af
 
 define $(package)_set_vars
 $(package)_config_opts += -DCMAKE_BUILD_TYPE:STRING=Release
