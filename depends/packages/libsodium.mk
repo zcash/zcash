3a6bd8ac38fab33cacf55d3f4b784ffa35686690
Jack Grigg <jack@electriccoin.co>
2024-05-04 00:34:42 +0000

    depends: Apply libsodium patch to fix aarch64 compilation with Clang 18
    Source: jedisct1/libsodium@8f453f41f8834e0fe47610f2a3e03e696ddb3450

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -4,12 +4,13 @@ $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=018d79fe0a045cca07331d37bd0cb57b2e838c51bc48fd837a1472e50068bbea
 $(package)_dependencies=
-$(package)_patches=1.0.15-pubkey-validation.diff 1.0.15-signature-validation.diff
+$(package)_patches=1.0.15-pubkey-validation.diff 1.0.15-signature-validation.diff 1321-aarch64-fix-compiler-attributes.patch
 $(package)_config_opts=
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/1.0.15-pubkey-validation.diff && \
   patch -p1 < $($(package)_patch_dir)/1.0.15-signature-validation.diff && \
+  patch -p1 < $($(package)_patch_dir)/1321-aarch64-fix-compiler-attributes.patch && \
   cd $($(package)_build_subdir); DO_NOT_UPDATE_CONFIG_SCRIPTS=1 ./autogen.sh
 endef
 
