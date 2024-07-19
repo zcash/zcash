2eded3e37850739b71832b9375da5966f4987064
Jack Grigg <jack@electriccoin.co>
2024-07-19 00:59:18 +0000

    depends: libsodium 1.0.20

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,16 +1,15 @@
 package=libsodium
-$(package)_version=1.0.19
+$(package)_version=1.0.20
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=018d79fe0a045cca07331d37bd0cb57b2e838c51bc48fd837a1472e50068bbea
+$(package)_sha256_hash=ebb65ef6ca439333c2bb41a0c1990587288da07f6c7fd07cb3a18cc18d30ce19
 $(package)_dependencies=
-$(package)_patches=1.0.15-pubkey-validation.diff 1.0.15-signature-validation.diff 1321-aarch64-fix-compiler-attributes.patch
+$(package)_patches=1.0.15-pubkey-validation.diff 1.0.15-signature-validation.diff
 $(package)_config_opts=
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/1.0.15-pubkey-validation.diff && \
   patch -p1 < $($(package)_patch_dir)/1.0.15-signature-validation.diff && \
-  patch -p1 < $($(package)_patch_dir)/1321-aarch64-fix-compiler-attributes.patch && \
   cd $($(package)_build_subdir); DO_NOT_UPDATE_CONFIG_SCRIPTS=1 ./autogen.sh
 endef
 
