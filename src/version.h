e81421f8d9e0706fe35988246eee116fdf4b959c
Daira-Emma Hopwood <daira@jacaranda.org>
2024-08-23 15:38:31 +0100

    Delete protocol version constants that are no longer used.
    Signed-off-by: Daira-Emma Hopwood <daira@jacaranda.org>

--- src/version.h
+++ src/version.h
@@ -15,21 +15,16 @@ static const int PROTOCOL_VERSION = 170100;
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-//! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
-
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 170002;
 
-//! nTime field added to CAddress, starting with this version;
-//! if possible, avoid requesting addresses nodes older than this
+//! nTime field added to CAddress, starting with this version.
+//! This can't be removed because it affects the encoding of the
+//! `addrFrom` field in a "version" message.
 static const int CADDR_TIME_VERSION = 31402;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
-
-//! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
 static const int NO_BLOOM_VERSION = 170004;
