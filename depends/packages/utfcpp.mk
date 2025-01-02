8e68e5b18153907dae7585303a65f5cd443c4ec9
Jack Grigg <jack@electriccoin.co>
2025-01-02 20:31:44 +0000

    depends: utfcpp 4.0.6

--- depends/packages/utfcpp.mk
+++ depends/packages/utfcpp.mk
@@ -1,9 +1,9 @@
 package=utfcpp
-$(package)_version=4.0.5
+$(package)_version=4.0.6
 $(package)_download_path=https://github.com/nemtrif/$(package)/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=v$($(package)_version).tar.gz
-$(package)_sha256_hash=ffc668a310e77607d393f3c18b32715f223da1eac4c4d6e0579a11df8e6b59cf
+$(package)_sha256_hash=6920a6a5d6a04b9a89b2a89af7132f8acefd46e0c2a7b190350539e9213816c0
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
