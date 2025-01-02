90ef1beea4d26eba7e580727937a867a008cfa63
Jack Grigg <jack@electriccoin.co>
2025-01-02 20:41:39 +0000

    depends: native_xxhash 0.8.3

--- depends/packages/native_xxhash.mk
+++ depends/packages/native_xxhash.mk
@@ -1,9 +1,9 @@
 package=native_xxhash
-$(package)_version=0.8.2
+$(package)_version=0.8.3
 $(package)_download_path=https://github.com/Cyan4973/xxHash/archive/refs/tags
 $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_file_name=xxhash-$($(package)_version).tar.gz
-$(package)_sha256_hash=baee0c6afd4f03165de7a4e67988d16f0f2b257b51d0e3cb91909302a26a79c4
+$(package)_sha256_hash=aae608dfe8213dfd05d909a57718ef82f30722c392344583d3f39050c7f29a80
 
 define $(package)_build_cmds
   $(MAKE) libxxhash.a
