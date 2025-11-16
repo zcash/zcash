25fdd2348d4b1d2be10bf9c1ea186630a05143e8
lau90eth <lau90eth@gmail.com>
2025-11-16 12:06:43 +0100

    fix: correct mining options help text

--- src/init.cpp
+++ src/init.cpp
@@ -495,12 +495,18 @@ std::string HelpMessage(HelpMessageMode mode)
 
 #ifdef ENABLE_MINING
     strUsage += HelpMessageGroup(_("Mining options:"));
-    strUsage += HelpMessageOpt("-gen", strprintf(_("Generate coins (default: %u)"), DEFAULT_GENERATE));
-    strUsage += HelpMessageOpt("-genproclimit=<n>", strprintf(_("Set the number of threads for coin generation if enabled (-1 = all cores, default: %d)"), DEFAULT_GENERATE_THREADS));
-    strUsage += HelpMessageOpt("-equihashsolver=<name>", _("Specify the Equihash solver to be used if enabled (default: \"default\")"));
-    strUsage += HelpMessageOpt("-mineraddress=<addr>", _("Send mined coins to a specific single address"));
-    strUsage += HelpMessageOpt("-minetolocalwallet", strprintf(
-            _("Require that mined blocks use a coinbase address in the local wallet (default: %u)"),
+strUsage += HelpMessageOpt("-gen", strprintf(_("Generate coins (default: %u)"), DEFAULT_GENERATE));
+strUsage += HelpMessageOpt("-genproclimit", strprintf(
+    _("Limit on number of processors to use for mining. Use -1 for all cores (default: %d)"),
+    DEFAULT_GENERATE_THREADS));
+strUsage += HelpMessageOpt("-equihashsolver=<name>",
+    _("Specify the Equihash solver to be used if enabled (default: %s)"),
+    DEFAULT_EQUIHASH_SOLVER.c_str()));
+strUsage += HelpMessageOpt("-mineraddress=<addr>",
+    _("Send mined coins to a specific single address"));
+strUsage += HelpMessageOpt("-minetolocalwallet", strprintf(
+    _("Require that mined blocks use a coinbase address in the local wallet (default: %u)"),
+    1));
  #ifdef ENABLE_WALLET
             1
  #else
