9a83cdcf0c8eeec7e6520d420dd86bbc9011fd19
Jack Grigg <thestr4d@gmail.com>
2026-05-30 22:57:08 +0000

    depends: Update Clang / libcxx to LLVM 22.1.2

--- depends/packages/libcxx.mk
+++ depends/packages/libcxx.mk
@@ -1,18 +1,18 @@
 package=libcxx
 $(package)_version=$(if $(native_clang_version_$(host_arch)_$(host_os)),$(native_clang_version_$(host_arch)_$(host_os)),$(if $(native_clang_version_$(host_os)),$(native_clang_version_$(host_os)),$(native_clang_default_version)))
-$(package)_msys2_version=18.1.6-1
+$(package)_msys2_version=22.1.2-1
 
 ifneq ($(canonical_host),$(build))
 ifneq ($(host_os),mingw32)
 # Clang is provided pre-compiled for a bunch of targets; fetch the one we need
 # and stage its copies of the static libraries.
 $(package)_download_path=$(native_clang_download_path)
-$(package)_download_file_aarch64_linux=clang+llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
-$(package)_file_name_aarch64_linux=clang-llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
-$(package)_sha256_hash_aarch64_linux=dcaa1bebbfbb86953fdfbdc7f938800229f75ad26c5c9375ef242edad737d999
-$(package)_download_file_linux=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-22.04.tar.xz
-$(package)_file_name_linux=clang-llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-22.04.tar.xz
-$(package)_sha256_hash_linux=54ec30358afcc9fb8aa74307db3046f5187f9fb89fb37064cdde906e062ebf36
+$(package)_download_file_aarch64_linux=LLVM-$($(package)_version)-Linux-ARM64.tar.xz
+$(package)_file_name_aarch64_linux=LLVM-$($(package)_version)-Linux-ARM64.tar.xz
+$(package)_sha256_hash_aarch64_linux=cf2e84d965a95954971cafc71d18c0eb38e723c3ac7276286fd5636df4374b3a
+$(package)_download_file_linux=LLVM-$($(package)_version)-Linux-X64.tar.xz
+$(package)_file_name_linux=LLVM-$($(package)_version)-Linux-X64.tar.xz
+$(package)_sha256_hash_linux=ff32497b6801267ee427bc69cdaeecfb2d19578af8c2a942e864c45215f9a2ac
 
 # Starting from LLVM 14.0.0, some Clang binary tarballs store libc++ in a
 # target-specific subdirectory.
@@ -30,7 +30,7 @@ else
 $(package)_download_path=https://repo.msys2.org/mingw/x86_64
 $(package)_download_file=mingw-w64-x86_64-libc++-$($(package)_msys2_version)-any.pkg.tar.zst
 $(package)_file_name=mingw-w64-x86_64-libcxx-$($(package)_msys2_version)-any.pkg.tar.zst
-$(package)_sha256_hash=6857cc24cbe326c0cd571e52872bffe95f64ab67a8c5032c04875a9778fdece7
+$(package)_sha256_hash=0b8a1fc7f074ebaf6b230d02c9de8c5fbf5f880908a12fd196b05a19ebf00c51
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/lib && \
