257d0a051fd1e23d262f78046375b093c462a255
nuttycom <nuttycom@users.noreply.github.com>
2026-07-09 13:34:32 +0000

    deploy: 95bc5e5c368cf9a4c46fd2f1da51cdf1c2333e19

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-73408213.js',
+                path_to_root + 'searchindex-d862c617.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
